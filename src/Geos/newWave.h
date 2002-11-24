6307f3dce4b14388d0705f5be7cbca4abf33cf1d
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2002-11-24 01:43:48 +0000

    initial commit
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@29 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Geos/newWave.h
@@ -0,0 +1,116 @@
+/*
+ *  GEM - Graphics Environment for Multimedia
+ *
+ *  newWave.h
+ *  gem_darwin
+ *
+ *  Created by Jamie Tittle on Thu Oct 10 2002.
+ *  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
+ *    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ *    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+ *
+ */
+#ifndef INCLUDE_NEWWAVE_H_
+#define INCLUDE_NEWWAVE_H_
+
+#ifdef MACOSX
+#include <OpenGL/glu.h>
+#else
+#include <GL/glu.h>
+#endif MACOSX
+
+#include "Base/GemShape.h"
+#define MAXGRID 63
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+CLASS
+    newWave
+    
+    based on the SGI demo New Wave
+
+KEYWORDS
+    geo
+    
+DESCRIPTION
+
+-----------------------------------------------------------------*/
+class GEM_EXTERN newWave : public GemShape
+{
+  CPPEXTERN_HEADER(newWave, GemShape)
+
+    public:
+
+  //////////
+  // Constructor
+  newWave( t_floatarg width, t_floatarg height);
+    	
+ protected:
+    	
+  //////////
+  // Destructor
+  virtual ~newWave();
+
+  //////////
+  // The height of the object
+  void	    	heightMess(float size);
+        
+  //////////
+  // Do the rendering
+  virtual void 	render(GemState *state);
+  
+    	//////////
+    	// How the object should be drawn
+    	virtual void	typeMess(t_symbol *type);
+		
+    	//////////
+    	// The height of the object
+        GLfloat	    	m_height;
+
+        //////////
+        // The height inlet
+        t_inlet         *m_inletH;
+  
+  //////////
+  // getStuff
+  void		getforce(void);
+  void 		getvelocity(void);
+  void 		getposition(void);
+  void		getTexCoords(void);
+  void		setSize( int value );
+  void		setSpeed( int value );
+
+  void copy( float vec0[3], float vec1[3]);
+  void sub(float vec0[3], float vec1[3], float vec2[3]);
+  void add(float vec0[3], float vec1[3], float vec2[3]);
+  void scalDiv(float vec[3], float c);
+  void cross(float vec0[3], float vec1[3], float vec2[3]);
+  void norm(float vec[3]);
+  void set(float vec[3], float x, float y, float z);
+  void reset( int value );
+  void setOther( int value );
+  
+  void getFaceNorms( void );
+  void getVertNorms( void );
+  void getFaceNormSegs( void );
+  
+  int 		m_size;
+  int		m_speed;
+  
+  float force[MAXGRID][MAXGRID],
+      veloc[MAXGRID][MAXGRID],
+      posit[MAXGRID][MAXGRID],
+      vertNorms[MAXGRID][MAXGRID][3],
+      faceNorms[2][MAXGRID][MAXGRID][3],
+      faceNormSegs[2][2][MAXGRID][MAXGRID][3];
+  int alreadyInit;
+  float texCoords[MAXGRID][MAXGRID][2];
+
+ private:
+
+  //////////
+  // static member functions
+  static void 	heightMessCallback(void *data, t_floatarg size);
+};
+
+#endif	// for header file
