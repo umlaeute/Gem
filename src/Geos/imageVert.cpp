4e8e33be630bcc04b36d26a5ef1c5ad687d4a47b
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2002-11-26 23:11:23 +0000

    added MACOSX support; initial YUV support
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@46 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Geos/imageVert.cpp
+++ src/Geos/imageVert.cpp
@@ -9,6 +9,7 @@
 //    Copyright (c) 1997-1999 Mark Danks.
 //    Copyright (c) Günther Geiger.
 //    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+//    Copyright (c) 2002 James Tittle & Chris Clepper
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 //
@@ -230,6 +231,14 @@ void imageVert :: processGrayPix(imageStruct &image, int texture)
     }
 }
 
+/////////////////////////////////////////////////////////
+// processYUVPix
+//
+/////////////////////////////////////////////////////////
+void imageVert :: processYUVPix(imageStruct &image, int texture)
+{
+    post("GEM:imageVert: YUV not yet implemented :-(");
+}
 /////////////////////////////////////////////////////////
 // render
 //
--- src/Geos/imageVert.h
+++ src/Geos/imageVert.h
@@ -7,6 +7,7 @@ LOG
     Copyright (c) 1997-1999 Mark Danks. mark@danks.org
     Copyright (c) Günther Geiger. geiger@epy.co.at
     Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+    Copyright (c) 2002 James Tittle & Chris Clepper
     For information on usage and redistribution, and for a DISCLAIMER OF ALL
     WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
@@ -63,6 +64,12 @@ class GEM_EXTERN imageVert : public GemPixObj
 		// [in] texture - Is texture mapping turned on?
     	virtual void 	processGrayPix(imageStruct &image, int texture);
 
+    	//////////
+    	// Do the processing for YUV image
+		// [in] image - The image to use
+		// [in] texture - Is texture mapping turned on?
+    	virtual void 	processYUVPix(imageStruct &image, int texture);
+        
     	//////////
     	// Do the rendering.
     	virtual void 	render(GemState *state);
