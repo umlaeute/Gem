3843b28cd9186fe86ff734d751319c4dcf388532
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2002-12-08 20:10:46 +0000

    added framewise delay!
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@64 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/TV/tv_delay.h
@@ -0,0 +1,69 @@
+/*-----------------------------------------------------------------
+LOG
+    GEM - Graphics Environment for Multimedia
+
+    play puzzle with a sequence of pixBufs
+
+    Copyright (c) 1997-1999 Mark Danks. mark@danks.org
+    Copyleft  (l) 2001 IOhannes m zmölnig. zmoelnig@iem.kug.ac.at
+    Copyleft (l) 2002 James Tittle & Chris Clepper
+    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_TV_DELAY_H_
+#define INCLUDE_TV_DELAY_H_
+
+#include "Pixes/pix_fx.h"
+
+#define MAX_FRAMES 256
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  tv_delay
+  
+  change the pixBuf into dots
+  
+  KEYWORDS
+  pix
+    
+  DESCRIPTION
+   
+  -----------------------------------------------------------------*/
+class GEM_EXTERN tv_delay : public pix_fx
+{
+  CPPEXTERN_HEADER(tv_delay, GemPixObj)
+
+    public:
+
+  //////////
+  // Constructor
+  tv_delay(t_float &f);
+    	
+ protected:
+    	
+  //////////
+  // Destructor
+  virtual ~tv_delay();
+
+  //////////
+  // Do the processing
+  virtual void 	processFX(imageStruct &image);
+
+  imageStruct    myImage;
+
+  virtual void  delayMess(int frames);
+
+  int m_maxframes, m_curframe;
+  int m_frame;
+   
+
+ private:
+  
+  //////////
+  // static member functions
+  static void delayMessCallback(void *data, t_floatarg);
+};
+
+#endif	// for header file
