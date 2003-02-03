c1dc50974dd86a3b5ab99691a10a3b7fd1ac7978
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-02-03 16:34:10 +0000

    rewrote the configure-thing hopefully it works now...
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@94 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/config.h
+++ src/Base/config.h
@@ -1,24 +1,9 @@
 /* ../Base/config.h.  Generated automatically by configure.  */
-/* Define if you have the <libmpeg3.h> header file.  */
+#define HAVE_LIBGLUT 1
 
-#define HAVE_LIBMPEG3_H 1 
-/* #undef HAVE_MPEG3 */ 
+/* #undef HAVE_LIBMPEG */
+#define HAVE_LIBMPEG3 1
+#define HAVE_LIBQUICKTIME 1
 
-/* Define if you have the <quicktime/quicktime.h> header file.  */
 
-/* #undef HAVE_QUICKTIME_H */ 
-/* #undef HAVE_QUICKTIME */
-
-/* Define if you have the <quicktime/libdv/libdv.h> header file. */
-
-/* #undef HAVE_LIBDV_H */ 
-/* #undef HAVE_LIBDV */
-
-/* Define if libglut is present */
-
-/* #undef HAVE_LIBGLUT */ 
-
-/* Define if the system has mmx support */
-
-/* #undef HAVE_MMX */ 
-
+/* #undef HAVE_MMX */
