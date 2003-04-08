db542c979972849a90f316c9830f5132b2bea350
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-04-08 08:32:08 +0000

    dos2unix
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@244 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Pixes/pix_videoDS.cpp
+++ src/Pixes/pix_videoDS.cpp
@@ -883,6 +883,7 @@ void pix_videoDS :: copyBuffer(IMediaSample* pSample)
 void pix_videoDS :: obj_setupCallback(t_class *classPtr)
 {
   class_addcreator((t_newmethod)_classpix_videoDS, gensym("pix_videoDS"), A_DEFFLOAT, A_NULL);
+  class_addcreator((t_newmethod)_classpix_videoDS, gensym("pix_dv"), A_DEFFLOAT, A_NULL);
   pix_video::real_obj_setupCallback(classPtr);
 
   class_addmethod(classPtr, (t_method)&pix_videoDS::dimenMessCallback,
