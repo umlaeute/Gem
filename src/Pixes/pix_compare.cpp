f626b608483bf4e0793078771724104c2355c9b5
cclepper <cclepper@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-05-28 03:26:14 +0000

    initial commit
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@419 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Pixes/pix_compare.cpp
@@ -0,0 +1,264 @@
+/*
+ *  pix_compare.cpp
+ *  gem_darwin
+ *
+ *  Created by chris clepper on Mon May 26 2003.
+ *  Copyright (c) 2003.  All rights reserved.
+ *
+ */
+
+#include "pix_compare.h"
+CPPEXTERN_NEW(pix_compare)
+
+/////////////////////////////////////////////////////////
+//
+// pix_compare
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+pix_compare :: pix_compare()
+{
+m_processOnOff=0;
+}
+
+/////////////////////////////////////////////////////////
+// Destructor
+//
+/////////////////////////////////////////////////////////
+pix_compare :: ~pix_compare()
+{
+
+}
+
+/////////////////////////////////////////////////////////
+// processImage
+//
+/////////////////////////////////////////////////////////
+void pix_compare :: processRGBA_RGBA(imageStruct &image, imageStruct &right)
+{
+      long src,h,w;
+   src =0;
+
+   //format is U Y V Y
+    if (m_direction) {
+    
+    for (h=0; h<image.ysize; h++){
+        for(w=0; w<image.xsize; w++){
+        
+            if ((image.data[src+chRed] < right.data[src+chRed])
+                &&(image.data[src+chBlue] < right.data[src+chBlue])
+                &&(image.data[src+chGreen] < right.data[src+chGreen]))
+                {
+                image.data[src+chRed] = right.data[src+chRed];
+                image.data[src+chBlue] = right.data[src+chBlue];
+                image.data[src+chGreen] = right.data[src+chGreen];
+                }
+        src+=4;
+            }
+        }
+    } else {
+        
+     for (h=0; h<image.ysize; h++){
+        for(w=0; w<image.xsize; w++){
+        
+            if ((image.data[src+chRed] > right.data[src+chRed])
+                &&(image.data[src+chBlue] > right.data[src+chBlue])
+                &&(image.data[src+chGreen] > right.data[src+chGreen]))
+                {
+                 image.data[src+chRed] = right.data[src+chRed];
+                image.data[src+chBlue] = right.data[src+chBlue];
+                image.data[src+chGreen] = right.data[src+chGreen];
+                }
+        src+=4;
+            }
+        }   
+        
+    };
+}
+
+
+/////////////////////////////////////////////////////////
+// do the YUV processing here
+//
+/////////////////////////////////////////////////////////
+
+
+void pix_compare :: processYUV_YUV(imageStruct &image, imageStruct &right)
+{
+#ifdef ALTIVEC
+processYUVAltivec(image,right);
+return;
+#else
+
+   long src,h,w;
+   src =0;
+
+   //format is U Y V Y
+    if (m_direction) {
+    
+    for (h=0; h<image.ysize; h++){
+        for(w=0; w<image.xsize/2; w++){
+        
+            if ((image.data[src+1] < right.data[src+1])&&(image.data[src+3] < right.data[src+3]))
+                {
+                image.data[src] = right.data[src];
+                image.data[src+1] = right.data[src+1];
+            
+                image.data[src+2] = right.data[src+2];
+                image.data[src+3] = right.data[src+3];
+                }
+        src+=4;
+            }
+        }
+    } else {
+        
+     for (h=0; h<image.ysize; h++){
+        for(w=0; w<image.xsize/2; w++){
+        
+            if ((image.data[src+1] > right.data[src+1])&&(image.data[src+3] > right.data[src+3]))
+                {
+                image.data[src] = right.data[src];
+                image.data[src+1] = right.data[src+1];
+            
+                image.data[src+2] = right.data[src+2];
+                image.data[src+3] = right.data[src+3];
+                }
+        src+=4;
+            }
+        }   
+        
+    }
+#endif //ALTIVEC
+}
+
+/////////////////////////////////////////////////////////
+// do the Altivec YUV processing here
+//
+/////////////////////////////////////////////////////////
+
+void pix_compare :: processYUVAltivec(imageStruct &image, imageStruct &right)
+{
+#ifdef ALTIVEC
+register int h,w,i,j,width;
+
+    h = image.ysize;
+    w = image.xsize/8;
+    width = image.xsize/8;
+    
+    //check to see if the buffer isn't 16byte aligned (highly unlikely)
+    if (image.ysize*image.xsize % 16 != 0){
+        post("pix_compare: image not properly aligned for Altivec");
+        return;
+        }
+
+    register vector unsigned short	UVres1, Yres1, UVres2, Yres2;//interleave;
+    register vector unsigned short	hiImage, loImage;
+    register vector bool short		Ymask1;
+    register vector unsigned char	one = vec_splat_u8(1);
+
+    vector unsigned char	*inData = (vector unsigned char*) image.data;
+    vector unsigned char	*rightData = (vector unsigned char*) right.data;
+   
+    
+    //setup the cache prefetch -- A MUST!!!
+    UInt32			prefetchSize = GetPrefetchConstant( 16, 1, 256 );
+    vec_dst( inData, prefetchSize, 0 );
+    vec_dst( rightData, prefetchSize, 1 );
+    
+    if (m_direction) {
+    
+    for ( i=0; i<h; i++){
+        for (j=0; j<w; j++)
+        {
+        
+        //this function is probably memory bound on most G4's -- what else is new?
+        vec_dst( inData, prefetchSize, 0 );
+        vec_dst( rightData, prefetchSize, 1 );
+
+        //separate the U and V from Y
+        UVres1 = (vector unsigned short)vec_mule(one,inData[0]);
+        UVres2 = (vector unsigned short)vec_mule(one,rightData[0]);
+            
+        //vec_mulo Y * 1 to short vector Y Y Y Y shorts
+        Yres1 = (vector unsigned short)vec_mulo(one,inData[0]);
+        Yres2 = (vector unsigned short)vec_mulo(one,rightData[0]);
+            
+         //compare the Y values   
+         Ymask1 = vec_cmpgt(Yres1,Yres2);
+         
+         //bitwise comparison and move using the result of the comparison as a mask
+         Yres1 = vec_sel(Yres2,Yres1,Ymask1);
+         
+         UVres1 = vec_sel(UVres2,UVres1,Ymask1);
+         
+         //merge the Y and UV back together
+         hiImage = vec_mergeh(UVres1,Yres1);
+         loImage = vec_mergel(UVres1,Yres1);
+         
+         //pack it back down to unsigned char to store
+         inData[0] = vec_packsu(hiImage,loImage);
+         
+            inData++;
+            rightData++;
+        
+        }
+        vec_dss(1);
+        vec_dss(0);
+    }
+    }else{
+    
+    for ( i=0; i<h; i++){
+        for (j=0; j<w; j++)
+        {
+        
+        vec_dst( inData, prefetchSize, 0 );
+        vec_dst( rightData, prefetchSize, 1 );
+
+        
+        UVres1 = (vector unsigned short)vec_mule(one,inData[0]);
+        UVres2 = (vector unsigned short)vec_mule(one,rightData[0]);
+            
+        //vec_mulo Y * 1 to short vector Y Y Y Y shorts
+        Yres1 = (vector unsigned short)vec_mulo(one,inData[0]);
+        Yres2 = (vector unsigned short)vec_mulo(one,rightData[0]);
+            
+         Ymask1 = vec_cmplt(Yres1,Yres2);
+         
+         Yres1 = vec_sel(Yres2,Yres1,Ymask1);
+         
+         UVres1 = vec_sel(UVres2,UVres1,Ymask1);
+         
+         hiImage = vec_mergeh(UVres1,Yres1);
+         loImage = vec_mergel(UVres1,Yres1);
+         
+         inData[0] = vec_packsu(hiImage,loImage);
+         
+            inData++;
+            rightData++;
+        
+        }
+        vec_dss(1);
+        vec_dss(0);
+    }
+
+    
+    }
+#endif
+}
+
+/////////////////////////////////////////////////////////
+// static member function
+//
+/////////////////////////////////////////////////////////
+void pix_compare :: obj_setupCallback(t_class *classPtr)
+{
+  class_addmethod(classPtr, (t_method)&pix_compare::directionCallback,
+		  gensym("direction"), A_DEFFLOAT, A_NULL);
+}
+
+void pix_compare :: directionCallback(void *data, t_floatarg state)
+{
+  GetMyClass(data)->m_direction=!(!(int)state);
+}
---
+++ src/Pixes/pix_compare.h
@@ -0,0 +1,80 @@
+/*-----------------------------------------------------------------
+LOG
+    GEM - Graphics Environment for Multimedia
+
+
+    Copyright (c) 1997-1999 Mark Danks. mark@danks.org
+    Copyright (c) G¸nther Geiger. geiger@epy.co.at
+    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::f¸r::uml‰ute. IEM. zmoelnig@iem.kug.ac.at
+    Copyright (c) 2003 Daniel Heckenberg
+
+    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_pix_compare_H_ 
+#define INCLUDE_pix_compare_H_ 
+
+#include "GemPixDualObj.h"
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+CLASS
+    pix_compare
+    
+    
+
+KEYWORDS
+    pix
+    yuv
+    
+DESCRIPTION
+
+   compares the pixel values of two images
+   
+-----------------------------------------------------------------*/
+
+class GEM_EXTERN pix_compare : public GemPixDualObj
+{
+CPPEXTERN_HEADER(pix_compare, GemPixDualObj)
+
+    public:
+
+	    //////////
+	    // Constructor
+    	pix_compare();
+    	
+    protected:
+    	
+    	//////////
+    	// Destructor
+    	virtual ~pix_compare();
+
+    	
+    	//////////
+    	// Do the processing
+    	virtual void 	processRGBA_RGBA(imageStruct &image, imageStruct &right);
+        
+    	//////////
+    	// Do the processing
+    	virtual void 	processYUV_YUV(imageStruct &image, imageStruct &right);
+
+        //////////
+    	// Do the Altivec processing
+    	virtual void 	processYUVAltivec(imageStruct &image, imageStruct &right);
+
+        
+        int m_direction;
+        
+    private:
+    
+    	//////////
+    	// Static member functions
+    	static void directionCallback       (void *data, t_floatarg state);
+
+
+};
+
+#endif
+
