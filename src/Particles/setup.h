4626c3b21b83c3fb47ea4511dd5e02ebbe4c182c
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-07-15 07:38:48 +0000

    added [part_vertex], [part_velocity], [part_veldomain]
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@462 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Particles/setup.h
+++ src/Particles/setup.h
@@ -12,43 +12,49 @@
 
 extern "C" {
 
-   void part_color_setup();
-   void part_damp_setup();
-   void part_draw_setup();
-   void part_follow_setup();
-   void part_gravity_setup();
-   void part_head_setup();
-   void part_killold_setup();
-   void part_killslow_setup();
-   void part_orbitpoint_setup();
-   void part_size_setup();
-   void part_source_setup();
-   void part_targetcolor_setup();
-   void part_targetsize_setup();
-   void part_velcone_setup();
-   void part_velsphere_setup();
+  void part_color_setup();
+  void part_damp_setup();
+  void part_draw_setup();
+  void part_follow_setup();
+  void part_gravity_setup();
+  void part_head_setup();
+  void part_killold_setup();
+  void part_killslow_setup();
+  void part_orbitpoint_setup();
+  void part_size_setup();
+  void part_source_setup();
+  void part_targetcolor_setup();
+  void part_targetsize_setup();
+  void part_velcone_setup();
+  void part_velsphere_setup();
+  void part_veldomain_setup();
+  void part_velocity_setup();
+  void part_vertex_setup();
   void part_render_setup();
   void part_info_setup();
 
 
-   void Particles_setup() {
-      part_color_setup();
-      part_damp_setup();
-      part_draw_setup();
-      part_follow_setup();
-      part_gravity_setup();
-      part_head_setup();
-      part_killold_setup();
-      part_killslow_setup();
-      part_orbitpoint_setup();
-      part_size_setup();
-      part_source_setup();
-      part_targetcolor_setup();
-      part_targetsize_setup();
-      part_velcone_setup();
-      part_velsphere_setup();
-      part_render_setup();
-      part_info_setup();
-   }
+  void Particles_setup() {
+    part_color_setup();
+    part_damp_setup();
+    part_draw_setup();
+    part_follow_setup();
+    part_gravity_setup();
+    part_head_setup();
+    part_killold_setup();
+    part_killslow_setup();
+    part_orbitpoint_setup();
+    part_size_setup();
+    part_source_setup();
+    part_targetcolor_setup();
+    part_targetsize_setup();
+    part_velcone_setup();
+    part_velsphere_setup();
+    part_veldomain_setup();
+    part_velocity_setup();
+    part_vertex_setup();
+    part_render_setup();
+    part_info_setup();
+  }
 } // for extern "C"
 
