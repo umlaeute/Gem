bb52de1b2f900fb52297e8cebef9643ad21b6cb6
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-09-17 12:22:19 +0000

    added methods for grey
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@557 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Pixes/pix_background.h
+++ src/Pixes/pix_background.h
@@ -13,64 +13,64 @@
 #include "Base/GemPixObj.h"
 
 /*-----------------------------------------------------------------
--------------------------------------------------------------------
-CLASS
-    pix_background
+  -------------------------------------------------------------------
+  CLASS
+  pix_background
     
     
 
-KEYWORDS
-    pix
-    yuv
+  KEYWORDS
+  pix
+  yuv
     
-DESCRIPTION
+  DESCRIPTION
 
-   template for yuv_ objects
+  template for yuv_ objects
    
------------------------------------------------------------------*/
+  -----------------------------------------------------------------*/
 
 class GEM_EXTERN pix_background : public GemPixObj
 {
-CPPEXTERN_HEADER(pix_background, GemPixObj)
+  CPPEXTERN_HEADER(pix_background, GemPixObj)
 
     public:
 
-	    //////////
-	    // Constructor
-    	pix_background();
+  //////////
+  // Constructor
+  pix_background();
     	
-    protected:
+ protected:
     	
-    	//////////
-    	// Destructor
-    	virtual ~pix_background();
+  //////////
+  // Destructor
+  virtual ~pix_background();
 
-    	//////////
-    	// Do the processing
-    	virtual void 	processRGBAImage(imageStruct &image);
-    	
-        //////////
-    	// Do the YUV processing
-    	virtual void 	processYUVImage(imageStruct &image);
+  //////////
+  // Do the processing
+  virtual void 	processRGBAImage(imageStruct &image);
+  virtual void 	processGrayImage(imageStruct &image);
+  virtual void 	processYUVImage(imageStruct &image);
         
-        //////////
-    	// Do the YUV Altivec processing
-    	virtual void 	processYUVImageAltivec(imageStruct &image);
+  //////////
+  // Do the YUV Altivec processing
+  virtual void 	processYUVImageAltivec(imageStruct &image);
         
-        unsigned char  *saved;
-        int		m_Yrange,m_Urange,m_Vrange;
-        int		m_blur,m_blurH,m_blurW,m_blurSize,m_blurBpp;
-        t_inlet         *inletBlur;
-        int		m_reset;
+  unsigned char  *saved;
+  int		m_Yrange,m_Urange,m_Vrange;
+  int		m_blur,m_blurH,m_blurW,m_blurSize,m_blurBpp;
+  t_inlet      *inletBlur;
+  int		m_reset;
 
         
-    private:
+ private:
     
-    	//////////
-    	// Static member functions
+  //////////
+  // Static member functions
     	
-        static void rangeCallback       (void *data, t_floatarg Y, t_floatarg U, t_floatarg V);
-        static void resetCallback       (void *data);
+  static void rangeCallback       (void *data, t_floatarg Y, t_floatarg U, t_floatarg V);
+  //  static void rangeNCallback      (void *data, t_floatarg Y, t_floatarg U, t_floatarg V);
+  static void rangeNCallback      (void *data, t_symbol*,int,t_atom*);
+  static void resetCallback       (void *data);
 
 
 };
