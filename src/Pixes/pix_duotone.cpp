80f06680b66f76e0315b570b307fef8ab7974aef
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2003-09-17 13:07:17 +0000

    result of merge
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@577 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Pixes/pix_duotone.cpp
+++ src/Pixes/pix_duotone.cpp
@@ -7,8 +7,8 @@
 // Implementation file
 //
 //    Copyright (c) 1997-1998 Mark Danks.
-//    Copyright (c) GÂ¸nther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::fÂ¸r::umlâ€°ute. IEM
+//    Copyright (c) Günther Geiger.
+//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
 //    Copyright (c) 2002 James Tittle & Chris Clepper
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
@@ -29,9 +29,13 @@ CPPEXTERN_NEW(pix_duotone)
 /////////////////////////////////////////////////////////
 pix_duotone :: pix_duotone()
 {
-    inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("float"), gensym("ft1"));
-    inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("list"), gensym("vec_offset"));
-  
+  m_color1[0]=m_color1[1]=m_color1[2]=255;
+  m_color2[0]=m_color2[1]=m_color2[2]=000;
+  m_thresh[0]=m_thresh[1]=m_thresh[2]=127;
+
+  inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("float"),gensym("thresh"));
+  inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("list"), gensym("color1"));
+  inlet_new(this->x_obj, &this->x_obj->ob_pd, gensym("list"), gensym("color2"));
 }
 
 /////////////////////////////////////////////////////////
@@ -51,18 +55,16 @@ void pix_duotone :: processRGBAImage(imageStruct &image)
   unsigned char *pixels = image.data;
 
   while(datasize--) {
-
     if ((pixels[chRed] > m_thresh[0]) && (pixels[chGreen] > m_thresh[1]) && (pixels[chBlue] > m_thresh[2])){
-    pixels[chRed]   = m_color1[0];
-    pixels[chGreen] = m_color1[1];
-    pixels[chBlue]  = m_color1[2];
-    
-  } else {
-  pixels[chRed]   = m_color2[0];
-    pixels[chGreen] = m_color2[1];
-    pixels[chBlue]  = m_color2[2];
-  }
-  pixels += 4;
+      pixels[chRed]   = m_color1[0];
+      pixels[chGreen] = m_color1[1];
+      pixels[chBlue]  = m_color1[2];
+    } else {
+      pixels[chRed]   = m_color2[0];
+      pixels[chGreen] = m_color2[1];
+      pixels[chBlue]  = m_color2[2];
+    }
+    pixels += 4;
   }
 }
 
@@ -82,7 +84,7 @@ void pix_duotone :: processRGBImage(imageStruct &image)
     pixels[chBlue]  = m_color1[2];
     
   } else {
-	pixels[chRed]   = m_color2[0];
+    pixels[chRed]   = m_color2[0];
     pixels[chGreen] = m_color2[1];
     pixels[chBlue]  = m_color2[2];
   }
@@ -96,7 +98,14 @@ void pix_duotone :: processRGBImage(imageStruct &image)
 /////////////////////////////////////////////////////////
 void pix_duotone :: processGrayImage(imageStruct &image)
 {
-
+  int datasize =  image.xsize * image.ysize*image.csize;
+  unsigned char *pixels = image.data;
+  unsigned char thresh=m_thresh[0], grey1=m_color1[0], grey2=m_color2[0];
+  
+  while(datasize--) {
+    *pixels=(*pixels>thresh)?grey1:grey2;
+    pixels++;
+  }
 }
 
 /////////////////////////////////////////////////////////
@@ -105,30 +114,28 @@ void pix_duotone :: processGrayImage(imageStruct &image)
 /////////////////////////////////////////////////////////
 void pix_duotone :: processYUVImage(imageStruct &image)
 {
- 
-    int h,w;
-    long src;
+  int h,w;
+  long src;
 
-src = 0;
+  src = 0;
 
-for (h=0; h<image.ysize; h++){
+  for (h=0; h<image.ysize; h++){
     for(w=0; w<image.xsize/2; w++){
-        if ((image.data[src] > m_thresh[1]) && (image.data[src+1] > m_thresh[0]) && (image.data[src+2] > m_thresh[2]))
+      if ((image.data[src] > m_thresh[1]) && (image.data[src+1] > m_thresh[0]) && (image.data[src+2] > m_thresh[2]))
         {
-        image.data[src] = m_color1[1];
-        image.data[src+1] = m_color1[0];
-        image.data[src+2] = m_color1[2];
-        image.data[src+3] = m_color1[0];
+	  image.data[src]   = m_color1[1];
+	  image.data[src+1] = m_color1[0];
+	  image.data[src+2] = m_color1[2];
+	  image.data[src+3] = m_color1[0];
         }else{
-        image.data[src] = m_color2[1];
-        image.data[src+1] = m_color2[0];
-       image.data[src+2] = m_color2[2];
-        image.data[src+3] = m_color2[0];
+	  image.data[src]   = m_color2[1];
+	  image.data[src+1] = m_color2[0];
+	  image.data[src+2] = m_color2[2];
+	  image.data[src+3] = m_color2[0];
         }
-        
-        src+=4;
+      src+=4;
     }
-}
+  }
 }
 
 
@@ -147,21 +154,21 @@ void pix_duotone :: obj_setupCallback(t_class *classPtr)
 }
 void pix_duotone :: color1MessCallback(void *data, t_floatarg value1, t_floatarg value2, t_floatarg value3)
 {
-    GetMyClass(data)->m_color1[0]=(unsigned char)value1;
-    GetMyClass(data)->m_color1[1]=(unsigned char)value2;
-    GetMyClass(data)->m_color1[2]=(unsigned char)value3;
+    GetMyClass(data)->m_color1[0]=(unsigned char)(value1*255);
+    GetMyClass(data)->m_color1[1]=(unsigned char)(value2*255);
+    GetMyClass(data)->m_color1[2]=(unsigned char)(value3*255);
 }
 void pix_duotone :: color2MessCallback(void *data, t_floatarg value1, t_floatarg value2, t_floatarg value3)
 {
-    GetMyClass(data)->m_color2[0]=(unsigned char)value1;
-    GetMyClass(data)->m_color2[1]=(unsigned char)value2;
-    GetMyClass(data)->m_color2[2]=(unsigned char)value3;
+    GetMyClass(data)->m_color2[0]=(unsigned char)(value1*255);
+    GetMyClass(data)->m_color2[1]=(unsigned char)(value2*255);
+    GetMyClass(data)->m_color2[2]=(unsigned char)(value3*255);
 }
 
 void pix_duotone :: threshMessCallback(void *data, t_floatarg value1, t_floatarg value2, t_floatarg value3)
 {
-    GetMyClass(data)->m_thresh[0]=(unsigned char)value1;
-    GetMyClass(data)->m_thresh[1]=(unsigned char)value2;
-    GetMyClass(data)->m_thresh[2]=(unsigned char)value3;
+    GetMyClass(data)->m_thresh[0]=(unsigned char)(value1*255);
+    GetMyClass(data)->m_thresh[1]=(unsigned char)(value2*255);
+    GetMyClass(data)->m_thresh[2]=(unsigned char)(value3*255);
 }
 
