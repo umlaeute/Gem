3c76d55404fc7982477fc1a511497fc8df6eb56d
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2004-08-06 10:20:28 +0000

    loads of functionality from [gemwin] and GemBase moved into this one: color,fog,viewpoint,....
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/branches/multiple_window/Gem@1107 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/GemOutput.cpp
+++ src/Base/GemOutput.cpp
@@ -15,6 +15,18 @@
 /////////////////////////////////////////////////////////
 
 #include "GemOutput.h"
+#include "GemMan.h"
+
+#ifdef  __APPLE__
+// or is it spelled Carbon/Carbon.h ??
+# include <Carbon/carbon.h>
+#include <OpenGL/glu.h>
+# include <OpenGL/glext.h>
+#include "macosx_math.h"
+#else
+#include <GL/glu.h>
+#include <math.h>
+#endif // OS
 
 /////////////////////////////////////////////////////////
 //
@@ -24,49 +36,338 @@
 // Constructor
 //
 /////////////////////////////////////////////////////////
-GemOutput :: GemOutput() : m_outputState(0)
-{}
+GemOutput :: GemOutput() : m_outputState(0), m_outputContext(false),
+			   m_clear_mask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT),
+			   m_fogMode(FOG_OFF),
+			   m_buffer(2),
+			   m_width(500), m_height(500),
+			   m_stereo(0), m_stereoSep(-15.f), m_stereoFocal(0.f), m_stereoLine(true)
+{
+
+  // setup default values
+  perspectiveMess(-1.f, 1.f, -1.f, 1.f, 1.f, 20.f);
+  viewMess       (0.f, 0.f, 4.f,  0.f, 0.f, 0.f,  0.f, 1.f, 0.f);
+
+  colorMess      (0.f, 0.f, 0.f, 0.f);
+  ambientMess    (0.f, 0.f, 0.f, 1.f);
+  specularMess   (1.f, 1.f, 1.f, 1.f);
+  shininessMess  (100.f);
+}
 
 /////////////////////////////////////////////////////////
 // Destructor
 //
 /////////////////////////////////////////////////////////
 GemOutput :: ~GemOutput()
-{
-  destroyMess();
-}
+{}
 
 /////////////////////////////////////////////////////////
 // renderMess
 //
 /////////////////////////////////////////////////////////
-void GemOutput :: doRender(gem_control cb){
-  if(cb!=NULL)(&cb);
+void GemOutput :: doRender(GemState currentState){
+  GemMan::render1(currentState);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  gluLookAt(0, 0, 4,
+	    0, 0, 0,
+	    0, 1, 0);
+
+  GemMan::render2(currentState);
 }
 
-void GemOutput :: renderMess(gem_control cb)
+void GemOutput :: renderMess()
 {
+  GemState currentState;
+  GemMan::fillGemState(currentState);
+
   if(!m_outputState)return;
-  post("GemOutput: rendering: %x", cb);
-  preRender(cb);
-  doRender(cb);
-  postRender(cb);
+  preRender(currentState);
+  doRender(currentState);
+  postRender(currentState);
 }
+
+
 /////////////////////////////////////////////////////////
-// createMess
+// viewMess
 //
 /////////////////////////////////////////////////////////
-void GemOutput :: createMess()
+void GemOutput :: viewMess(t_float eyeX,    t_float eyeY,    t_float eyeZ, 
+			   t_float centerX, t_float centerY, t_float centerZ, 
+			   t_float upX,     t_float upY,     t_float upZ)
 {
-  post("gemoutput: create");
+  m_lookat[0]=eyeX;
+  m_lookat[1]=eyeY;
+  m_lookat[2]=eyeZ;
+
+  m_lookat[3]=centerX;
+  m_lookat[4]=centerY;
+  m_lookat[5]=centerZ;
+
+  m_lookat[6]=upX;
+  m_lookat[7]=upY;
+  m_lookat[8]=upZ;
 }
 /////////////////////////////////////////////////////////
-// destroyMess
+// perspectiveMess
 //
 /////////////////////////////////////////////////////////
-void GemOutput :: destroyMess()
+void GemOutput :: perspectiveMess(t_float left,   t_float right,
+				  t_float bottom, t_float top, 
+				  t_float front,  t_float back)
 {
+  m_perspect[0]=left;
+  m_perspect[1]=right;
+
+  m_perspect[2]=bottom;
+  m_perspect[3]=top;
+
+  m_perspect[4]=front;
+  m_perspect[5]=back;
 }
+
+/////////////////////////////////////////////////////////
+// colorMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: colorMess(t_float r, t_float g, t_float b, t_float a)
+{
+#ifdef __APPLE__
+  if (HaveValidContext ())
+#endif
+  glClearColor(r, g, b, a);
+  m_clear_color[0] = r;
+  m_clear_color[1] = g;
+  m_clear_color[2] = b; 
+  m_clear_color[3] = a; 
+
+}
+/////////////////////////////////////////////////////////
+// clearmaskMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: clearmaskMess(float bitmask)
+{
+  m_clear_mask = (GLbitfield) bitmask;
+}
+
+/////////////////////////////////////////////////////////
+// ambientMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: ambientMess(float r, float g, float b, t_float a)
+{
+  m_mat_ambient[0] = r;
+  m_mat_ambient[1] = g;
+  m_mat_ambient[2] = b;
+  m_mat_ambient[3] = a;
+
+}
+
+/////////////////////////////////////////////////////////
+// specularMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: specularMess(float r, float g, float b, t_float a)
+{
+  m_mat_specular[0] = r;
+  m_mat_specular[1] = g;
+  m_mat_specular[2] = b;
+  m_mat_specular[3] = a;
+
+}
+
+/////////////////////////////////////////////////////////
+// shininessMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: shininessMess(float val)
+{
+  m_mat_shininess = val;
+}
+
+/////////////////////////////////////////////////////////
+// fogDensityMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: fogDensityMess(float val)
+{
+  if (val < 0.f)
+    val = 0.f;
+
+  m_fog = val;
+}
+
+/////////////////////////////////////////////////////////
+// fogRangeMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: fogRangeMess(float start, float end)
+{
+  if (start < 0.f)
+    start= 0.f;
+
+  if (end < 0.f)
+    end  = 0.f;
+
+  m_fogStart = start;
+  m_fogEnd = end;
+}
+
+/////////////////////////////////////////////////////////
+// fogColorMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: fogColorMess(float red, float green, float blue, float alpha)
+{
+  m_fogColor[0] = red;
+  m_fogColor[1] = green;
+  m_fogColor[2] = blue;
+  m_fogColor[3] = alpha;
+}
+
+/////////////////////////////////////////////////////////
+// fogModeMess
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: fogModeMess(int mode)
+{
+  switch (mode)
+    {
+    case 0 :
+      m_fogMode = FOG_OFF;
+      break;
+    case 1 :
+      m_fogMode = FOG_LINEAR;
+      break;
+    case 2 :
+      m_fogMode = FOG_EXP;
+      break;
+    case 3 :
+      m_fogMode = FOG_EXP2;
+      break;
+    default :
+      error("GEM : fogmode must be 0, 1, 2 or 3");
+      break;
+    }
+}
+
+
+
+
+/////////////////////////////////////////////////////////
+// reset openGL state
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: resetState()
+{
+  int s_lightState=0; // LATER: remove this
+  if (s_lightState)
+    {
+      glEnable(GL_LIGHTING);
+      glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+      glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+      glEnable(GL_COLOR_MATERIAL);
+      glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, m_mat_ambient);
+      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, m_mat_specular);
+      glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, &m_mat_shininess);
+      glEnable(GL_AUTO_NORMAL);
+      glEnable(GL_NORMALIZE);
+      glShadeModel(GL_SMOOTH);
+    }
+  else
+    {
+      glDisable(GL_LIGHTING);
+      glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+      glDisable(GL_COLOR_MATERIAL);
+      glDisable(GL_AUTO_NORMAL);
+      glDisable(GL_NORMALIZE);
+      glShadeModel(GL_FLAT);
+    }
+
+  // setup the transformation matrices
+  float xDivy = (float)m_width / (float)m_height;
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glFrustum(m_perspect[0] * xDivy, m_perspect[1] * xDivy,	// left, right
+	    m_perspect[2], m_perspect[3],				// bottom, top
+	    m_perspect[4], m_perspect[5]);				// front, back
+    
+  glMatrixMode(GL_MODELVIEW);
+  gluLookAt(m_lookat[0], m_lookat[1], m_lookat[2], m_lookat[3], m_lookat[4],
+	    m_lookat[5], m_lookat[6], m_lookat[7], m_lookat[8]);
+
+  if (m_fogMode == FOG_OFF) {
+    glDisable(GL_FOG);
+  } else {
+    glEnable(GL_FOG);
+    switch (m_fogMode)  {
+    case (FOG_LINEAR):
+      glFogf(GL_FOG_MODE, GL_LINEAR);
+      break;
+    case (FOG_EXP):
+      glFogf(GL_FOG_MODE, GL_EXP);
+      break;
+    case (FOG_EXP2):
+      glFogf(GL_FOG_MODE, GL_EXP2);
+      break;
+    case (FOG_OFF):
+        glDisable(GL_FOG);
+        break;
+    }
+    glFogf(GL_FOG_DENSITY, m_fog);
+    glFogf(GL_FOG_START, m_fogStart);
+    glFogf(GL_FOG_END, m_fogEnd);
+    glFogfv(GL_FOG_COLOR, m_fogColor);
+  }
+
+  glClearColor(m_clear_color[0], m_clear_color[1], m_clear_color[2], m_clear_color[3]);
+}
+
+/////////////////////////////////////////////////////////
+// resetValues
+//
+/////////////////////////////////////////////////////////
+void GemOutput :: resetValues()
+{
+#ifdef DEBUG
+  post("GemOutput::resetState entered");
+#endif
+  colorMess(0.0, 0.0, 0.0, 0.0);
+  m_clear_mask = GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT;
+
+  ambientMess(0.1f, 0.1f, 0.1f, 1.0f);
+  specularMess(1.0, 1.0, 1.0, 1.0);
+  shininessMess(100.0);
+
+  //s_lightState = 0;
+
+  m_buffer = 2;
+
+  m_stereo=0;
+  m_stereoSep = -15.f;
+  m_stereoFocal = 0.f;
+  m_stereoLine = true;
+
+  // setup the perspective values
+  perspectiveMess(-1.f, 1.f, -1.f, 1.f, 1.f, 20.f);
+  // setup the lookat values
+  viewMess(0.f, 0.f, 4.f, 0.f, 0.f, 0.f,  0.f, 1.f, 0.f);
+
+  // setup the fog
+  m_fogMode	= FOG_OFF;
+  m_fogStart	= 1.f;
+  m_fogEnd	= 20.f;
+  m_fog		= 0.5f;
+  fogColorMess(1.f, 1.f, 1.f, 1.f);
+}
+
+
+
+
+
 
 /////////////////////////////////////////////////////////
 // static member function
@@ -75,23 +376,203 @@ void GemOutput :: destroyMess()
 void GemOutput :: obj_setupCallback(t_class *classPtr)
 {
   class_addmethod(classPtr, (t_method)&GemOutput::renderMessCallback,
-		  gensym("gem_render"), A_POINTER, A_NULL);
-  /*
-  class_addmethod(classPtr, (t_method)&GemOutput::createMessCallback,
-		  gensym("create"), A_NULL);
-  class_addmethod(classPtr, (t_method)&GemOutput::destroyMessCallback,
-		  gensym("destroy"), A_NULL);
-  */
+		  gensym("gem_render"), A_NULL);
+
+  class_addmethod(classPtr, (t_method)&GemOutput::perspectiveMessCallback,
+		  gensym("perspec"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::viewMessCallback,
+		  gensym("view"), A_GIMME, A_NULL);
+
+
+  class_addmethod(classPtr, (t_method)&GemOutput::colorMessCallback,
+		  gensym("color"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::clearmaskMessCallback,
+		  gensym("clearmask"), A_FLOAT, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::ambientMessCallback,
+		  gensym("ambient"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::specularMessCallback,
+		  gensym("specular"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::shininessMessCallback,
+		  gensym("shininess"), A_FLOAT, A_NULL);
+
+  class_addmethod(classPtr, (t_method)&GemOutput::fogMessCallback,
+		  gensym("fog"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::fogColorMessCallback,
+		  gensym("fogcolor"), A_GIMME, A_NULL);
+  class_addmethod(classPtr, (t_method)&GemOutput::fogModeMessCallback,
+		  gensym("fogmode"), A_FLOAT, A_NULL);
+
 }
-void GemOutput :: renderMessCallback(void *data, t_gpointer*cb)
+void GemOutput :: renderMessCallback(void *data)
 {
-  GetMyClass(data)->renderMess((gem_control)cb);
+  GetMyClass(data)->renderMess();
 }
-void GemOutput :: createMessCallback(void *data)
-{
-  GetMyClass(data)->createMess();
+
+void GemOutput :: colorMessCallback(void *data, t_symbol*, int argc, t_atom*argv){
+  GLfloat r, g, b, a;
+  a=0;
+  switch (argc){
+  case 1:
+    r=g=b=atom_getfloat(argv);
+    break;
+  case 4:
+    a=atom_getfloat(argv+3);
+  case 3:
+    b=atom_getfloat(argv+2);
+    g=atom_getfloat(argv+1);
+    r=atom_getfloat(argv+0);
+    break;
+  default:
+    error("GEM: GemOutput: color needs 1, 3 or 4 arguments");
+    return;
+  }
+  GetMyClass(data)->colorMess(r, g, b, a);
 }
-void GemOutput :: destroyMessCallback(void *data)
-{
-  GetMyClass(data)->destroyMess();
+void GemOutput :: clearmaskMessCallback(void *data, t_floatarg bitmask){
+  GetMyClass(data)->clearmaskMess((float)bitmask);
 }
+void GemOutput :: ambientMessCallback(void *data,t_symbol*, int argc, t_atom*argv){
+  GLfloat r, g, b, a;
+  a=1;
+  switch (argc){
+  case 1:
+    r=g=b=atom_getfloat(argv);
+    break;
+  case 4:
+    a=atom_getfloat(argv+3);
+  case 3:
+    b=atom_getfloat(argv+2);
+    g=atom_getfloat(argv+1);
+    r=atom_getfloat(argv+0);
+    break;
+  default:
+    error("GEM: GemOutput: color needs 1, 3 or 4 arguments");
+    return;
+  }
+  GetMyClass(data)->ambientMess(r, g, b, a);
+}
+void GemOutput :: specularMessCallback(void *data,t_symbol*, int argc, t_atom*argv){
+  GLfloat r, g, b, a;
+  a=1;
+  switch (argc){
+  case 1:
+    r=g=b=atom_getfloat(argv);
+    break;
+  case 4:
+    a=atom_getfloat(argv+3);
+  case 3:
+    b=atom_getfloat(argv+2);
+    g=atom_getfloat(argv+1);
+    r=atom_getfloat(argv+0);
+    break;
+  default:
+    error("GEM: GemOutput: color needs 1, 3 or 4 arguments");
+    return;
+  }
+  GetMyClass(data)->specularMess(r, g, b, a);
+}
+void GemOutput :: shininessMessCallback(void *data, t_floatarg val){
+  GetMyClass(data)->shininessMess((float)val);
+}
+void GemOutput :: fogMessCallback(void *data, t_symbol *, int argc, t_atom *argv){
+  switch (argc)
+    {
+    case (1):
+      GetMyClass(data)->fogDensityMess(atom_getfloat(&argv[0]));
+      break;
+    case (2):
+      GetMyClass(data)->fogRangeMess(atom_getfloat(&argv[0]),atom_getfloat(&argv[1]));
+      break;
+    default:
+      error("GEM : fog message needs 1 or 2 arguments");
+    }
+}
+void GemOutput :: fogColorMessCallback(void *data, t_symbol*, int argc, t_atom*argv){
+  // LATER: add fog
+  GLfloat r, g, b, a;
+  a=0;
+  switch (argc){
+  case 1:
+    r=g=b=atom_getfloat(argv);
+    break;
+  case 4:
+    a=atom_getfloat(argv+3);
+  case 3:
+    b=atom_getfloat(argv+2);
+    g=atom_getfloat(argv+1);
+    r=atom_getfloat(argv+0);
+    break;
+  default:
+    error("GEM: GemOutput: color needs 1, 3 or 4 arguments");
+    return;
+  }
+  GetMyClass(data)->fogColorMess(r, g, b, a);
+}
+void GemOutput :: fogModeMessCallback(void *data, t_floatarg val){
+  GetMyClass(data)->fogModeMess((int)val);
+}
+
+void GemOutput :: perspectiveMessCallback(void *data, t_symbol *, int argc, t_atom *argv){
+  if (argc != 6)  {
+    error("GEM: perspec message needs 6 arguments: left, right ,bottom, top, front, back");
+    return;
+  }
+   GetMyClass(data)->perspectiveMess(atom_getfloat(&argv[0]),	// left
+				     atom_getfloat(&argv[1]),	// right
+				     atom_getfloat(&argv[2]),	// bottom
+				     atom_getfloat(&argv[3]),	// top
+				     atom_getfloat(&argv[4]),	// front
+				     atom_getfloat(&argv[5]));	// back
+}
+void GemOutput :: viewMessCallback(void *data, t_symbol *, int argc, t_atom *argv){
+  const float DEG2RAD = 0.01745329251994f;
+  float azimuth = 0.f;
+  float theta = 0.f;
+  
+  switch (argc)  {
+    // setting all lookat values directly
+  case 9 :
+    GetMyClass(data)->viewMess(atom_getfloat(argv+0),	// eyex
+			       atom_getfloat(argv+1),	// eyey
+			       atom_getfloat(argv+2),	// eyez
+			       atom_getfloat(argv+3),	// centerx
+			       atom_getfloat(argv+4),	// centery
+			       atom_getfloat(argv+5),	// centerz
+			       atom_getfloat(argv+6),	// upx
+			       atom_getfloat(argv+7),	// upy
+			       atom_getfloat(argv+8));  // upz
+    break;
+  case 5 :
+    theta	= (float)DEG2RAD * atom_getfloat(&argv[4]);
+  case 4 :
+    azimuth = (float)DEG2RAD * atom_getfloat(&argv[3]);
+  case 3 : {  // just have position
+    float distance = GetMyClass(data)->m_stereoFocal;
+    if (distance<=0.f)distance=1.0f;
+  
+    const float x = atom_getfloat(&argv[0]);
+    const float y = atom_getfloat(&argv[1]);
+    const float z = atom_getfloat(&argv[2]);
+    const float dx =  (float)(cos(theta) * sinf(azimuth));
+    const float dy =  (float)(sin(theta));
+    const float dz = -(float)(cos(theta) * cosf(azimuth));
+    GetMyClass(data)->viewMess(x,		// eyex
+			       y,		// eyey
+			       z,		// eyez
+			       x + dx * distance,	// centerx
+			       y + dy * distance,	// centery
+			       z + dz * distance,	// centery
+			       -dx*dy,			// upx
+			       dx*dx+dz*dz,		// upy
+			       -dy*dz);			// upz
+  }
+    break;
+
+  default:
+    error("GEM: view message needs 3, 4, 5 or 9 arguments");
+
+    // note :: LATER set the StereoView ...
+  }
+}
+
+
--- src/Base/GemOutput.h
+++ src/Base/GemOutput.h
@@ -17,7 +17,17 @@
 
 #include "Base/CPPExtern.h"
 
-#define gem_control void*
+#if defined _WINDOWS
+# include <windows.h>
+# include <GL/gl.h>
+#elif defined __APPLE__
+# include <OpenGL/gl.h>
+#elif defined __unix__
+# include <GL/gl.h>
+#endif // OS
+
+#include "GemState.h"
+
 /*-----------------------------------------------------------------
   -------------------------------------------------------------------
   CLASS
@@ -34,17 +44,16 @@
   a callback-pointer to the actual rendering code
 
   what is going on:
-    on "create" the output-device (e.g.: window) is created
-    on "destroy" the output-device (e.g.: window) is closed
     on "render" the following happens:
       1. preRender() is called, where the output-objects sets the display-context
       2. callback the rendering-function: doRender()
       3. postRender() for objects that want to grab the pixel-data and send it e.g. to a video-out
 
 
-  "render" - render to this window
-  "create" - create a graphics window
-  "destroy" - destroy the graphics window
+  "render" - render to this context
+  "view", "perspec" - set up the camera
+  "color", "fog", ... - set up the lighting/color/...
+
 
   -----------------------------------------------------------------*/
 class GEM_EXTERN GemOutput : public CPPExtern
@@ -63,23 +72,90 @@ class GEM_EXTERN GemOutput : public CPPExtern
   // Destructor
   virtual     	~GemOutput();
     	
-  void		renderMess(gem_control gc);
-  void          doRender  (gem_control gc);
-  virtual void  preRender (gem_control gc)=0;
-  virtual void  postRender(gem_control gc){};
+  void		renderMess();
+  void          doRender  (GemState currentState);
+  virtual void  preRender (GemState currentState)=0; // get the current context
+  virtual void  postRender(GemState currentState){};
   
-  virtual void 	createMess();
-  virtual void  destroyMess();
-
+  unsigned int m_width, m_height;
   bool m_outputState; // whether we can output (and thus want to render) or not
+  bool m_outputContext; // whether we have a valid rendering context
+
+  int m_buffer; // buffer mode (single or double)
+
+
+  /* the viewpoint */
+  float	m_perspect[6]; // values for the perspective matrix
+  void	perspectiveMess(t_float, t_float, t_float, t_float, t_float, t_float);
+
+  float	m_lookat[9];	  // values for the lookat matrix
+  void	viewMess(t_float, t_float, t_float, t_float, t_float, t_float, t_float, t_float, t_float);
+
+  /* the overall material */
+  GLfloat m_clear_color[4];	// the frame buffer clear
+  void 	  colorMess(t_float red, t_float green, t_float blue, t_float alpha);
+
+  GLbitfield m_clear_mask;	// the clear bitmask
+  void 	     clearmaskMess(t_float bitmask);
+
+  GLfloat m_mat_ambient[4];	// default ambient material
+  void 	  ambientMess(t_float red, t_float green, t_float blue, t_float alpha);
+
+  GLfloat m_mat_specular[4];	// default specular material
+  void 	  specularMess(t_float red, t_float green, t_float blue, t_float alpha);
+
+  GLfloat m_mat_shininess;	// default shininess material
+  void 	  shininessMess(t_float val);
+
+  /* fog */ 
+  void  fogDensityMess(t_float density);
+  void  fogRangeMess(t_float start, t_float stop);
+  void 	fogColorMess(t_float red, t_float green, t_float blue, t_float alpha);
+  void	fogModeMess(int val);
+
+  float	   m_fog;		// fog density
+  enum FOG_TYPE { FOG_OFF = 0, FOG_LINEAR, FOG_EXP, FOG_EXP2 };
+  FOG_TYPE m_fogMode;		// what kind of fog we have
+  GLfloat  m_fogColor[4];	// colour of the fog
+  float	   m_fogStart;		// start of the linear fog
+  float	   m_fogEnd;		// start of the linear fog
+
+
+  int               m_stereo;                   // stereo-mode
+  GLfloat	    m_stereoSep;		// stereo seperation
+  GLfloat	    m_stereoFocal;		// distance to focal point
+  bool	            m_stereoLine;		// draw a line between 2 stereo-screens
+
+
+  virtual void resetState();
+  void resetValues();
+
+
 
  private:
     
   //////////
   // Static member functions
-  static void 	renderMessCallback (void *data, t_gpointer*);
-  static void 	createMessCallback (void *data);
-  static void 	destroyMessCallback(void *data);
+  static void 	renderMessCallback (void *data);
+
+  //////////
+  // change the rendering somehow
+
+  /* camera settings */
+  static void	perspectiveMessCallback(void *, t_symbol *, int argc, t_atom *argv);
+  static void	viewMessCallback(void *, t_symbol *, int argc, t_atom *argv);
+
+  /* lights and colors */
+  static void 	colorMessCallback(void *data, t_symbol*, int, t_atom*);
+  static void 	clearmaskMessCallback(void *data, t_floatarg bitmask);
+  static void 	ambientMessCallback(void *data, t_symbol*, int, t_atom*);
+  static void 	specularMessCallback(void *data, t_symbol*, int, t_atom*);
+  static void 	shininessMessCallback(void *data, t_floatarg val);
+  static void	fogMessCallback(void *, t_symbol *, int argc, t_atom *argv);
+  static void 	fogColorMessCallback(void *, t_symbol*, int, t_atom*);
+  static void	fogModeMessCallback(void *, t_floatarg val);
+
+
 };
 
 #endif	// for header file
