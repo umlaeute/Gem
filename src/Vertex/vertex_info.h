60c6bd6c533d62de21c9c5a13a4ee768ed4cbbeb
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2004-08-26 12:21:50 +0000

    moved all the vertex_-objects from src/Geos to src/Vertex; added vertex_mul, vertex_add vertex_offset,... now work on all arrays
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1161 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Vertex/vertex_info.h
@@ -0,0 +1,62 @@
+/*-----------------------------------------------------------------
+LOG
+    GEM - Graphics Environment for Multimedia
+
+    A vertex_info
+
+    Copyright (c) 1997-2000 Mark Danks. mark@danks.org
+    Copyright (c) G¸nther Geiger. geiger@epy.co.at
+    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::f¸r::uml‰ute. IEM. zmoelnig@iem.kug.ac.at
+    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_vertex_info_H_
+#define INCLUDE_vertex_info_H_
+
+#include "Base/GemVertex.h"
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+CLASS
+    vertex_info
+    
+    Creates a vertex_info
+
+KEYWORDS
+    geo
+    
+DESCRIPTION
+    
+-----------------------------------------------------------------*/
+class GEM_EXTERN vertex_info : public GemBase
+{
+    CPPEXTERN_HEADER(vertex_info, GemBase)
+
+    public:
+
+        //////////
+        // Constructor
+    	vertex_info();
+    	
+    protected:
+    	
+    	//////////
+    	// Destructor
+    	virtual ~vertex_info();
+        
+        int 		m_previousSize;
+        int		m_vertNum,m_vertCount;
+        t_outlet	*m_Vsize;
+
+    	//////////
+    	// Do the rendering
+    	virtual void 	render(GemState *state);
+
+ private:
+        static void 	vertexMessCallback(void *data, t_floatarg num, t_floatarg counter);
+
+};
+
+#endif	// for header file
