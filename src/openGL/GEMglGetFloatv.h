7860e40b34f77289667e2d93e93780c2e3afad4c
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2004-09-15 15:06:39 +0000

    initial addition (thanks cyrille)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1188 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/openGL/GEMglGetFloatv.h
@@ -0,0 +1,56 @@
+ /* ------------------------------------------------------------------
+  * GEM - Graphics Environment for Multimedia
+  *
+  *  Copyright (c) 2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+  *	zmoelnig@iem.kug.ac.at
+  *  For information on usage and redistribution, and for a DISCLAIMER
+  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+  *
+  *  this file has been generated...
+  * ------------------------------------------------------------------
+  */
+
+#ifndef INCLUDE_GEM_GLGETPOINTERV_H_
+#define INCLUDE_GEM_GLGETPOINTERV_H_
+
+#include "Base/GemGLUtil.h"
+#include "Base/GemBase.h"
+
+/*
+ CLASS
+	GEMglGetFloatv
+ KEYWORDS
+	openGL	0
+ DESCRIPTION
+	wrapper for the openGL-function
+	"glGetFloatv( GLenum pname, GLfloat *params)"
+ */
+
+class GEM_EXTERN GEMglGetFloatv : public GemBase
+{
+	CPPEXTERN_HEADER(GEMglGetFloatv, GemBase)
+
+	public:
+	  // Constructor
+	  GEMglGetFloatv (t_floatarg);	// CON
+	protected:
+	  // Destructor
+	  virtual ~GEMglGetFloatv ();
+	  // Do the rendering
+	  virtual void	render (GemState *state);
+	// variable
+	  GLenum pname;
+	  virtual void	pnameMess(t_floatarg);	// FUN
+
+	  t_atom m_alist[16];
+
+	private:
+	// we need one inlet
+	  t_inlet *m_inlet;
+    // The outlet
+      t_outlet    	*m_outlet;
+
+	// static member functions
+         static void    pnameMessCallback (void*,t_floatarg);
+};
+#endif // for header file
