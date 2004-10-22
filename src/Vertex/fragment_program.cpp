a26fc8317df8aa9f840dc0acd925c77bd930a56f
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2004-10-22 13:47:06 +0000

    *** empty log message ***
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/branches/vertex_array/Gem@1203 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Vertex/fragment_program.cpp
@@ -0,0 +1,273 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// Created by tigital on 10/16/04.
+// Copyright 2004 James Tittle
+//
+// Implementation file
+//
+//    Copyright (c) 1997-1999 Mark Danks.
+//    Copyright (c) G¬∏nther Geiger.
+//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::f¬∏r::uml‚Ä∞ute. IEM
+//    Copyright (c) 2002 James Tittle & Chris Clepper
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+
+#include "fragmentProgram.h"
+#include "Base/GemState.h"
+#include "string.h"
+#ifdef __APPLE__
+#include <AGL/agl.h>
+extern bool HaveValidContext (void);
+#endif
+
+CPPEXTERN_NEW_WITH_ONE_ARG(fragment_program, t_symbol *, A_DEFSYM)
+
+/////////////////////////////////////////////////////////
+//
+// fragment_program
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+fragment_program :: fragment_program(t_symbol *filename) :
+	m_programTarget (0), m_programID(0)
+{
+ // make sure that there are some characters
+ x_filename=gensym("");
+}
+
+/////////////////////////////////////////////////////////
+// Destructor
+//
+/////////////////////////////////////////////////////////
+fragment_program :: ~fragment_program()
+{
+#ifdef GL_FRAGMENT_PROGRAM_NV
+	if(m_programTarget == GL_FRAGMENT_PROGRAM_NV) glDeleteProgramsNV(1,&m_programID);
+	else 
+#endif
+	if(m_programID)
+		glDeleteProgramsARB(1,&m_programID);
+	m_programID=0;
+	m_programTarget=0;
+}
+
+
+/////////////////////////////////////////////////////////
+// openMess
+//
+/////////////////////////////////////////////////////////
+
+void fragment_program :: openMess(t_symbol *filename)
+{
+#ifdef __APPLE__
+  if (!HaveValidContext ()) {
+    post("GEM: geo: model - need window to load model");
+    return;
+  }
+#endif
+  //  if (filename==x_filename)return;
+  x_filename=filename;
+
+  char buf[MAXPDSTRING];
+  canvas_makefilename(getCanvas(), filename->s_name, buf, MAXPDSTRING);
+
+  // Clean up any open files
+  closeMess();
+
+	//char *data;
+	FILE *file = fopen(buf,"r");
+	if(file) {
+		fseek(file,0,SEEK_END);
+		m_size = ftell(file);
+		m_programString = new char[m_size + 1];
+		memset(m_programString,0,m_size + 1);
+		fseek(file,0,SEEK_SET);
+		fread(m_programString,1,m_size,file);
+		fclose(file);
+	} else {
+		m_programString = new char[strlen(buf) + 1];
+		strcpy(m_programString,buf);
+	}
+	
+	GLint error = -1;
+/*	if(!strncmp(m_programString,"!!ARBvp1.0",10)) {
+		m_programTarget = GL_VERTEX_PROGRAM_ARB;
+		glGenProgramsARB(1, &m_programID);
+		glBindProgramARB(m_programTarget, m_programID);
+		glProgramStringARB(m_programTarget, GL_PROGRAM_FORMAT_ASCII_ARB, strlen(m_programString), m_programString);
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &error);
+	} else */
+	if(!strncmp(m_programString,"!!ARBfp1.0",10)) {
+		m_programTarget = GL_FRAGMENT_PROGRAM_ARB;
+		glGenProgramsARB(1, &m_programID);
+		glBindProgramARB(m_programTarget, m_programID);
+		glProgramStringARB(m_programTarget, GL_PROGRAM_FORMAT_ASCII_ARB, strlen(m_programString), m_programString);
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &error);
+#ifdef GL_FRAGMENT_PROGRAM_NV
+	} else if(!strncmp(data,"!!FP1.0",7)) {
+		m_programTarget = GL_FRAGMENT_PROGRAM_NV;
+		glGenProgramsNV(1, &m_programID);
+		glBindProgramNV(m_programTarget, m_programID);
+		glLoadProgramNV(m_programTarget, m_programID, strlen(data), (GLubyte*)data);
+		glGetIntegerv(GL_PROGRAM_ERROR_POSITION_NV, &error);
+#endif
+	} else {
+		m_programTarget = 0;
+		m_programID = 0;
+		char *s = m_programString;
+		while(*s && *s != '\n') s++;
+		*s = '\0';
+		post("fragment_program:  unknown program header \"%s\" or error open \"%s\" file\n",m_programString,x_filename);
+		post("fragment_program:  %s\n", glGetString(GL_PROGRAM_ERROR_STRING_ARB));
+		delete m_programString;
+		return;
+	}
+
+	if(error != -1) {
+		int line = 0;
+		char *s = m_programString;
+		while(error-- && *s) if(*s++ == '\n') line++;
+		while(s >= m_programString && *s != '\n') s--;
+		char *e = ++s;
+		while(*e != '\n' && *e != '\0') e++;
+		*e = '\0';
+		post("fragment_program:  program error at line %d:\n\"%s\"\n",line,s);
+		post("fragment_program:  %s\n", glGetString(GL_PROGRAM_ERROR_STRING_ARB));
+	}
+
+	delete m_programString;
+	post("GEM: fragment_program: Loaded file: %s\n", buf);
+	GLint bitnum;
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_UNDER_NATIVE_LIMITS_ARB, &bitnum);
+	if (!bitnum)
+		post("GEM: fragment_program not within native limits!!!");
+}
+
+/////////////////////////////////////////////////////////
+// render
+//
+/////////////////////////////////////////////////////////
+void fragment_program :: startRendering()
+{
+}
+
+void fragment_program :: render(GemState *state)
+{
+	if (m_programString == NULL)
+	{
+		post("need to load a fragment program");
+		return;
+	}
+	if (m_programID==0)
+	{
+		glEnable(GL_FRAGMENT_PROGRAM_ARB);
+		glGenProgramsARB(1, &m_programID);
+		glBindProgramARB( GL_FRAGMENT_PROGRAM_ARB, m_programID);
+		glProgramStringARB( GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB, strlen(m_programString), m_programString);
+	}
+
+	glEnable( GL_FRAGMENT_PROGRAM_ARB);
+	glBindProgramARB( GL_FRAGMENT_PROGRAM_ARB, m_programID);
+}
+
+/////////////////////////////////////////////////////////
+// postrender
+//
+/////////////////////////////////////////////////////////
+void fragment_program :: postrender(GemState *state)
+{
+	glDisable( GL_FRAGMENT_PROGRAM_ARB );
+}
+
+/////////////////////////////////////////////////////////
+// printInfo
+//
+/////////////////////////////////////////////////////////
+void fragment_program :: printInfo()
+{
+#ifdef __APPLE__
+	if (!HaveValidContext ()) {
+		post("GEM: fragment_program - need window/context to load program");
+		return;
+	}
+#endif
+	GLint bitnum = 0;
+	post("fragment_Program Hardware Info");
+	post("============================");
+	
+	glGetIntegerv( GL_MAX_PROGRAM_MATRICES_ARB, &bitnum );
+	post("MAX_PROGRAM_MATRICES: %d", bitnum);
+	glGetIntegerv( GL_MAX_PROGRAM_MATRIX_STACK_DEPTH_ARB, &bitnum );
+	post("MAX_PROGRAM_MATRIX_STACK_DEPTH: %d", bitnum);
+	glGetIntegerv( GL_MAX_TEXTURE_COORDS_ARB, &bitnum );
+	post("MAX_TEXTURE_COORDS: %d", bitnum);
+	glGetIntegerv( GL_MAX_TEXTURE_IMAGE_UNITS_ARB, &bitnum );
+	post("MAX_TEXTURE_IMAGE_UNITS: %d", bitnum);
+	
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_TEMPORARIES_ARB, &bitnum);
+	post("MAX_PROGRAM_TEMPORARIES: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_TEMPORARIES_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_TEMPORARIES: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_PARAMETERS_ARB, &bitnum);
+	post("MAX_PROGRAM_PARAMETERS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_PARAMETERS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_PARAMETERS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_ATTRIBS_ARB, &bitnum);
+	post("MAX_PROGRAM_ATTRIBS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_ATTRIBS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_ATTRIBS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_ADDRESS_REGISTERS_ARB, &bitnum);
+	post("MAX_PROGRAM_ADDRESS_REGISTERS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_ADDRESS_REGISTERS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_LOCAL_PARAMETERS_ARB, &bitnum);
+	post("MAX_PROGRAM_LOCAL_PARAMETERS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_ENV_PARAMETERS_ARB, &bitnum);
+	post("MAX_PROGRAM_ENV_PARAMETERS: %d", bitnum);
+	post("");
+	
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_ALU_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_ALU_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_TEX_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_TEX_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_TEX_INDIRECTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_TEX_INDIRECTIONS: %d", bitnum);
+	
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_ALU_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_ALU_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_TEX_INSTRUCTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_TEX_INSTRUCTIONS: %d", bitnum);
+	glGetProgramivARB( GL_FRAGMENT_PROGRAM_ARB, GL_MAX_PROGRAM_NATIVE_TEX_INDIRECTIONS_ARB, &bitnum);
+	post("MAX_PROGRAM_NATIVE_TEX_INDIRECTIONS: %d", bitnum);
+	post("");
+}
+
+/////////////////////////////////////////////////////////
+// static member function
+//
+/////////////////////////////////////////////////////////
+void fragment_program :: obj_setupCallback(t_class *classPtr)
+{
+  class_addmethod(classPtr, (t_method)&fragment_program::openMessCallback,
+		  gensym("open"), A_SYMBOL, A_NULL);
+  class_addmethod(classPtr, (t_method)&fragment_program::printMessCallback,
+		  gensym("print"), A_NULL);
+}
+void fragment_program :: openMessCallback(void *data, t_symbol *filename)
+{
+	    GetMyClass(data)->openMess(filename);
+}
+void fragment_program :: printMessCallback(void *data)
+{
+	GetMyClass(data)->printInfo();
+}
---
+++ src/Vertex/vertex_program.h
@@ -0,0 +1,113 @@
+/*-----------------------------------------------------------------
+
+GEM - Graphics Environment for Multimedia
+
+Load an ARB vertex program/shader
+ 
+ *  Created by tigital on 10/16/04.
+ *  Copyright 2004 tigital.
+ 
+Copyright (c) 1997-1999 Mark Danks. mark@danks.org
+Copyright (c) G¸nther Geiger. geiger@epy.co.at
+Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::f¸r::uml‰ute. IEM. zmoelnig@iem.kug.ac.at
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_VERTEX_PROGRAM_H_
+#define INCLUDE_VERTEX_PROGRAM_H_
+
+#include <string.h>
+#include <stdio.h>
+
+#include "Base/config.h"
+
+#include "Base/GemBase.h"
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  vertex_program
+    
+  Loads in an ARB vertex program/shader
+    
+  KEYWORDS
+  
+    
+  DESCRIPTION
+
+  -----------------------------------------------------------------*/
+class GEM_EXTERN vertex_program : public GemBase
+{
+  CPPEXTERN_HEADER(vertex_program, GemBase)
+    
+    public:
+  
+  //////////
+  // Constructor
+  vertex_program(t_symbol *filename);
+  //  pix_film();
+
+ protected:
+    
+  //////////
+  // Destructor
+  virtual ~vertex_program();
+
+  //////////
+  // close the program file
+  virtual void closeMess(void){}
+  //////////
+  // open a program up
+  virtual void openMess(t_symbol *filename);
+
+  //////////
+  // prepare for texturing (on open)
+  virtual void prepareTexture() {}
+
+  //////////
+  // Do the rendering
+  virtual void render(GemState *state);
+
+
+  //////////
+  // Clear the dirty flag on the pixBlock
+  virtual void postrender(GemState *state);
+
+  //////////
+  virtual void startRendering();
+
+  //////////
+  // Delete texture object
+  virtual void stopRendering() {}
+  
+  //////////
+  // Print Info about Hardware limits
+  virtual void printInfo();
+
+  //////////
+  // the current file
+  t_symbol *x_filename;
+	
+  //////////
+  //
+  GLuint 	    m_programTarget;
+  GLuint        m_programID;
+  char*			m_programString;
+  int			m_size;
+
+  //////////
+  // a outlet for information like #frames and "reached end"
+  t_outlet     *m_outNumFrames;
+  t_outlet     *m_outEnd;
+
+ protected:
+	
+  //////////
+  // static member functions
+  static void openMessCallback   (void *data, t_symbol *filename);
+  static void printMessCallback  (void *);
+};
+
+#endif	// for header file
