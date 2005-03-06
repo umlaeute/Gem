b7b8adad3af4a27e99803797df48e713480ef76e
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2005-03-06 11:21:50 +0000

    removed [change] (is in pd for some times)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1282 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/MarkEx/m_control.cpp
+++ src/MarkEx/m_control.cpp
@@ -43,43 +43,6 @@ void invert_setup(void)
     class_addfloat(invert_class, (t_method)invert_float);
 }
 
-/* -------------------------- change ------------------------------ */
-
-/* instance structure */
-
-static t_class *change_class;
-
-typedef struct _change
-{
-	t_object    x_obj;			/* obligatory object header */
-	float		x_cur;
-	t_outlet    *t_out1;	    /* the outlet */
-} t_change;
-
-static void change_float(t_change *x, t_floatarg n)
-{
-    if (n != x->x_cur)
-	{
-		outlet_float(x->t_out1, n);
-		x->x_cur = n;
-    }
-}
-
-static void *change_new() /* init vals in struc */
-{
-    t_change *x = (t_change *)pd_new(change_class);
-    x->x_cur = -1.f;
-	x->t_out1 = outlet_new(&x->x_obj, 0);
-    return(x);
-}
-
-void change_setup(void)
-{
-    change_class = class_new(gensym("change"), (t_newmethod)change_new, 0,
-    	    	    	sizeof(t_change), 0, A_NULL);
-    class_addfloat(change_class, change_float);
-}
-
 /* -------------------------- alternate ------------------------------ */
 
 /* instance structure */
