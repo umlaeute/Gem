146927c4b45b853fd663126f64ca5254fc7c8644
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2005-03-22 02:48:51 +0000

    more OpenGL wrappers I've had sitting on my drive
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1287 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/openGL/GEMglMultiTexCoord2fARB.h
@@ -0,0 +1,62 @@
+ /* ------------------------------------------------------------------
+  * GEM - Graphics Environment for Multimedia
+  *
+  *  Copyright (c) 2004-2005 tigital@mac.com
+  *  For information on usage and redistribution, and for a DISCLAIMER
+  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+  *
+  * ------------------------------------------------------------------
+  */
+
+#ifndef INCLUDE_GEM_GLMULTITEXCOORD2FARB_H_
+#define INCLUDE_GEM_GLMULTITEXCOORD2FARB_H_
+
+#include "Base/GemGLUtil.h"
+#include "Base/GemBase.h"
+
+/*
+ CLASS
+	GEMglMultiTexCoord2fARB
+ KEYWORDS
+	openGL	0
+ DESCRIPTION
+	wrapper for the openGL-function
+	"glMultiTexCoord2fARB( GLenum texUnit, GLfloat s, GLfloat t )"
+ */
+
+class GEM_EXTERN GEMglMultiTexCoord2fARB : public GemBase
+{
+	CPPEXTERN_HEADER(GEMglMultiTexCoord2fARB, GemBase)
+
+	public:
+	  // Constructor
+	  GEMglMultiTexCoord2fARB (t_float, t_float, t_float);	// CON
+
+	protected:
+	  // Destructor
+	  virtual ~GEMglMultiTexCoord2fARB ();
+	  // Do the rendering
+	  virtual void	render (GemState *state);
+
+	// variables
+	  GLenum	texUnit;		// VAR
+	  virtual void	texUnitMess(t_float);	// FUN
+
+	  GLfloat	s;		// VAR
+	  virtual void	sMess(t_float);	// FUN
+
+	  GLfloat	t;		// VAR
+	  virtual void	tMess(t_float);	// FUN
+
+
+	private:
+
+	// we need some inlets
+	  t_inlet *m_inlet[3];
+
+	// static member functions
+	  static void	 texUnitMessCallback (void*, t_floatarg);
+	  static void	 sMessCallback (void*, t_floatarg);
+	  static void	 tMessCallback (void*, t_floatarg);
+};
+#endif // for header file
---
+++ src/openGL/GEMgluPerspective.cpp
@@ -0,0 +1,98 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// Implementation file
+//
+// Copyright (c) 2005 tigital@mac.com
+//  For information on usage and redistribution, and for a DISCLAIMER
+//  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+//
+////////////////////////////////////////////////////////
+
+#include "GEMgluPerspective.h"
+
+CPPEXTERN_NEW_WITH_FOUR_ARGS ( GEMgluPerspective , t_floatarg, A_DEFFLOAT, t_floatarg, A_DEFFLOAT, t_floatarg, A_DEFFLOAT, t_floatarg, A_DEFFLOAT)
+
+/////////////////////////////////////////////////////////
+//
+// GEMgluPerspective
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+GEMgluPerspective :: GEMgluPerspective	(t_floatarg arg0=0, t_floatarg arg1=0, t_floatarg arg2=0, t_floatarg arg3=0) :
+		fovy((GLdouble)arg0), 
+		aspect((GLdouble)arg1), 
+		near((GLdouble)arg2), 
+		far((GLdouble)arg3)
+{
+	m_inlet[0] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("fovy"));
+	m_inlet[1] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("aspect"));
+	m_inlet[2] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("near"));
+	m_inlet[3] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("far"));
+}
+/////////////////////////////////////////////////////////
+// Destructor
+//
+GEMgluPerspective :: ~GEMgluPerspective () {
+inlet_free(m_inlet[0]);
+inlet_free(m_inlet[1]);
+inlet_free(m_inlet[2]);
+inlet_free(m_inlet[3]);
+}
+
+/////////////////////////////////////////////////////////
+// Render
+//
+void GEMgluPerspective :: render(GemState *state) {
+	gluPerspective (fovy, aspect, near, far);
+}
+
+/////////////////////////////////////////////////////////
+// Variables
+//
+void GEMgluPerspective :: fovyMess (t_float arg1) {	// FUN
+	fovy = (GLdouble)arg1;
+	setModified();
+}
+
+void GEMgluPerspective :: aspectMess (t_float arg1) {	// FUN
+	aspect = (GLdouble)arg1;
+	setModified();
+}
+
+void GEMgluPerspective :: nearMess (t_float arg1) {	// FUN
+	near = (GLdouble)arg1;
+	setModified();
+}
+
+void GEMgluPerspective :: farMess (t_float arg1) {	// FUN
+	far = (GLdouble)arg1;
+	setModified();
+}
+
+
+/////////////////////////////////////////////////////////
+// static member functions
+//
+
+void GEMgluPerspective :: obj_setupCallback(t_class *classPtr) {
+	 class_addmethod(classPtr, (t_method)&GEMgluPerspective::fovyMessCallback,  	gensym("fovy"), A_DEFFLOAT, A_NULL);
+	 class_addmethod(classPtr, (t_method)&GEMgluPerspective::aspectMessCallback,  	gensym("aspect"), A_DEFFLOAT, A_NULL);
+	 class_addmethod(classPtr, (t_method)&GEMgluPerspective::nearMessCallback,  	gensym("near"), A_DEFFLOAT, A_NULL);
+	 class_addmethod(classPtr, (t_method)&GEMgluPerspective::farMessCallback,  	gensym("far"), A_DEFFLOAT, A_NULL);
+};
+
+void GEMgluPerspective :: fovyMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->fovyMess ( (t_float)    arg0);
+}
+void GEMgluPerspective :: aspectMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->aspectMess ( (t_float)    arg0);
+}
+void GEMgluPerspective :: nearMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->nearMess ( (t_float)    arg0);
+}
+void GEMgluPerspective :: farMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->farMess ( (t_float)    arg0);
+}
