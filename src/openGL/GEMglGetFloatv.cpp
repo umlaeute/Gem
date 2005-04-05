54986844aee5ee3101f91e6cec8832d994a4f99d
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2005-04-05 10:52:23 +0000

    added wrapper for glEndList() and glNewList(): this is cool as it allows to makeuser defined display-lists (performance boost)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1306 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/openGL/GEMglGetFloatv.cpp
+++ src/openGL/GEMglGetFloatv.cpp
@@ -25,20 +25,20 @@ CPPEXTERN_NEW_WITH_ONE_ARG ( GEMglGetFloatv , t_floatarg, A_DEFFLOAT )
 //
 GEMglGetFloatv :: GEMglGetFloatv	(t_floatarg arg0=0) {
 #ifndef GL_VERSION_1_1
-        error("GEMglGetFloatv: GEM was compiled without GL_VERSION_1_1");
-        error("GEMglGetFloatv: therefore this object will do nothing");
+  error("GEMglGetFloatv: GEM was compiled without GL_VERSION_1_1");
+  error("GEMglGetFloatv: therefore this object will do nothing");
 #endif
-	pnameMess(arg0);
-	m_inlet = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("pname"));
-    m_outlet = outlet_new(this->x_obj, 0);
+  pnameMess(arg0);
+  m_inlet = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("pname"));
+  m_outlet = outlet_new(this->x_obj, 0);
 
 }
 /////////////////////////////////////////////////////////
 // Destructor
 //
 GEMglGetFloatv :: ~GEMglGetFloatv () {
-	inlet_free(m_inlet);
-	outlet_free(m_outlet);
+  inlet_free(m_inlet);
+  outlet_free(m_outlet);
 }
 
 /////////////////////////////////////////////////////////
@@ -46,28 +46,28 @@ GEMglGetFloatv :: ~GEMglGetFloatv () {
 //
 void GEMglGetFloatv :: render(GemState *state) {
 #ifdef GL_VERSION_1_1
-	  float mi[16]={0};
+  float mi[16]={0};
 
-	glGetFloatv(pname,mi);
+  glGetFloatv(pname,mi);
 
-	SETFLOAT(m_alist+0, mi[0]);
-    SETFLOAT(m_alist+1, mi[1]);
-    SETFLOAT(m_alist+2, mi[2]);
-    SETFLOAT(m_alist+3, mi[3]);
-    SETFLOAT(m_alist+4, mi[4]);
-    SETFLOAT(m_alist+5, mi[5]);
-    SETFLOAT(m_alist+6, mi[6]);
-    SETFLOAT(m_alist+7, mi[7]);
-    SETFLOAT(m_alist+8, mi[8]);
-    SETFLOAT(m_alist+9, mi[9]);
-    SETFLOAT(m_alist+10, mi[10]);
-    SETFLOAT(m_alist+11, mi[11]);
-    SETFLOAT(m_alist+12, mi[12]);
-    SETFLOAT(m_alist+13, mi[13]);
-    SETFLOAT(m_alist+14, mi[14]);
-    SETFLOAT(m_alist+15, mi[15]);
+  SETFLOAT(m_alist+0, mi[0]);
+  SETFLOAT(m_alist+1, mi[1]);
+  SETFLOAT(m_alist+2, mi[2]);
+  SETFLOAT(m_alist+3, mi[3]);
+  SETFLOAT(m_alist+4, mi[4]);
+  SETFLOAT(m_alist+5, mi[5]);
+  SETFLOAT(m_alist+6, mi[6]);
+  SETFLOAT(m_alist+7, mi[7]);
+  SETFLOAT(m_alist+8, mi[8]);
+  SETFLOAT(m_alist+9, mi[9]);
+  SETFLOAT(m_alist+10, mi[10]);
+  SETFLOAT(m_alist+11, mi[11]);
+  SETFLOAT(m_alist+12, mi[12]);
+  SETFLOAT(m_alist+13, mi[13]);
+  SETFLOAT(m_alist+14, mi[14]);
+  SETFLOAT(m_alist+15, mi[15]);
 
-	outlet_list(m_outlet, gensym("list"), 16, m_alist);
+  outlet_list(m_outlet, gensym("list"), 16, m_alist);
 #endif // GL_VERSION_1_1
 }
 
@@ -88,5 +88,5 @@ void GEMglGetFloatv :: obj_setupCallback(t_class *classPtr) {
 }
 
 void GEMglGetFloatv :: pnameMessCallback (void* data, t_floatarg arg0) {
-	GetMyClass(data)->pnameMess ( arg0 );
+  GetMyClass(data)->pnameMess ( arg0 );
 }
---
+++ src/openGL/GEMglNewList.cpp
@@ -0,0 +1,76 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// Implementation file
+//
+// Copyright (c) 2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+//	zmoelnig@iem.kug.ac.at
+//  For information on usage and redistribution, and for a DISCLAIMER
+//  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+//
+//  this file has been generated...
+////////////////////////////////////////////////////////
+
+#include "GEMglNewList.h"
+
+CPPEXTERN_NEW_WITH_TWO_ARGS ( GEMglNewList , t_floatarg, A_DEFFLOAT, t_floatarg, A_DEFFLOAT)
+
+/////////////////////////////////////////////////////////
+//
+// GEMglViewport
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+GEMglNewList :: GEMglNewList	(t_floatarg arg0=0, t_floatarg arg1=GL_COMPILE_AND_EXECUTE) :
+		mode((GLenum)arg1), list((GLuint)arg0)
+{
+	m_inlet[0] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("displaylist"));
+	m_inlet[1] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("mode"));
+}
+/////////////////////////////////////////////////////////
+// Destructor
+//
+GEMglNewList :: ~GEMglNewList () {
+inlet_free(m_inlet[0]);
+inlet_free(m_inlet[1]);
+}
+
+/////////////////////////////////////////////////////////
+// Render
+//
+void GEMglNewList :: render(GemState *state) {
+	glNewList (list, mode);
+}
+
+/////////////////////////////////////////////////////////
+// Variables
+//
+void GEMglNewList :: modeMess (t_float arg1) {	// FUN
+	mode = (GLenum)arg1;
+	setModified();
+	post("mode=%d", mode);
+}
+void GEMglNewList :: listMess (t_float arg1) {	// FUN
+	list = (GLuint)arg1;
+	setModified();
+	post("list=%d", list);
+}
+
+
+/////////////////////////////////////////////////////////
+// static member functions
+//
+
+void GEMglNewList :: obj_setupCallback(t_class *classPtr) {
+	 class_addmethod(classPtr, (t_method)&GEMglNewList::listMessCallback,  	gensym("displaylist"), A_DEFFLOAT, A_NULL);
+	 class_addmethod(classPtr, (t_method)&GEMglNewList::modeMessCallback,  	gensym("mode"), A_DEFFLOAT, A_NULL);
+};
+
+void GEMglNewList :: modeMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->modeMess ( (t_float)    arg0);
+}
+void GEMglNewList :: listMessCallback (void* data, t_floatarg arg0){
+	GetMyClass(data)->listMess ( (t_float)    arg0);
+}
