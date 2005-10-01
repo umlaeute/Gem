4df571e234df9cc4edde3dd7f58b8c0f02696fac
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2005-10-01 19:14:36 +0000

    moved the openGL-magic into Base/GemGL.h
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1526 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Manips/fragment_program.h
+++ src/Manips/fragment_program.h
@@ -20,27 +20,6 @@ WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
 #include "Manips/vertex_program.h"
 
-#ifndef DONT_USE_ARB
-# ifndef GL_ARB_fragment_program
-#  define GL_ARB_fragment_program
-#  define GL_FRAGMENT_PROGRAM_ARB           0x8804
-#  define GL_PROGRAM_ALU_INSTRUCTIONS_ARB   0x8805
-#  define GL_PROGRAM_TEX_INSTRUCTIONS_ARB   0x8806
-#  define GL_PROGRAM_TEX_INDIRECTIONS_ARB   0x8807
-#  define GL_PROGRAM_NATIVE_ALU_INSTRUCTIONS_ARB 0x8808
-#  define GL_PROGRAM_NATIVE_TEX_INSTRUCTIONS_ARB 0x8809
-#  define GL_PROGRAM_NATIVE_TEX_INDIRECTIONS_ARB 0x880A
-#  define GL_MAX_PROGRAM_ALU_INSTRUCTIONS_ARB 0x880B
-#  define GL_MAX_PROGRAM_TEX_INSTRUCTIONS_ARB 0x880C
-#  define GL_MAX_PROGRAM_TEX_INDIRECTIONS_ARB 0x880D
-#  define GL_MAX_PROGRAM_NATIVE_ALU_INSTRUCTIONS_ARB 0x880E
-#  define GL_MAX_PROGRAM_NATIVE_TEX_INSTRUCTIONS_ARB 0x880F
-#  define GL_MAX_PROGRAM_NATIVE_TEX_INDIRECTIONS_ARB 0x8810
-#  define GL_MAX_TEXTURE_COORDS_ARB         0x8871
-#  define GL_MAX_TEXTURE_IMAGE_UNITS_ARB    0x8872
-# endif /* GL_ARB_fragment_program */
-#endif /* DONT_USE_ARB */
-
 /*-----------------------------------------------------------------
   -------------------------------------------------------------------
   CLASS
