6b2915fb48a034d52b8aa1eba4dde0900ca63f16
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2006-01-18 12:11:01 +0000

    found this lying around on my harddisk
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1655 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Particles/part_sink.h
@@ -0,0 +1,72 @@
+/*-----------------------------------------------------------------
+  LOG
+  GEM - Graphics Environment for Multimedia
+
+  Generate particles
+
+  Copyright (c) 1997-1999 Mark Danks. mark@danks.org
+  Copyright (c) Günther Geiger. geiger@epy.co.at
+  Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+  -----------------------------------------------------------------*/
+
+#ifndef INCLUDE_PART_SINK_H_
+#define INCLUDE_PART_SINK_H_
+
+#include "Base/GemBase.h"
+#include "papi.h"
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+
+  part_sink
+    
+  Generate particles
+
+  DESCRIPTION
+
+  -----------------------------------------------------------------*/
+class GEM_EXTERN part_sink : public GemBase
+{
+  CPPEXTERN_HEADER(part_sink, GemBase)
+
+    public:
+
+  //////////
+  // Constructor
+  part_sink(int argc, t_atom*argv);
+    	
+  //////////
+  virtual void 	render(GemState *state);
+
+ protected:
+    	
+  //////////
+  // Destructor
+  virtual ~part_sink();
+		
+  //////////
+  // kill inside?
+  void			killMess(int num);
+  bool			m_kill;
+
+  //////////
+  // vel.domain
+  PDomainEnum           m_domain;
+  void                  domainMess(t_symbol *s);
+  float			m_arg[9];
+  void			vectorMess(int argc, t_atom*argv);
+	
+ private:
+
+  //////////
+  // static member functions
+  static void		killMessCallback(void *data, t_floatarg num);
+  static void		domainMessCallback(void *data, t_symbol*s);
+  static void		vectorMessCallback(void *data, t_symbol*, int, t_atom*);
+};
+
+#endif	// for header file
