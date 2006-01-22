b315d3140c6dcccfbecf70d25d3cb459c15593db
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2006-01-22 21:25:15 +0000

    turn glBlendEquation OFF on w32 (stupid restriction, but faster than finding anything more sophisticated)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1658 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/openGL/GEMglBlendEquation.cpp
+++ src/openGL/GEMglBlendEquation.cpp
@@ -26,6 +26,9 @@ CPPEXTERN_NEW_WITH_ONE_ARG ( GEMglBlendEquation , t_floatarg, A_DEFFLOAT)
 GEMglBlendEquation :: GEMglBlendEquation	(t_floatarg arg0=0) :
   mode((GLenum)arg0)
 {
+#ifdef __WIN32__
+  post("[%s]: has been compiled without glBlendEquation", m_objectname);
+#endif
   m_inlet[0] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("mode"));
 }
 /////////////////////////////////////////////////////////
@@ -39,7 +42,9 @@ GEMglBlendEquation :: ~GEMglBlendEquation () {
 // Render
 //
 void GEMglBlendEquation :: render(GemState *state) {
+#ifndef __WIN32__
   glBlendEquation (mode);
+#endif
 }
 
 /////////////////////////////////////////////////////////
