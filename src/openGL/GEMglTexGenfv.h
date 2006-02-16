e9302062d2a61850b807074016ea22a0f63e8789
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2006-02-16 19:53:22 +0000

    useful for automatic texture-coordinate generation
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1725 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/openGL/GEMglTexGenfv.h
@@ -0,0 +1,64 @@
+ /* ------------------------------------------------------------------
+  * GEM - Graphics Environment for Multimedia
+  *
+  *  Copyright (c) 2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+  *	zmoelnig@iem.kug.ac.at
+  *  For information on usage and redistribution, and for a DISCLAIMER
+  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+  *
+  *  this file has been generated...
+  * ------------------------------------------------------------------
+  */
+
+#ifndef INCLUDE_GEM_GLTEXGENFV_H_
+#define INCLUDE_GEM_GLTEXGENFV_H_
+
+#include "Base/GemGLUtil.h"
+#include "Base/GemBase.h"
+
+/*
+ CLASS
+	GEMglTexGenfv
+ KEYWORDS
+	openGL	0
+ DESCRIPTION
+	wrapper for the openGL-function
+	"glTexGenfv( GLenum coord, GLenum pname, GLfloat[4] params)"
+ */
+
+class GEM_EXTERN GEMglTexGenfv : public GemBase
+{
+	CPPEXTERN_HEADER(GEMglTexGenfv, GemBase)
+
+	public:
+	  // Constructor
+	  GEMglTexGenfv (t_float, t_float, t_float);	// CON
+
+	protected:
+	  // Destructor
+	  virtual ~GEMglTexGenfv ();
+	  // Do the rendering
+	  virtual void	render (GemState *state);
+
+	// variables
+	  GLenum	coord;		// VAR
+	  virtual void	coordMess(t_float);	// FUN
+
+	  GLenum	pname;		// VAR
+	  virtual void	pnameMess(t_float);	// FUN
+
+	  GLfloat	params[4];		// VAR
+	  virtual void	paramsMess(int argc, t_atom* argv);	// FUN
+
+
+	private:
+
+	// we need some inlets
+	  t_inlet *m_inlet[3];
+
+	// static member functions
+	  static void	 coordMessCallback (void*, t_floatarg);
+	  static void	 pnameMessCallback (void*, t_floatarg);
+	  static void	 paramsMessCallback (void*, t_symbol*, int, t_atom*);
+};
+#endif // for header file
