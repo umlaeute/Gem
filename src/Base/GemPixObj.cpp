f4e12ba881aea94fe9822d06e6ea596ffacf1af3
tigital <tigital@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2006-03-22 23:06:11 +0000

    var initializing re-ordering
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@1806 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/GemPixObj.cpp
+++ src/Base/GemPixObj.cpp
@@ -8,7 +8,7 @@
 //
 //    Copyright (c) 1997-1999 Mark Danks.
 //    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute
+//    Copyright (c) 2001-2006 IOhannes m zmoelnig. forum::für::umläute
 //
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
@@ -24,7 +24,7 @@
 //
 /////////////////////////////////////////////////////////
 GemPixObj :: GemPixObj() : 
-  m_processOnOff(1), orgPixBlock(NULL),
+  orgPixBlock(NULL), m_processOnOff(1),
 #ifdef __MMX__
   m_simd(GEM_SIMD_MMX)
 #elif defined __SSE2__
