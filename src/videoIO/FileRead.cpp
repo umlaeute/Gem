a648a05bfc4ca5d490f20fa688a61a2ac887df94
holzi1 <holzi1@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2007-08-15 19:35:47 +0000

    this file is needed to generate the FileRead.o object file
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/branches/videoIO/Gem@2146 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/videoIO/FileRead.cpp
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//   VideoIO-Framework for GEM/PD
+//
+//   Writes a digital video (like AVI, Mpeg, Quicktime) to the harddisc.
+//
+//   FileWrite
+//   header file
+//
+//   copyright            : (C) 2007 by Thomas Holzmann
+//   email                : holzi1@gmx.at
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 3 of the License, or
+//   (at your option) any later version.
+//
+///////////////////////////////////////////////////////////////////////////
+
+#include "FileRead.h"
+
+// ATTENTION: although there is not much code in this file it
+// must exist, because we need this object file !
+
+namespace VideoIO_
+{
+    FileRead::FileRead() :
+      cspace_(-1), nr_of_frames_(0), framerate_(0)
+    {}
+}
