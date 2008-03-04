3f95e13d66b49364157cee79ec32830a612e3876
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-03-04 15:10:48 +0000

    make glew used always (not _really_ part of configure any more); print an error at the very end of configure if openGL and/or m_pd.h seem to be missing
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2306 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/configNT.h
+++ src/Base/configNT.h
@@ -32,11 +32,6 @@
 #define DONT_INCLUDE_GLEXT
 
 
-/* wanna use the OpenGL Extension Wrangler for runtime extension checking ? */
-/* #define USE_GLEW */
-
-
-
 /*
  * we want libjpeg and libtiff for reading/writing images
  */
