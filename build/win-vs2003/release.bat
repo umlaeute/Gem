64759f223ec36acb849260f3e3d73c7b4972c364
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-04-08 09:31:13 +0000

    a small script to produce simple alpha-releases
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2364 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ build/win-vs2003/release.bat
@@ -0,0 +1,25 @@
+mkdir gem-release
+
+cd ..\..\
+
+xcopy build\Gem.dll build\win-vs2003\gem-release\
+xcopy /E /I abstractions build\win-vs2003\gem-release\abstractions
+
+xcopy /E /I help build\win-vs2003\gem-release\help
+
+xcopy /E /I examples build\win-vs2003\gem-release\examples
+xcopy /E /I doc build\win-vs2003\gem-release\doc
+xcopy /E /I manual build\win-vs2003\gem-release\manual
+
+xcopy /E /I tests build\win-vs2003\gem-release\tests
+
+copy ChangeLog build\win-vs2003\gem-release
+copy GEM_INSTALL.bat build\win-vs2003\gem-release
+copy GEM.LICENSE.TERMS build\win-vs2003\gem-release
+copy GEM.README build\win-vs2003\gem-release
+copy GnuGPL.LICENSE build\win-vs2003\gem-release
+
+
+cd build\win-vs2003
+
+copy README_W32.txt.template gem-release\README_W32.txt
