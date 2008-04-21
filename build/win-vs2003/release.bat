ed27f7d2cee80cb9eb1b13e0b7797be927d0e3af
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-04-21 17:07:00 +0000

    create a date-based directory
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2410 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- build/win-vs2003/release.bat
+++ build/win-vs2003/release.bat
@@ -1,26 +1,27 @@
-mkdir gem-release
+set GEMDIR=gem-%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 
-xcopy Gem.dll gem-release\
+mkdir %GEMDIR%
+
+xcopy Gem.dll %GEMDIR%\
+copy README_W32.txt.template %GEMDIR%\README_W32.txt
 
 cd ..\..\
 
-xcopy /E /I abstractions build\win-vs2003\gem-release\abstractions
+xcopy /E /I abstractions build\win-vs2003\%GEMDIR%\abstractions
 
-xcopy /E /I help build\win-vs2003\gem-release\help
+xcopy /E /I help build\win-vs2003\%GEMDIR%\help
 
-xcopy /E /I examples build\win-vs2003\gem-release\examples
-xcopy /E /I doc build\win-vs2003\gem-release\doc
-xcopy /E /I manual build\win-vs2003\gem-release\manual
+xcopy /E /I examples build\win-vs2003\%GEMDIR%\examples
+xcopy /E /I doc build\win-vs2003\%GEMDIR%\doc
+xcopy /E /I manual build\win-vs2003\%GEMDIR%\manual
 
-xcopy /E /I tests build\win-vs2003\gem-release\tests
+xcopy /E /I tests build\win-vs2003\%GEMDIR%\tests
 
-copy ChangeLog build\win-vs2003\gem-release
-copy GEM_INSTALL.bat build\win-vs2003\gem-release
-copy GEM.LICENSE.TERMS build\win-vs2003\gem-release
-copy GEM.README build\win-vs2003\gem-release
-copy GnuGPL.LICENSE build\win-vs2003\gem-release
+copy ChangeLog build\win-vs2003\%GEMDIR%
+copy GEM_INSTALL.bat build\win-vs2003\%GEMDIR%
+copy GEM.LICENSE.TERMS build\win-vs2003\%GEMDIR%
+copy GEM.README build\win-vs2003\%GEMDIR%
+copy GnuGPL.LICENSE build\win-vs2003\%GEMDIR%
 
 
 cd build\win-vs2003
-
-copy README_W32.txt.template gem-release\README_W32.txt
