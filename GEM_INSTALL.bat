e41e63689bf6a1c26b6aca7e2b838b374906eaf0
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-05-06 10:07:35 +0000

    updated install-script (not checked!)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2450 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- GEM_INSTALL.bat
+++ GEM_INSTALL.bat
@@ -3,7 +3,7 @@ REM where does PD reside ??
 set PDPATH="%PROGRAMFILES%\pd"
 
 REM which pd-version do we have ? 
-set PDVERSION=0.37
+set PDVERSION=0.41
 
 
 rem ==============================================
@@ -14,43 +14,48 @@ echo Installing GEM...
 IF NOT EXIST %PDPATH%\bin\pd.exe goto location_error
 
 rem setting path for reference-patches accordingly
-set REFPATH=extra\help-Gem
-if %PDVERSION% LSS 0.37 set REFPATH=doc\5.reference\Gem
+set REFPATH=extra\Gem
+IF %PDVERSION% LSS 0.37 set REFPATH=doc\5.reference\Gem
+
+set GEMPATH=extra\Gem
+set EXAMPLEPATH=extra\Gem\examples
 
 echo Copying binary...
-copy Gem.dll %PDPATH%\extra > tempInstall.trash
 
-mkdir %PDPATH%\doc\gem\01.basic	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\02.advanced	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\03.lighting	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\04.pix	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\04.video	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\05.text	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\06.particle	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\08.io	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\99.games	> tempInstall.trash
-mkdir %PDPATH%\doc\gem\data > tempInstall.trash
-mkdir %PDPATH%\doc\gem\manual	> tempInstall.trash
+mkdir %PDPATH%\%GEMPATH%	> tempInstall.trash
+copy Gem.dll %PDPATH%\%GEMPATH% > tempInstall.trash
+
+mkdir %PDPATH%\%EXAMPLEPATH%\01.basic	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\02.advanced	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\03.lighting	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\04.pix	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\04.video	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\05.text	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\06.particle	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\08.io	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\99.games	> tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\data > tempInstall.trash
+mkdir %PDPATH%\%EXAMPLEPATH%\manual	> tempInstall.trash
 
 echo assuming you are using pd-%PDVERSION%
 mkdir %PDPATH%\%REFPATH%
 
 echo Copying examples...
-copy examples\01.basic\* %PDPATH%\doc\gem\01.basic > tempInstall.trash
-copy examples\02.advanced\* %PDPATH%\doc\gem\02.advanced > tempInstall.trash
-copy examples\03.lighting\* %PDPATH%\doc\gem\03.lighting > tempInstall.trash
-copy examples\04.pix\* %PDPATH%\doc\gem\04.pix > tempInstall.trash
-copy examples\04.video\* %PDPATH%\doc\gem\04.video > tempInstall.trash
-copy examples\05.text\* %PDPATH%\doc\gem\05.text > tempInstall.trash
-copy examples\06.particle\* %PDPATH%\doc\gem\06.particle > tempInstall.trash
-copy examples\07.texture\* %PDPATH%\doc\gem\07.texture > tempInstall.trash
-copy examples\08.io\* %PDPATH%\doc\gem\08.io > tempInstall.trash
-copy examples\99.games\* %PDPATH%\doc\gem\99.games > tempInstall.trash
-copy examples\data\*  %PDPATH%\doc\gem\data > tempInstall.trash
+copy examples\01.basic\* %PDPATH%\%EXAMPLEPATH%\01.basic > tempInstall.trash
+copy examples\02.advanced\* %PDPATH%\%EXAMPLEPATH%\02.advanced > tempInstall.trash
+copy examples\03.lighting\* %PDPATH%\%EXAMPLEPATH%\03.lighting > tempInstall.trash
+copy examples\04.pix\* %PDPATH%\%EXAMPLEPATH%\04.pix > tempInstall.trash
+copy examples\04.video\* %PDPATH%\%EXAMPLEPATH%\04.video > tempInstall.trash
+copy examples\05.text\* %PDPATH%\%EXAMPLEPATH%\05.text > tempInstall.trash
+copy examples\06.particle\* %PDPATH%\%EXAMPLEPATH%\06.particle > tempInstall.trash
+copy examples\07.texture\* %PDPATH%\%EXAMPLEPATH%\07.texture > tempInstall.trash
+copy examples\08.io\* %PDPATH%\%EXAMPLEPATH%\08.io > tempInstall.trash
+copy examples\99.games\* %PDPATH%\%EXAMPLEPATH%\99.games > tempInstall.trash
+copy examples\data\*  %PDPATH%\%EXAMPLEPATH%\data > tempInstall.trash
 
 echo Copying documentation...
-copy doc\gem*.* %PDPATH%\doc\gem\manual > tempInstall.trash
-copy manual\*.html %PDPATH%\doc\gem\manual > tempInstall.trash
+copy doc\gem*.* %PDPATH%\%EXAMPLEPATH%\manual > tempInstall.trash
+copy manual\*.html %PDPATH%\%EXAMPLEPATH%\manual > tempInstall.trash
 echo Copying reference files...
 copy help\* %PDPATH%\%REFPATH% > tempInstall.trash
 
