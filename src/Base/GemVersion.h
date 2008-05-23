37dbaac5b666c8b25e491489260edac09aaa3b75
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-05-23 08:28:37 +0000

    added automatic version checks from TRUNK
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/branches/v0-91/Gem@2482 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/GemVersion.h
+++ src/Base/GemVersion.h
@@ -1,6 +1,21 @@
 #ifndef GEM_VERSION_H
 #define GEM_VERSION_H
-static const char *GEM_VERSION = "0.91 'tigital'";
+
+#define GEM_VERSION_MAJOR 0
+#define GEM_VERSION_MINOR 91
+#define GEM_VERSION_BUGFIX 0
+#define GEM_VERSION_CODENAME "'tigital'"
+
+
+#define STRINGIFY2(x) #x
+#define STRINGIFY(x) STRINGIFY2(x)
+
+static const char *GEM_VERSION = \
+  "" STRINGIFY(GEM_VERSION_MAJOR)  \
+  "." STRINGIFY(GEM_VERSION_MINOR) \
+  "." STRINGIFY(GEM_VERSION_BUGFIX) \
+  " " GEM_VERSION_CODENAME;
+
 
 static const char *GEM_MAINTAINER = "IOhannes m zmoelnig";
 
