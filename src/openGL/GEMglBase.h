1df929817ec596333ba2d9ef2a228556ec7092d2
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2008-09-16 15:26:37 +0000

    a new wrapper object (+ a base class to be used for all wrapper objects)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2579 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/openGL/GEMglBase.h
@@ -0,0 +1,23 @@
+/* ------------------------------------------------------------------
+ * GEM - Graphics Environment for Multimedia
+ *
+ *  Copyright (c) 2008 zmoelnig@iem.at
+ *  For information on usage and redistribution, and for a DISCLAIMER
+ *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+ *
+ *  this file has been generated...
+ * ------------------------------------------------------------------
+ */
+#ifndef INCLUDE_GEM_GLBASE_H_
+#define INCLUDE_GEM_GLBASE_H_
+
+#include "Base/GemGLUtil.h"
+#include "Base/GemBase.h"
+
+class GEM_EXTERN GEMglBase : public GemBase
+{
+
+};
+
+
+#endif /* for header file */
