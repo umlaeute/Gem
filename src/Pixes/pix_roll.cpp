ea0d2dd0672a8ff827cad394c7b820b66f0b4631
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2009-04-06 10:21:42 +0000

    reindentation
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2735 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Pixes/pix_roll.cpp
+++ src/Pixes/pix_roll.cpp
@@ -24,16 +24,16 @@ CPPEXTERN_NEW(pix_roll)
 pix_roll :: pix_roll()
 {	long size,src;
     
-inletBlur = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("roll"));
+  inletBlur = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float, gensym("roll"));
 
-m_vroll = 0;
-m_blurH = 240;
-m_blurW = 240;
-m_blurBpp = 2;
-size = 320 * 240 * 4;
-saved = new unsigned char [size];
-src=0;
-m_axis = 0;
+  m_vroll = 0;
+  m_blurH = 240;
+  m_blurW = 240;
+  m_blurBpp = 2;
+  size = 320 * 240 * 4;
+  saved = new unsigned char [size];
+  src=0;
+  m_axis = 0;
 
 }
 
@@ -86,7 +86,7 @@ void pix_roll :: processRGBAImage(imageStruct &image)
       w=((image.xsize-m_vroll)*image.csize)/4;
       while(w--){
         *dstdata++=*srcdata++;
-	*dstdata++=*srcdata++;
+        *dstdata++=*srcdata++;
         *dstdata++=*srcdata++;
         *dstdata++=*srcdata++;
       }
@@ -101,7 +101,7 @@ void pix_roll :: processRGBAImage(imageStruct &image)
       w=(m_vroll*image.csize)/4;
       while(w--){
         *dstdata++=*srcdata++;
-	*dstdata++=*srcdata++;
+        *dstdata++=*srcdata++;
         *dstdata++=*srcdata++;
         *dstdata++=*srcdata++;
       }
@@ -171,11 +171,11 @@ void pix_roll :: processImage(imageStruct &image)
 void pix_roll :: processYUVImage(imageStruct &image)
 {
 
-    int h,w,length;
-    long src,dst;
-    long rollstart,pixsize;
+  int h,w,length;
+  long src,dst;
+  long rollstart,pixsize;
 
-if (m_blurH != image.ysize || m_blurW != image.xsize || m_blurBpp != image.csize) {
+  if (m_blurH != image.ysize || m_blurW != image.xsize || m_blurBpp != image.csize) {
 
     m_blurH = image.ysize;
     m_blurW = image.xsize;
@@ -183,69 +183,69 @@ if (m_blurH != image.ysize || m_blurW != image.xsize || m_blurBpp != image.csize
     m_blurSize = m_blurH * m_blurW * m_blurBpp;
     if(saved)delete saved;
     saved = new unsigned char [m_blurSize];
-}    
-pixsize = image.ysize * image.xsize * image.csize;
-if (!m_axis) {
-//start at the breakpoint
-dst = 0;
- m_vroll%=image.ysize;
- m_vroll+=image.ysize;
- m_vroll%=image.ysize;
-rollstart = m_vroll * image.xsize * image.csize;
+  }    
+  pixsize = image.ysize * image.xsize * image.csize;
+  if (!m_axis) {
+    //start at the breakpoint
+    dst = 0;
+    m_vroll%=image.ysize;
+    m_vroll+=image.ysize;
+    m_vroll%=image.ysize;
+    rollstart = m_vroll * image.xsize * image.csize;
 
-for (src = rollstart; src < pixsize;src++){
-    saved[dst] = image.data[src];
-    dst++;
+    for (src = rollstart; src < pixsize;src++){
+      saved[dst] = image.data[src];
+      dst++;
     }
-//finish the rest
-for(src = 0;src<rollstart-1;src++){
-    saved[dst] = image.data[src];
-    dst++;
+    //finish the rest
+    for(src = 0;src<rollstart-1;src++){
+      saved[dst] = image.data[src];
+      dst++;
     }
-}else{
-  if (m_vroll % 2 ){m_vroll += 1;}//check for odd in YUV
- m_vroll%=image.ysize;
- m_vroll+=image.ysize;
- m_vroll%=image.ysize;
+  }else{
+    if (m_vroll % 2 ){m_vroll += 1;}//check for odd in YUV
+    m_vroll%=image.ysize;
+    m_vroll+=image.ysize;
+    m_vroll%=image.ysize;
 
-rollstart = m_vroll * image.csize;
-dst = 0;
-src=rollstart;
-length = (image.xsize-m_vroll)*2;
-//moved the rolled part of the data
-src = rollstart;//offset source to start of roll
-dst = 0; 
+    rollstart = m_vroll * image.csize;
+    dst = 0;
+    src=rollstart;
+    length = (image.xsize-m_vroll)*2;
+    //moved the rolled part of the data
+    src = rollstart;//offset source to start of roll
+    dst = 0; 
 
-   for (h=0; h<image.ysize; h++){ //fixed...we have to do every row
+    for (h=0; h<image.ysize; h++){ //fixed...we have to do every row
    
-    for(w=m_vroll; w<image.xsize; w++){//from rollstart to the last column
+      for(w=m_vroll; w<image.xsize; w++){//from rollstart to the last column
         
         saved[dst] = image.data[src];
         saved[dst+1] = image.data[src+1];
         
         src+=2;dst+=2;
+      }
+      src+= rollstart;
+      dst+= rollstart;
     }
-    src+= rollstart;
-    dst+= rollstart;
-    }
-//finish up
-src=0;
-dst=(image.xsize-m_vroll)*2;
-for (h=0; h<image.ysize; h++){ //fixed...we have to do every row
+    //finish up
+    src=0;
+    dst=(image.xsize-m_vroll)*2;
+    for (h=0; h<image.ysize; h++){ //fixed...we have to do every row
    
-    for(w=image.xsize-m_vroll; w<image.xsize; w++){//from rollstart to the last column
+      for(w=image.xsize-m_vroll; w<image.xsize; w++){//from rollstart to the last column
         
         saved[dst] = image.data[src];
         saved[dst+1] = image.data[src+1];
         
        
         src+=2;dst+=2;
+      }
+      src+= image.xsize*2-rollstart;
+      dst+= image.xsize*2-rollstart;
     }
-    src+= image.xsize*2-rollstart;
-    dst+= image.xsize*2-rollstart;
-    }
-}
-memcpy(image.data,saved,pixsize);     
+  }
+  memcpy(image.data,saved,pixsize);     
 
 }
 
@@ -257,10 +257,10 @@ memcpy(image.data,saved,pixsize);
 void pix_roll :: obj_setupCallback(t_class *classPtr)
 {
 
-    class_addmethod(classPtr, (t_method)&pix_roll::rollCallback,
-		  gensym("roll"), A_DEFFLOAT, A_NULL);
-    class_addmethod(classPtr, (t_method)&pix_roll::axisCallback,
-		  gensym("axis"), A_DEFFLOAT, A_NULL);
+  class_addmethod(classPtr, (t_method)&pix_roll::rollCallback,
+                  gensym("roll"), A_DEFFLOAT, A_NULL);
+  class_addmethod(classPtr, (t_method)&pix_roll::axisCallback,
+                  gensym("axis"), A_DEFFLOAT, A_NULL);
 }
 
 void pix_roll :: rollCallback(void *data, t_floatarg value)
