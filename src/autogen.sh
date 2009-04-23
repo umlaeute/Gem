ea92c0a7c0b5e2346eff04c9299345a6ab032db4
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2009-04-23 09:06:16 +0000

    simple autogen-sh script that runs aclocal and autoconf; LATER these calls should be removed from the Makefile entirely
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2746 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/autogen.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#-----------------------------------------------------------------------
+# File: autogen.sh
+# Description:
+#   + wrapper for m4 black-magic
+#-----------------------------------------------------------------------
+
+# taken from Brian Jurish's readdir-external and simplfied for use with Gem
+
+MY_ALDIRS="."
+MY_ACDIRS="."
+
+test -z "$ACLOCAL" && ACLOCAL=aclocal
+test -z "$AUTOCONF" && AUTOCONF=autoconf
+
+if test -n "$MY_ALDIRS"; then
+ for d in $MY_ALDIRS ; do
+    echo "(cd $d ; $ACLOCAL)"
+    (cd $d ; $ACLOCAL)
+ done
+fi
+
+if test -n "$MY_ACDIRS"; then
+ for d in $MY_ACDIRS ; do
+    echo "(cd $d ; $AUTOCONF)"
+    (cd $d ; $AUTOCONF)
+ done
+fi
+
