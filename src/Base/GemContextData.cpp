40fddcc45d18a2f76527af8a8dfeb32cd53def79
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2009-05-25 15:00:12 +0000

    base class for context-specific data (to be used LATER)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2814 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Base/GemContextData.cpp
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.kug.ac.at
+//
+// Implementation file
+//
+//    Copyright (c) 2009 IOhannes m zmoelnig. forum::für::umläute. IEM @ KUG
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+
+
+
+#include "GemContextData.h"
+#include "GemContext.h"
+
+int GemContextDataBase::getCurContext(void) {
+  /* this should get an integer-index of the current context from GemContext */
+  int id=0;
+  id=GemContext::getContextId();
+  return id;
+}
---
+++ src/Base/GemContextData.h
@@ -0,0 +1,140 @@
+/*-----------------------------------------------------------------
+  LOG
+  GEM - Graphics Environment for Multimedia
+
+  data specific to a rendering context
+
+  Copyright (c) 2009 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+  -----------------------------------------------------------------*/
+
+#ifndef INCLUDE_GEMCONTEXTDATA_H_
+#define INCLUDE_GEMCONTEXTDATA_H_
+
+#include "Base/config.h"
+#include "Base/GemExportDef.h"
+#include <vector>
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  GemContextData
+    
+  rendering context specific data
+  this is heavily inspired by VrJuggler
+
+  DESCRIPTION
+
+  several things in openGL like display-lists are context dependent
+  if we have multiple contexts, such values most be generated for each context
+  GemContextData provides a generic (templated) datatype for this
+
+  LATER (SOONER) think about splitting the render() into a context-specific section that 
+  set's up display-lists,... and a draw() function that just calls the pre-generated values
+    
+  -----------------------------------------------------------------*/
+
+class GEM_EXTERN GemContextDataBase {
+ protected:
+  int getCurContext(void);
+};
+
+
+template<class GemContextDataType = int>
+  class GEM_EXTERN GemContextData : GemContextDataBase
+  {
+    public:
+    
+    //////////
+    // Constructor
+    GemContextData(void) {;}
+    	
+    /**
+     * Returns reference to user data for the current context.
+     *
+     * @pre We are in a draw process.
+     * @note Should only be called from the draw function.
+     *        Results are un-defined for other functions.
+     */
+    GemContextDataType& operator*()
+    {
+      return (*getPtrToCur());
+    }
+
+    /**
+     * Returns reference to user data for the current context.
+     *
+     * @pre We are in a draw process.
+     * @note Should only be called from the draw function.
+     *       Results are un-defined for other functions.
+     */
+    GemContextDataType* operator->()
+    {
+      return getPtrToCur();
+    }
+
+    /** Container for the thread-specific context-specific data. */
+    template<class DATA_TYPE>
+    struct ThreadContextData
+    {
+      public:
+      typedef std::vector<DATA_TYPE*> data_list_t;
+
+      ThreadContextData()
+        : mContextDataVector()
+      {;}
+
+      ~ThreadContextData()
+      {
+        for (typename data_list_t::iterator itr = mContextDataVector.begin();
+             mContextDataVector.end() != itr; itr++)
+          {
+            delete *itr;
+            (*itr) = NULL;
+          }
+        mContextDataVector.clear();
+      }
+
+      /* Makes sure that the vector is at least requiredSize large */
+      void checkSize(unsigned int requiredSize)
+      {
+        if(requiredSize > mContextDataVector.size())
+          {
+            mContextDataVector.reserve(requiredSize);          // Resize smartly
+            while(mContextDataVector.size() < requiredSize)    // Add any new items needed
+              {
+                mContextDataVector.push_back(new DATA_TYPE());
+              }
+          }
+      }
+
+      data_list_t mContextDataVector;   /**< Vector of user data */
+    };
+
+    /**
+     * Returns a pointer to the correct data element in the current context.
+     *
+     * @pre We are in the draw function.
+     * @post Synchronized.
+     * @note ASSERT: Same context is rendered by same thread each time.
+     */
+    GemContextDataType* getPtrToCur(void)
+    {
+      // Get current context
+      int context_id = getCurContext();
+      // Cache ref for better performance
+      ThreadContextData<GemContextDataType>* thread_specific_context_data = &(*mThreadSpecificContextData);
+
+      thread_specific_context_data->checkSize(context_id+1);     // Make sure we are large enough (+1 since we have index)
+
+      return thread_specific_context_data->mContextDataVector[context_id];
+    }
+    private:
+    ThreadContextData<GemContextDataType>  mThreadSpecificContextData;
+  };
+
+
+
+#endif	// for header file
