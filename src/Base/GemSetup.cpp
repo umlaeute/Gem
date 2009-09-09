45064bb00617286e16945c938353bf3c90912a96
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2009-09-09 14:17:31 +0000

    merged back changes from 0.92
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@2890 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/GemSetup.cpp
+++ src/Base/GemSetup.cpp
@@ -20,51 +20,90 @@
 #include "GemMan.h"
 #include "GemVersion.h"
 
+#include <stdio.h>
+
+#ifdef _WIN32
+# include <io.h>
+# include <windows.h>
+# define snprintf _snprintf
+#else
+# include <unistd.h>
+#endif
+
+# include <fcntl.h>
+
 
 static const char GEM_MAINTAINER[] = "IOhannes m zmoelnig";
 
 static const char *GEM_AUTHORS[] = {
   "Chris Clepper",
-  "James Tittle",
+  "Cyrille Henry",
   "IOhannes m zmoelnig"
 };
 
 static const char GEM_OTHERAUTHORS[] =
-  "Guenter Geiger, Daniel Heckenberg, Cyrille Henry, et al.";
-
-
+  "Guenter Geiger, Daniel Heckenberg, James Tittle, Hans-Christop Steiner, et al.";
 
 extern "C" {
-//#define GEM_ADDOWNPATH
+#define GEM_ADDOWNPATH
 #ifdef GEM_ADDOWNPATH
 # include "s_stuff.h"
 # include <string.h>
 
   /* this is ripped from m_imp.h */
-struct _gemclass
-{
+  struct _gemclass
+  {
     t_symbol *c_name;                   /* name (mostly for error reporting) */
     t_symbol *c_helpname;               /* name of help file */
     t_symbol *c_externdir;              /* directory extern was loaded from */
-  /* ... */ /* the real t_class continues here... */
-};
+    /* ... */ /* the real t_class continues here... */
+  };
+# define t_gemclass struct _gemclass
 
-#define t_gemclass struct _gemclass
-static t_class *Gem_setup_class;
-#endif
+  static void Gem_addownpath(const char*filename) {
+    char buf[MAXPDSTRING];
+    char*bufptr=NULL;
+    int fd=-1;
 
-  GEM_EXTERN void Gem_setup()
-  {
-#ifdef GEM_ADDOWNPATH
-    t_namelist*searchpath=sys_searchpath;
+    int flags=O_RDONLY;
+# ifdef _WIN32
+    flags |= _O_BINARY;
+# endif
+
+    /* check whether we can find the abstractions (because they are already in Pd's path) */
+    if ((fd=canvas_open(NULL, filename, "", buf, &bufptr, MAXPDSTRING, 1))>=0){
+      close(fd);
+      return;
+    }
+
     char*mypath=0;
     t_gemclass *c = (t_gemclass*)class_new(gensym("Gem"), 0, 0, 0, 0, A_NULL);
     mypath=c->c_externdir->s_name;
 
+    /* check whether we can find the abstractions in Gem's own path */
+    snprintf(buf, MAXPDSTRING-1, "%s/%s", mypath, filename);
+    buf[MAXPDSTRING-1]=0;
+    if (fd=open(buf, flags)>=0){
+      close(fd);
+    } else {
+      // can't find this abstraction...giving up
+      return;
+    }
+
     verbose(1, "eventually adding Gem path '%s' to search-paths", mypath);
+# ifndef _MSC_VER
+    /* MSVC cannot really handle these non-exported symbols */
     sys_searchpath = namelist_append(sys_searchpath, mypath, 0);
+# endif
+  }
+#else
+  static void Gem_addownpath(const char*filename) {  }
 #endif
 
+  GEM_EXTERN void Gem_setup()
+  {
+    Gem_addownpath("hsv2rgb.pd");
+    
     // startup GEM
 
     post("GEM: Graphics Environment for Multimedia");
