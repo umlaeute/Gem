681bf1d83b6d2150b5233861ce8cbccc1997f4b0
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-02-25 13:06:27 +0000

    realtime environment header (e.g. include m_pd.h)
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3140 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Helpers/RTE.h
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------
+  LOG
+  GEM - Graphics Environment for Multimedia
+
+  include Realtime-Environments headers
+
+  Copyright (c) 2010 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+  -----------------------------------------------------------------*/
+
+#ifndef INCLUDE_GEM_RTE_H_
+#define INCLUDE_GEM_RTE_H_
+
+#include "m_pd.h"
+
+
+#endif /* INCLUDE_GEM_RTE_H_ */
