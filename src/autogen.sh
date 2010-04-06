5d05216ccd141206044c975bb6a5d2c63071854d
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-04-06 17:15:10 +0000

    use autoreconf if available
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3194 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/autogen.sh
+++ src/autogen.sh
@@ -8,26 +8,40 @@
 
 # taken from Brian Jurish's readdir-external and simplfied for use with Gem
 
-MY_ALDIRS="."
-MY_ACDIRS="."
+MY_ALDIRS=". extra/pix_artoolkit"
+MY_ACDIRS=". extra/pix_artoolkit"
+MY_M4DIRS=`pwd`/m4
 
 test -z "$ACLOCAL" && ACLOCAL=aclocal
 test -z "$AUTOCONF" && AUTOCONF=autoconf
+test -z "$AUTORECONF" && AUTORECONF=autoreconf
 
-if test -n "$MY_ALDIRS"; then
- for d in $MY_ALDIRS ; do
-    echo "(cd $d ; $ACLOCAL -I m4)"
-    (cd $d ; $ACLOCAL -I m4)
- done
+# check whether autoreconf is up and running
+echo checking $AUTORECONF
+if $AUTORECONF --version > /dev/null 2>&1; then
+ :
+else
+ AUTORECONF=""
 fi
 
-if test -n "$MY_ACDIRS"; then
- for d in $MY_ACDIRS ; do
-    echo "(cd $d ; $AUTOCONF)"
-    (cd $d ; $AUTOCONF)
- done
-fi
+if test -n "$AUTORECONF"; then
+ $AUTORECONF --force --verbose --install -I $MY_M4DIRS
+else
 
+ if test -n "$MY_ALDIRS"; then
+  for d in $MY_ALDIRS ; do
+     echo "(cd $d ; $ACLOCAL -I $MY_M4DIRS)"
+     (cd $d ; $ACLOCAL -I $MY_M4DIRS)
+  done
+ fi
+ 
+ if test -n "$MY_ACDIRS"; then
+  for d in $MY_ACDIRS ; do
+     echo "(cd $d ; $AUTOCONF)"
+     (cd $d ; $AUTOCONF)
+  done
+ fi
+fi
 
 echo "if everything above succeeded, you can now run './configure'"
 echo "for options see './configure --help'"
