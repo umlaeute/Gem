be29b572e693556cc3a2ac955093cc723b6ffb46
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-04-08 16:05:29 +0000

    wrapper for file operations
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3214 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/Base/GemFiles.cpp
@@ -0,0 +1,78 @@
+#include "Base/GemConfig.h"
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0400
+#include <windows.h>
+#else
+# include <glob.h>
+#endif
+
+#include "Base/GemFiles.h"
+
+
+namespace gem {
+  namespace files {
+
+    std::vector<std::string>getFilenameListing(std::string basename) {
+      std::vector<std::string>result;
+#ifdef _WIN32
+      WIN32_FIND_DATA findData;
+      DWORD errorNumber;
+      HANDLE hFind;
+      
+      hFind = FindFirstFile(basename.c_str(), &findData);
+      if (hFind == INVALID_HANDLE_VALUE) 
+        {
+          errorNumber = GetLastError();
+          switch (errorNumber)
+            {
+            case ERROR_FILE_NOT_FOUND:
+            case ERROR_PATH_NOT_FOUND:
+              break;
+            default:
+              FormatMessage(
+                            FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+                            FORMAT_MESSAGE_FROM_SYSTEM,
+                            NULL,
+                            errorNumber,
+                            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                            (LPTSTR) &lpErrorMessage,
+                            0, NULL );
+              //pd_error(x,"[folder_list] %s", (char *)lpErrorMessage);
+            }
+          return result;
+        }
+      do {
+        result.push_back(findData.cFileName());
+      } while (FindNextFile(hFind, &findData) != 0);
+
+      FindClose(hFind);
+#else
+      /* use glob */
+      glob_t glob_buffer;
+      int i=0;
+      switch( glob( basename.c_str(), GLOB_TILDE, NULL, &glob_buffer ) ) {
+      case GLOB_NOSPACE: 
+        //        error("out of memory for \"%s\"",basename.c_str()); 
+        return result;
+# ifdef GLOB_ABORTED
+      case GLOB_ABORTED: 
+        //error("aborted \"%s\"",basename.c_str()); 
+        return result;
+# endif
+# ifdef GLOB_NOMATCH
+      case GLOB_NOMATCH: 
+        //error("nothing found for \"%s\"",basename.c_str()); 
+        return result;
+# endif
+      }
+      for(i=0; i<glob_buffer.gl_pathc; i++) {
+        result.push_back(glob_buffer.gl_pathv[i]);
+      }
+      globfree( &(glob_buffer) );
+#endif
+
+      return result;
+    }
+  };
+};
---
+++ src/Base/GemFiles.h
@@ -0,0 +1,33 @@
+/*-----------------------------------------------------------------
+LOG
+    GEM - Graphics Environment for Multimedia
+
+	- file handling with Gem
+
+    Copyright (c) 2010 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+
+
+#ifndef INCLUDE_GEMFILES_H_
+#define INCLUDE_GEMFILES_H_
+
+#include <string>
+#include <vector>
+
+
+namespace gem {
+  
+  namespace file {
+
+    std::vector<std::string>getFilenameListing(std::string pattern);
+
+
+
+  };
+};
+
+#endif /* INCLUDE_GEMFILES_H_ */
