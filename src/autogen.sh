889b2d2bcc81fc501e6b63459a7be6c0efabb290
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-04-26 09:37:48 +0000

    stripped down to only run autoreconf
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3240 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/autogen.sh
+++ src/autogen.sh
@@ -1,48 +1,7 @@
 #!/bin/sh
 
-#-----------------------------------------------------------------------
-# File: autogen.sh
-# Description:
-#   + wrapper for m4 black-magic
-#-----------------------------------------------------------------------
 
-# taken from Brian Jurish's readdir-external and simplfied for use with Gem
+echo PATH: $PATH
 
-MY_ALDIRS=". extra/pix_artoolkit"
-MY_ACDIRS=". extra/pix_artoolkit"
-MY_M4DIRS=`pwd`/m4
-
-test -z "$ACLOCAL" && ACLOCAL=aclocal
-test -z "$AUTOCONF" && AUTOCONF=autoconf
-test -z "$AUTORECONF" && AUTORECONF=autoreconf
-
-# check whether autoreconf is up and running
-echo checking $AUTORECONF
-if $AUTORECONF --version > /dev/null 2>&1; then
- :
-else
- AUTORECONF=""
-fi
-
-if test -n "$AUTORECONF"; then
- echo $AUTORECONF --force --verbose --install 
- $AUTORECONF --force --verbose --install 
-else
-
- if test -n "$MY_ALDIRS"; then
-  for d in $MY_ALDIRS ; do
-     echo "(cd $d ; $ACLOCAL -I $MY_M4DIRS)"
-     (cd $d ; $ACLOCAL -I $MY_M4DIRS)
-  done
- fi
- 
- if test -n "$MY_ACDIRS"; then
-  for d in $MY_ACDIRS ; do
-     echo "(cd $d ; $AUTOCONF)"
-     (cd $d ; $AUTOCONF)
-  done
- fi
-fi
-
-echo "if everything above succeeded, you can now run './configure'"
-echo "for options see './configure --help=recursive'"
+echo running autoreconf
+autoreconf --force --verbose --install 
