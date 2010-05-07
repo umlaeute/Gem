23c4597859da8a8b5c0e125a3fab16a9cf1f6d93
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-05-07 09:37:48 +0000

    when changing the dimension for pix_resize, this should trigger setPixModified()
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3262 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Pixes/pix_resize.cpp
+++ src/Pixes/pix_resize.cpp
@@ -86,6 +86,8 @@ void pix_resize :: dimenMess(int width, int height) {
 
   m_width =width;
   m_height=height;
+
+  setPixModified();
 }
 
 /////////////////////////////////////////////////////////
