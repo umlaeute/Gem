dd088308d33278d84bd8057a4a6baf26d20d81cd
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-06-25 06:35:44 +0000

    fixing scopeXYZ~
    working, though not yet with textures
    
    
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3394 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Geos/scopeXYZ.cpp
+++ src/Geos/scopeXYZ.cpp
@@ -22,6 +22,8 @@
 
 #include "Base/GemState.h"
 
+#define MARK post("%s:%d:: %s", __FILE__, __LINE__, __FUNCTION__)
+
 CPPEXTERN_NEW_WITH_ONE_ARG(scopeXYZ, t_floatarg, A_DEFFLOAT)
 
 /////////////////////////////////////////////////////////
@@ -34,7 +36,7 @@ CPPEXTERN_NEW_WITH_ONE_ARG(scopeXYZ, t_floatarg, A_DEFFLOAT)
 /////////////////////////////////////////////////////////
 scopeXYZ :: scopeXYZ(t_floatarg len)
   : GemShape(), 
-	m_drawType(GL_LINE_STRIP),
+    m_drawType(GL_LINE_STRIP),
     m_length(0), m_position(0),
     m_vertarray(NULL), m_vert(NULL)
 {
@@ -46,7 +48,7 @@ scopeXYZ :: scopeXYZ(t_floatarg len)
     inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_signal, &s_signal);
 }
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
 // Destructor
 //
 /////////////////////////////////////////////////////////
@@ -54,6 +56,45 @@ scopeXYZ :: ~scopeXYZ()
 {
   if(m_vert)delete[]m_vert;
   if(m_vertarray)delete[]m_vertarray;
+}
+
+void scopeXYZ :: doLengthMess(int L) {
+  // this resizes to m_length if this is set, or to L otherwise
+  // actually, resizing is done to the double-size!
+
+  int length=0;
+  if(m_length>0)
+    length=m_length;
+  else if (L>0)
+    length=L;
+
+  if(0==length)return; // oops
+
+  if(length*2==m_realLength) {
+    // cool, no need to resize
+    return;
+  }
+
+  if(m_vert)delete[]m_vert;
+  if(m_vertarray)delete[]m_vertarray;
+
+
+  m_realLength=length;
+  length=m_realLength*2;
+
+  m_vert = new t_sample*[length];
+  m_vertarray = new t_sample[length*3];
+  post("m_vertarray: %d*3 samples at %x", length, m_vertarray);
+
+  int i;
+  for (i = 0; i < length*3; i++)  {
+    m_vertarray[i]=0.0f;
+  }
+  for (i = 0; i < length; i++)  {
+    m_vert[i]=m_vertarray+3*i;
+  }
+
+  m_position=0;
 }
 
 
@@ -64,22 +105,8 @@ void scopeXYZ :: lengthMess(int l)
     return;
   }
 
-  if(l==m_length)return;
-
-  if(m_vert)delete[]m_vert;
-  if(m_vertarray)delete[]m_vertarray;
-
-  m_length = l;
-
-  m_vert = new float*[m_length];
-  m_vertarray = new float[m_length*3];
-
-  for (i = 0; i < m_length*3; i++)  {
-    m_vertarray[i]=0.0f;
-  }
-  for (i = 0; i < m_length; i++)  {
-    m_vert[i]=m_vertarray+3*i;
-  }
+  m_length=l;
+  doLengthMess();
 }
 
 /////////////////////////////////////////////////////////
@@ -88,71 +115,85 @@ void scopeXYZ :: lengthMess(int l)
 /////////////////////////////////////////////////////////
 void scopeXYZ :: renderShape(GemState *state)
 {
+  MARK;
   if(m_drawType==GL_DEFAULT_GEM)m_drawType=GL_LINE_STRIP;
   glNormal3f(0.0f, 0.0f, 1.0f);
   glLineWidth(m_linewidth);
-  if (state->texture && state->numTexCoords)
-    {
-      glBegin(m_drawType);
-      for (int i = 0; i < m_length; i++)
-	{
-	  if (state->numTexCoords < i)
-	    glTexCoord2f(state->texCoords[state->numTexCoords - 1].s,
-			 state->texCoords[state->numTexCoords - 1].t);
-	  else
-	    glTexCoord2f(state->texCoords[i].s,
-			 state->texCoords[i].t);
-	  glVertex3fv(m_vert[i]);
-	}
-      glEnd();
-    }
-  else
-    {
-      if (state->texture)
-	{
-	  float maxVal[2];
-	  maxVal[0] = maxVal[1] = 0.f;
-
-	  if(maxVal[0]<0)maxVal[0]*=-1.f;
-	  if(maxVal[1]<0)maxVal[1]*=-1.f;
-
-	  for (int i = 0; i < m_length; i++)
-    	    {
-	      for (int j = 0; j < 2; j++)
-		{
-		  if (m_vert[i][j] < 0)
-		    {
-		      if (-m_vert[i][j] > maxVal[j])
-			maxVal[j] = -m_vert[i][j];
-		    }
-		  else
-		    {
-		      if (m_vert[i][j] > maxVal[j])
-			maxVal[j] = m_vert[i][j];
-		    }
-		}
-    	    }
-	  glBegin(m_drawType);
-	  for(int n=0; n < m_length; n++)
-	    {
-	      glTexCoord2f(m_vert[n][0] / maxVal[0],
-			   m_vert[n][1] / maxVal[1]);
-	      glVertex3fv(m_vert[n]);
-	    }
-	  glEnd();
-	}
+  if (state->texture && state->numTexCoords) {
+    // texture and texCoords present
+    glBegin(m_drawType);
+    for (int i = 0; i < m_length; i++) {
+      if (state->numTexCoords < i)
+        glTexCoord2f(state->texCoords[state->numTexCoords - 1].s,
+                     state->texCoords[state->numTexCoords - 1].t);
       else
-	{
-	  glBegin(m_drawType);
-	  for(int n=0; n < m_length; n++)
-	    {
-	      glVertex3fv(m_vert[(n+m_position)%m_length]);
-	    }
-	  glEnd();
-	}
+        glTexCoord2f(state->texCoords[i].s,
+                     state->texCoords[i].t);
+      glVertex3fv(m_vert[i]);
     }
+    glEnd();
+  } else {
+    if (state->texture) {
+      // texture present (but no texcoords)
+      // generate texcoords automatically from x/y coords
+      float maxVal[2];
+      maxVal[0] = maxVal[1] = 0.f;
+
+      if(maxVal[0]<0)maxVal[0]*=-1.f;
+      if(maxVal[1]<0)maxVal[1]*=-1.f;
+
+      for (int i = 0; i < m_length; i++) {
+        for (int j = 0; j < 2; j++) {
+          if (m_vert[i][j] < 0) {
+            if (-m_vert[i][j] > maxVal[j])
+              maxVal[j] = -m_vert[i][j];
+          } else {
+            if (m_vert[i][j] > maxVal[j])
+              maxVal[j] = m_vert[i][j];
+          }
+        }
+      }
+      glBegin(m_drawType);
+      for(int n=0; n < m_length; n++) {
+        glTexCoord2f(m_vert[n][0] / maxVal[0],
+                     m_vert[n][1] / maxVal[1]);
+        glVertex3fv(m_vert[n]);
+      }
+      glEnd();
+    } else {
+      // activate and specify pointer to vertex array
+      glEnableClientState(GL_VERTEX_ARRAY);
+      glVertexPointer(3, GL_FLOAT, 0, m_vertarray+3*m_position);
+
+      // draw a cube
+      glDrawArrays(m_drawType, 0, m_length);
+
+      // deactivate vertex arrays after drawing
+      glDisableClientState(GL_VERTEX_ARRAY);
+    }
+  }
   glLineWidth(1.0);
 }
+
+void scopeXYZ :: bangMess(void){
+  int i;
+  t_float*vertL=m_vertarray;
+  t_float*vertR=m_vertarray+m_realLength;
+
+  post("x\ty\tz\t\tx\ty\tz");
+  for(i=0; i<m_realLength; i++) {
+    post("%f\t%f\t%f\t\t%f\t%f\t%f",
+         vertL[i*3+0],
+         vertL[i*3+1],
+         vertL[i*3+2],
+
+         vertR[i*3+0],
+         vertR[i*3+1],
+         vertR[i*3+2]);
+  }
+}
+
+
 
 /////////////////////////////////////////////////////////
 // linewidthMess
@@ -160,8 +201,8 @@ void scopeXYZ :: renderShape(GemState *state)
 /////////////////////////////////////////////////////////
 void scopeXYZ :: linewidthMess(float linewidth)
 {
-    m_linewidth = (linewidth < 0.0f) ? 0.0f : linewidth;
-    setModified();
+  m_linewidth = (linewidth < 0.0f) ? 0.0f : linewidth;
+  setModified();
 }
 
 /////////////////////////////////////////////////////////
@@ -185,14 +226,14 @@ void scopeXYZ :: typeMess(t_symbol *type)
       char c2=s[4];
       switch(c2){
       case 's':
-	if(s[5])
-	  m_drawType = GL_LINE_STRIP;
-	else
-	  m_drawType = GL_LINES;
-	break;
+        if(s[5])
+          m_drawType = GL_LINE_STRIP;
+        else
+          m_drawType = GL_LINES;
+        break;
       default:
-	m_drawType = GL_LINE_LOOP;
-	break;
+        m_drawType = GL_LINE_LOOP;
+        break;
       }
     }
     break;
@@ -201,14 +242,14 @@ void scopeXYZ :: typeMess(t_symbol *type)
       char c2=s[3];
       switch(c2){
       case 's':
-	m_drawType = GL_TRIANGLE_STRIP;
-	break;
+        m_drawType = GL_TRIANGLE_STRIP;
+        break;
       case 'f':
-	m_drawType = GL_TRIANGLE_FAN;
-	break;
+        m_drawType = GL_TRIANGLE_FAN;
+        break;
       default:
-	m_drawType = GL_TRIANGLES;
-	break;
+        m_drawType = GL_TRIANGLES;
+        break;
       }
     }
     break;
@@ -217,11 +258,11 @@ void scopeXYZ :: typeMess(t_symbol *type)
       char c2=s[4];
       switch(c2){
       case 's':
-	m_drawType = GL_QUAD_STRIP;
-	break;
+        m_drawType = GL_QUAD_STRIP;
+        break;
       default:
-	m_drawType = GL_QUADS;
-	break;
+        m_drawType = GL_QUADS;
+        break;
       }
     }
     break;
@@ -239,17 +280,24 @@ void scopeXYZ :: typeMess(t_symbol *type)
 void scopeXYZ :: obj_setupCallback(t_class *classPtr)
 {
   class_addcreator(reinterpret_cast<t_newmethod>(create_scopeXYZ), 
-		   gensym("scopeXYZ~"), A_DEFFLOAT, A_NULL);
+                   gensym("scopeXYZ~"), A_DEFFLOAT, A_NULL);
 
   class_addmethod(classPtr, reinterpret_cast<t_method>(&scopeXYZ::linewidthMessCallback),
-    	    gensym("linewidth"), A_FLOAT, A_NULL);
+                  gensym("linewidth"), A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&scopeXYZ::lengthMessCallback),
-    	    gensym("length"), A_FLOAT, A_NULL);
+                  gensym("length"), A_FLOAT, A_NULL);
 
   class_addmethod(classPtr, reinterpret_cast<t_method>(&scopeXYZ::dspCallback), 
-		    gensym("dsp"), A_NULL);
+                  gensym("dsp"), A_NULL);
   class_addmethod(classPtr, nullfn, gensym("signal"), A_NULL);
+
+  class_addbang(classPtr, reinterpret_cast<t_method>(&scopeXYZ::bangCallback));
 }
+void scopeXYZ :: bangCallback(void *data)
+{
+  GetMyClass(data)->bangMess();
+}
+
 void scopeXYZ :: linewidthMessCallback(void *data, t_floatarg linewidth)
 {
   GetMyClass(data)->linewidthMess(linewidth);
@@ -260,49 +308,59 @@ void scopeXYZ :: lengthMessCallback(void *data, t_floatarg l)
 }
 void scopeXYZ ::  dspCallback(void *data,t_signal** sp)
 {
-  if(GetMyClass(data)->m_length==0)GetMyClass(data)->lengthMess(sp[1]->s_n);
+  GetMyClass(data)->doLengthMess(sp[1]->s_n);
   dsp_add(perform, 5, data, sp[1]->s_vec, sp[2]->s_vec, sp[3]->s_vec, sp[1]->s_n);
+}
+
+void scopeXYZ :: perform(t_int count, t_sample*X, t_sample*Y, t_sample*Z)
+{
+  int length=m_length;
+  int position=m_position;
+
+  t_float*vert=m_vertarray+3*position;
+  int i=0;
+  
+  /* fill in the left-side of the double-array */
+  for(i=0; i<count; i++) {
+    *vert++=*X++;
+    *vert++=*Y++;
+    *vert++=*Z++;
+  }
+  /* copy the data over to the right-side of the double-array */
+  if((m_position+count)>m_realLength) {
+    // wrap around!
+    // 1st copy the lower half
+    memcpy(m_vertarray+3*(position+m_realLength),
+           m_vertarray+3*(position),
+           3*(m_realLength-m_position)*sizeof(t_sample));
+    // then the upper half
+    memcpy(m_vertarray,
+           m_vertarray+3*(m_realLength),
+           3*(m_position+count-m_realLength)*sizeof(t_sample));
+
+  } else {
+    // ordinary copy
+  
+    memcpy(m_vertarray+3*(position+m_realLength),
+           m_vertarray+3*(position),
+           3*count*sizeof(t_sample));
+  }
+  m_position=(m_position+count)%m_realLength;
+
+  setModified();
 }
 
 t_int* scopeXYZ :: perform(t_int* w)
 {
   int index=1;
   scopeXYZ *x = GetMyClass(reinterpret_cast<void*>(w[index++]));
-  t_float* in_X = reinterpret_cast<t_float*>(w[index++]);
-  t_float* in_Y = reinterpret_cast<t_float*>(w[index++]);
-  t_float* in_Z = reinterpret_cast<t_float*>(w[index++]);
+  t_sample* in_X = reinterpret_cast<t_sample*>(w[index++]);
+  t_sample* in_Y = reinterpret_cast<t_sample*>(w[index++]);
+  t_sample* in_Z = reinterpret_cast<t_sample*>(w[index++]);
 
   t_int n = (w[index++]);
 
-  int length=x->m_length;
-  int position=0;
+  x->perform(n, in_X, in_Y, in_Z);
 
-  if (length<n){
-    n=length;
-  } else 
-    position=(x->m_position)%length;
-
-  if(position<0)position=0;
-
-  t_float*vert=x->m_vert[position];
-
-  x->m_position=position+n;
-
-  if(x->m_position>length){
-    // we have a wrap inside !
-    for(int i=0; i<n; i++){
-      x->m_vert[i%length][0]=*in_X++;
-      x->m_vert[i%length][1]=*in_Y++;
-      x->m_vert[i%length][2]=*in_Z++;
-    }
-  } else {
-    while(n--){
-      *vert++=*in_X++;
-      *vert++=*in_Y++;
-      *vert++=*in_Z++;
-    }
-  }    
-
-  x->setModified();
   return (w+index);
 }
--- src/Geos/scopeXYZ.h
+++ src/Geos/scopeXYZ.h
@@ -1,16 +1,16 @@
 /*-----------------------------------------------------------------
-LOG
-    GEM - Graphics Environment for Multimedia
+  LOG
+  GEM - Graphics Environment for Multimedia
 
-    A 3D-scope
+  A 3D-scope
 
-    Copyright (c) 1997-2000 Mark Danks. mark@danks.org
-    Copyright (c) Günther Geiger. geiger@epy.co.at
-    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
-    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+  Copyright (c) 1997-2000 Mark Danks. mark@danks.org
+  Copyright (c) Günther Geiger. geiger@epy.co.at
+  Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
------------------------------------------------------------------*/
+  -----------------------------------------------------------------*/
 
 #ifndef INCLUDE_SCOPEXYZ_H_
 #define INCLUDE_SCOPEXYZ_H_
@@ -18,82 +18,89 @@ LOG
 #include "Base/GemShape.h"
 
 /*-----------------------------------------------------------------
--------------------------------------------------------------------
-CLASS
-    scopeXYZ
+  -------------------------------------------------------------------
+  CLASS
+  scopeXYZ
     
-    Creates a scopeXYZ
+  Creates a scopeXYZ
 
-KEYWORDS
-    geo
+  KEYWORDS
+  geo
     
-DESCRIPTION
+  DESCRIPTION
 
-    Inlet~ for signal X
-    Inlet~ for signal Y
-    Inlet~ for signal Z
+  Inlet~ for signal X
+  Inlet~ for signal Y
+  Inlet~ for signal Z
      
------------------------------------------------------------------*/
+  -----------------------------------------------------------------*/
 class GEM_EXTERN scopeXYZ : public GemShape
 {
-    CPPEXTERN_HEADER(scopeXYZ, GemShape)
+  CPPEXTERN_HEADER(scopeXYZ, GemShape)
 
     public:
 
-        //////////
-        // Constructor
-    	scopeXYZ(t_floatarg len=0);
+  //////////
+  // Constructor
+  scopeXYZ(t_floatarg len=0);
     	
-    protected:
+ protected:
     	
-    	//////////
-    	// Destructor
-    	virtual ~scopeXYZ();
+  //////////
+  // Destructor
+  virtual ~scopeXYZ();
 
-    	//////////
-    	// Do the renderShapeing
-    	virtual void 	renderShape(GemState *state);
+  //////////
+  // Do the renderShapeing
+  virtual void 	renderShape(GemState *state);
 
-    	//////////
-    	// The width of the lines in line draw mode
-    	void	    	linewidthMess(float linewidth);
+  //////////
+  // The width of the lines in line draw mode
+  void	    	linewidthMess(float linewidth);
     	
-    	//////////
-    	// How the object should be drawn
-    	virtual void	typeMess(t_symbol *type);
+  //////////
+  // How the object should be drawn
+  virtual void	typeMess(t_symbol *type);
 
 	//-----------------------------------
 	// GROUP:	Member variables
 	//-----------------------------------
         
-		//////////
-        // The drawing style (GL_LINE, GL_SCOPEXYZ, etc)
-        GLenum	    	m_drawType;
+  //////////
+  // The drawing style (GL_LINE, GL_SCOPEXYZ, etc)
+  GLenum	    	m_drawType;
 		
-    	//////////
-    	// the number of points
-    	int 	    	m_length;
-    	virtual void	lengthMess(int length);
+  //////////
+  // the number of points
+  unsigned int 	    	m_length;
+  unsigned int        m_realLength;
+  virtual void	doLengthMess(int length=0);
+  virtual void	lengthMess(int length);
 
-		int m_position; 
+  virtual void	bangMess();
+
+  int m_position; 
     	
-    	//////////
-    	// The vertices
-		float  *m_vertarray;
-    	float **m_vert;
+  //////////
+  // The vertices
+  float  *m_vertarray;
+  float **m_vert;
 
-	t_inlet*m_inX, *m_inY, *m_inZ;
+  t_inlet*m_inX, *m_inY, *m_inZ;
 
 
-    private:
+  void perform(t_int count, t_sample*X, t_sample*Y, t_sample*Z);
+
+ private:
     	    
-    	//////////
-    	// Static member functions
-    	static void 	linewidthMessCallback(void *data, t_floatarg linewidth);
-    	static void 	lengthMessCallback(void *data, t_floatarg linewidth);
+  //////////
+  // Static member functions
+  static void 	linewidthMessCallback(void *data, t_floatarg linewidth);
+  static void 	lengthMessCallback(void *data, t_floatarg linewidth);
+  static void 	bangCallback(void *data);
 
-    	static void 	dspCallback(void *data, t_signal** sp);
-	static t_int*   perform(t_int* w);
+  static void 	dspCallback(void *data, t_signal** sp);
+  static t_int*   perform(t_int* w);
 
 };
 
