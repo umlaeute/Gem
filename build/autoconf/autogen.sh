bb27b6049ac146a26be0553993075989453a0e8b
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2010-07-13 07:23:47 +0000

    "force install verbose" autoreconf
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3431 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- build/autoconf/autogen.sh
+++ build/autoconf/autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-autoreconf || exit -1
+autoreconf -fiv || exit -1
 
 echo "if everything above succeeded, you can now run './configure'"
 echo "for options see './configure --help=recursive'"
