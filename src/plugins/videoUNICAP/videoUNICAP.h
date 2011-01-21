2c610ee3e962b3edd61254590984e145a2322e4b
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-01-21 16:53:18 +0000

    stripped everythig off
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3628 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/plugins/videoUNICAP/videoUNICAP.h
+++ src/plugins/videoUNICAP/videoUNICAP.h
@@ -17,68 +17,24 @@ WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
 #include "plugins/video.h"
 
-#if defined HAVE_LIBUNICAP && !defined HAVE_VIDEO4LINUX2
-# define HAVE_VIDEO4LINUX2
+#if defined HAVE_LIBUNICAP && !defined HAVE_UNICAP
+# define HAVE_UNICAP
 #endif
 
 
-#ifdef HAVE_VIDEO4LINUX2
-# ifdef HAVE_LIBUNICAP
-#  include <libv4l2.h> 
-# endif /* HAVE_LIBUNICAP */
-
-# include <stdio.h>
-# include <stdlib.h>
-//# include <stdarg.h>
-# include <unistd.h>
-# include <string.h>
-//# include <ctype.h>
-# include <fcntl.h>
-# include <errno.h>
-# include <sys/ioctl.h>
-//# include <sys/types.h>
-//# include <sys/time.h>
-# include <asm/types.h>
-# include <linux/videodev2.h>
-# include <sys/mman.h>
-#ifdef HAVE_PTHREADS
-/* the bad thing is, that we currently don't have any alternative to using PTHREADS 
- * LATER: make threading optional
- *        (or at least disabled capturing when no pthreads are available)
- */
-# include <pthread.h>
-#endif
-# define UNICAP_DEVICENO 0
-/* request 4 buffers (but if less are available, it's fine too... */
-# define UNICAP_NBUF 4
-
-
-struct t_v4l2_buffer {
-  void *                  start;
-  size_t                  length;
-};
-
-
+#ifdef HAVE_UNICAP
+# include <unicap.h>
 #endif
 /*-----------------------------------------------------------------
   -------------------------------------------------------------------
   CLASS
   pix_video
     
-  Loads in a video
+  grabs a video via UNICAP (http://unicap-imaging.org/)
     
   KEYWORDS
   pix
-    
-  DESCRIPTION
 
-  "dimen" (int, int) - set the x,y dimensions
-  "zoom" (int, int) - the zoom factor (1.0 is nominal) (num / denom)
-  "bright" (int) - the brightnes
-  "contrast" (int) - the contrast
-  "hue" (int) - the hue
-  "sat" (int) - the saturation
-    
   -----------------------------------------------------------------*/
 namespace gem { class GEM_EXPORT videoUNICAP : public video {
  public:
@@ -90,7 +46,7 @@ namespace gem { class GEM_EXPORT videoUNICAP : public video {
   // Destructor
   virtual ~videoUNICAP();
   
-#ifdef HAVE_VIDEO4LINUX2
+#ifdef HAVE_UNICAP
   ////////
   // open the video-device
   virtual bool           openDevice(gem::Properties&writeprops);
@@ -126,59 +82,6 @@ namespace gem { class GEM_EXPORT videoUNICAP : public video {
   virtual void getProperties(gem::Properties&readprops);
 
  protected:
-
-  //-----------------------------------
-  // GROUP:	Linux specific video data
-  //-----------------------------------
-	
-
-  int m_gotFormat; // the format returned by the v4l2-device (not an openGL-format!)
-  bool m_colorConvert; // do we have to convert the colour-space manually ?
-
-
-  int m_tvfd;
-
-  struct t_v4l2_buffer*m_buffers;
-  int  m_nbuffers;
-  void*m_currentBuffer;
-
-  int m_frame, m_last_frame;
-
-  //////////////////
-  // capabilities of the device
-  int m_maxwidth;
-  int m_minwidth;
-  int m_maxheight;
-  int m_minheight;
-  
-  //////////
-  // the capturing thread
-  pthread_t m_thread_id;
-  bool      m_continue_thread;
-  bool      m_frame_ready;
-
-  /* capture frames (in a separate thread! */
-  void*capturing(void); 
-  /* static callback for pthread_create: calls capturing() */
-  static void*capturing_(void*);
-
-  int       init_mmap(void);
-
-  // rendering might be needed when we are currently not capturing because we cannot (e.g: couldn't open device)
-  // although we should. when reopening another device, we might be able to render again...
-  // example: we have only 1 video-device /dev/video0;
-  // when we try to open /dev/video1 we fail, and m_capturing is set to 0
-  // now when rendering is turned on and we want to switch back to /dev/video0 we should reconnect to the good device
-  bool      m_rendering; // "true" when rendering is on, false otherwise
-
-  /* use this in the capture-thread to cleanup */
-  bool      m_stopTransfer;  
-
-  /* internal housekeeping of properties */
-  void addProperties(struct v4l2_queryctrl queryctrl,
-		     gem::Properties&readable,
-		     gem::Properties&writeable);
-  std::map<std::string, struct v4l2_queryctrl>m_readprops, m_writeprops;
 
 #endif /* HAVE_VIDEO4LINUX2 */
 
