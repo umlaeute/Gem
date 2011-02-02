1f5909ae43ffaf1304e3633c9d5c52b34e0d1e30
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-02-02 17:46:53 +0000

    another try
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3685 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Output/gemcocoawindow.h
+++ src/Output/gemcocoawindow.h
@@ -13,8 +13,8 @@
 #ifndef INCLUDE_GEMCOCOAWINDOW_H_
 #define INCLUDE_GEMCOCOAWINDOW_H_
 
+#import <Cocoa/Cocoa.h>
 #include "Base/GemContext.h"
-
 
 /*-----------------------------------------------------------------
   -------------------------------------------------------------------
@@ -51,6 +51,13 @@
 
   -----------------------------------------------------------------*/
 
+@interface GemCocoaWindow : NSOpenGLView
+{
+  gemcocoawindow*gemwin;
+}
+@end
+
+
 class GEM_EXTERN gemcocoawindow : public GemContext
 {
   CPPEXTERN_HEADER(gemcocoawindow, GemContext)
@@ -59,52 +66,94 @@ class GEM_EXTERN gemcocoawindow : public GemContext
 
   //////////
   // Constructor
-  gemcocoawindow();
+  gemcocoawindow(void);
 
   //////////
   // Destructor
-  virtual         ~gemcocoawindow();
+  virtual ~gemcocoawindow(void);
 
-  virtual void bang(void);
+
+  // create window
+  virtual bool create(void);
+
+  // destroy window
+  virtual void destroy(void);
+
+  // check whether we have a window and if so, make it current
+  virtual bool makeCurrent(void);
+
+  void doRender(void);
+
+  /* rendering */
+  void renderMess(void);
+
+  /* dispatch window events */
+  void dispatch(void);
+
+  /* render context (pre creation) */
+  void  bufferMess(int buf);
+  int         m_buffer;
+  void    fsaaMess(int value);
+  int         m_fsaa;
+
+  /* window decoration (pre creation) */
+  void titleMess(t_symbol* s);
+  std::string     m_title;
+  void borderMess(bool on);
+  bool       m_border;
+
+  /* window position/dimension (pre creation) */
+  virtual void    dimensionsMess(int width, int height);
+  unsigned int         m_width, m_height;
+
+  void    fullscreenMess(bool on);
+  bool              m_fullscreen;
+  void        offsetMess(int x, int y);
+  unsigned int      m_xoffset, m_yoffset;
+
+  /* creation/destruction */
+  void        createMess(void);
+  void       destroyMess(void);
+
+  /* post creation */
+  void        cursorMess(bool on);
+  bool              m_cursor;
 
  private:
 
+  GemCocoaWindow*m_win;
+
   //////////
   // Static member functions (rendering)
-  static void     bangMessCallback(void *data);
+  static void     renderMessCallback(void *data);
 
   //////////
   // Static member functions (window pre-creation)
   static void     bufferMessCallback(void *data, t_floatarg buf);
   static void     fsaaMessCallback(void *data,t_floatarg val);
-
   static void     titleMessCallback(void *data, t_symbol* s);
-
   static void     dimensionsMessCallback(void *data, t_floatarg width, t_floatarg height);
   static void     offsetMessCallback(void *data, t_floatarg x, t_floatarg y);
   static void     fullscreenMessCallback(void *data, t_floatarg on);
-  static void     secondscreenMessCallback(void *data, t_floatarg on);
-
   static void     borderMessCallback(void *, t_floatarg state);
-
 
   //////////
   // Static member functions (window creation)
-  static void     createMessCallback(void *data, t_symbol* s);
+  static void     createMessCallback(void *);
   static void     destroyMessCallback(void *);
 
   //////////
   // Static member functions (window post-creation)
-  static void     cursorMessCallback(void *data, t_floatarg val);
-  static void     topmostMessCallback(void *data, t_floatarg val);
-  static void     menuBarMessCallback(void *data, t_floatarg on);
+  static void     cursorMessCallback(void *, t_floatarg);
 
   //////////
   // Static member functions (misc)
   static void     printMessCallback(void *);
 
-  class PIMPL;
-  PIMPL*m_pimpl;
+  t_clock*m_clock;
+  int m_polltime;
+  static void clockCallback(void*);
+  void clock(void);
 };
 
 #endif    // for header file
--- src/Output/gemcocoawindow.mm
+++ src/Output/gemcocoawindow.mm
@@ -6,43 +6,48 @@
 //
 // Implementation file
 //
-//    Copyright (c) 1997-2000 Mark Danks.
-//    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+//    Copyright (c) 2011 IOhannes m zmoelnig. forum::für::umläute. IEM
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 //
 /////////////////////////////////////////////////////////
-#ifdef __APPLE__
+
 #include "gemcocoawindow.h"
 
 #include "Base/GemGL.h"
-#import <Cocoa/Cocoa.h>
+#include <stdio.h>
 
-/*
- * http://www.cocoabuilder.com/archive/cocoa/221350-opengl-renderloop-in-cocoa.html
- */
-
-@interface GemView : NSOpenGLView
-{
-  gemcocoawindow*gemwin;
-}
-@end
 
 @implementation GemView 
 - (id)initWithFrame:(NSRect)frameRect parent:(gemcocoawindow*)gw
 {
   if(NULL==gw)return NULL;
   gemwin=gw;
+
+  std::vector<NSOpenGLPixelFormatAttribute>attr;
+#if 0
   NSOpenGLPixelFormatAttribute attr[] = 
     {
-      (2==gemwin->m_buffer)?NSOpenGLPFADoubleBuffer:NSOpenGLPFASingleBuffer,
+      NSOpenGLPFADoubleBuffer,
       NSOpenGLPFAAccelerated,
       NSOpenGLPFAColorSize, (NSOpenGLPixelFormatAttribute) 32,
       NSOpenGLPFADepthSize, (NSOpenGLPixelFormatAttribute) 23,
       (NSOpenGLPixelFormatAttribute) 0
     };
-  NSOpenGLPixelFormat *nsglFormat = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attr] autorelease];
+#endif
+
+  if(gemwin->m_buffer==2)
+    attr.push_back(NSOpenGLPFADoubleBuffer);
+
+  attr.push_back(NSOpenGLPFAAccelerated);
+
+  attr.push_back(NSOpenGLPFAColorSize);
+  attr.push_back(static_cast<NSOpenGLPixelFormatAttribute>(32));
+
+  attr.push_back(NSOpenGLPFADepthSize);
+  attr.push_back(static_cast<NSOpenGLPixelFormatAttribute>(23));
+
+  NSOpenGLPixelFormat *nsglFormat = [[[NSOpenGLPixelFormat alloc] initWithAttributes:attr.data()] autorelease];
   
   self = [super initWithFrame:frameRect pixelFormat:nsglFormat];
   return self;
@@ -61,7 +66,7 @@
 
 - (void)drawRect:(NSRect)rect
 {
-  gemwin->bang();
+  gemwin->doRender();
 }
 
 
@@ -80,60 +85,82 @@
 
 
 
-struct gemcocoawindow::PIMPL {
-  GemView*view;
-  gemcocoawindow*gcw;
-#define error gcw->error
-
-  int         m_buffer;
-  int         m_fsaa;
-  std::string m_title;
-  bool        m_border;
-  unsigned int      m_width, m_height;
-  bool              m_fullscreen;
-  int      m_xoffset, m_yoffset;
-  bool              m_secondscreen;
 
 
-  PIMPL(gemcocoawindow*gcw_) : 
-	view(NULL),
-	gcw(gcw_),
-	m_buffer(2),
-	m_fsaa(0),
-	m_title("Gem"),
-	m_border(true),
-	m_width(500), m_height(500),
-	m_xoffset(0), m_yoffset(0),
-	m_secondscreen(true)
-  {
-  }
 
-  ~PIMPL(void) {
-    destroy();
-    gcw=NULL;
-  }
-  void destroy(void) {
-    if(view) [view dealloc];
-    view=NULL;
-  }
-  bool create(void) {
-    if(view) return false;
-    view = [[[GemView alloc] initWithFrame:[contentView bounds]] gw:gwc];
-    return (NULL!=view);
-  }
+
+
+
+
+CPPEXTERN_NEW(gemcocoawindow)
+
+/////////////////////////////////////////////////////////
+//
+// gemcocoawindow
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+gemcocoawindow :: gemcocoawindow(void) :
+  m_buffer(2),
+  m_fsaa(0),
+  m_title("GEM"),
+  m_border(false),
+  m_fullscreen(false),
+  m_xoffset(-1), m_yoffset(-1),
+  m_cursor(false),
+{
+  m_width =500;
+  m_height=500;
+
+}
+
+/////////////////////////////////////////////////////////
+// Destructor
+//
+/////////////////////////////////////////////////////////
+gemcocoawindow :: ~gemcocoawindow()
+{
+  destroyMess();
+}
+
+
+bool gemcocoawindow :: makeCurrent(void){
+
+
+  return(false);
+}
+
+/////////////////////////////////////////////////////////
+// renderMess
+//
+/////////////////////////////////////////////////////////
+void gemcocoawindow :: doRender()
+{
+  bang();
+}
+void gemcocoawindow :: renderMess()
+{
+  bang();
+}
 
 /////////////////////////////////////////////////////////
 // bufferMess
 //
 /////////////////////////////////////////////////////////
-void bufferMess(int buf)
+void gemcocoawindow :: bufferMess(int buf)
 {
   switch(buf) {
   case 1: case 2:
     m_buffer=buf;
+    if(m_window) {
+      post("changing buffer type will only effect newly created windows");
+    }
     break;
   default:
-    error("can only handle single (1) or double (2) buffer");
+    error("buffer can only be '1' (single) or '2' (double) buffered");
+    break;
   }
 }
 
@@ -141,24 +168,24 @@ void bufferMess(int buf)
 // fsaaMess
 //
 /////////////////////////////////////////////////////////
-void fsaaMess(int value)
+void gemcocoawindow :: fsaaMess(int value)
 {
-  m_fsaa=value;
 }
 
 /////////////////////////////////////////////////////////
 // titleMess
 //
 /////////////////////////////////////////////////////////
-void titleMess(std::string s)
+void gemcocoawindow :: titleMess(t_symbol* s)
 {
-  m_title = s
+  m_title = s->s_name;
+
 }
 /////////////////////////////////////////////////////////
 // border
 //
 /////////////////////////////////////////////////////////
-void borderMess(bool setting)
+void gemcocoawindow :: borderMess(bool setting)
 {
   m_border=setting;
 }
@@ -166,7 +193,7 @@ void borderMess(bool setting)
 // dimensionsMess
 //
 /////////////////////////////////////////////////////////
-void dimensionsMess(int width, int height)
+void gemcocoawindow :: dimensionsMess(int width, int height)
 {
   if (width <= 0) {
     error("width must be greater than 0");
@@ -184,7 +211,7 @@ void dimensionsMess(int width, int height)
 // fullscreenMess
 //
 /////////////////////////////////////////////////////////
-void fullscreenMess(bool on)
+void gemcocoawindow :: fullscreenMess(bool on)
 {
   m_fullscreen = on;
 }
@@ -193,203 +220,123 @@ void fullscreenMess(bool on)
 // offsetMess
 //
 /////////////////////////////////////////////////////////
-void offsetMess(int x, int y)
+void gemcocoawindow :: offsetMess(int x, int y)
 {
   m_xoffset = x;
   m_yoffset = y;
-}
 
-/////////////////////////////////////////////////////////
-// secondScreen
-//
-/////////////////////////////////////////////////////////
-void secondscreenMess(bool on)
-{
-  m_secondscreen = on;
 }
 
 /////////////////////////////////////////////////////////
 // createMess
 //
 /////////////////////////////////////////////////////////
-void createMess(std::string disp)
+void gemcocoawindow :: createMess(void)
 {
+
+
 }
 /////////////////////////////////////////////////////////
 // destroy window
 //
 /////////////////////////////////////////////////////////
-void destroyMess(void)
+void gemcocoawindow :: destroy(void)
 {
+
 }
+void gemcocoawindow :: destroyMess(void)
+{
+
+  destroy();
+}
+
+
 /////////////////////////////////////////////////////////
 // cursorMess
 //
 /////////////////////////////////////////////////////////
-void cursorMess(bool setting)
+void gemcocoawindow :: cursorMess(bool setting)
 {
-}
-
-/////////////////////////////////////////////////////////
-// topmostMess
-//
-/////////////////////////////////////////////////////////
-void topmostMess(bool topmost)
-{
-}
-
-
-/////////////////////////////////////////////////////////
-// menuBarMess		1 = show	-1 = hide, but autoshow
-//					0 = hide, but neverShow
-/////////////////////////////////////////////////////////
-void menuBarMess(int on)
-{
-  /* LATER move this into the OS-specific part of GemWin */
-#ifdef __APPLE__
-  if (on == 0) {
-    SetSystemUIMode( kUIModeAllHidden, kUIOptionDisableAppleMenu |
-									   kUIOptionDisableProcessSwitch |
-									   kUIOptionDisableSessionTerminate |
-									   kUIOptionDisableForceQuit );
-  }else if (on > 0) {
-    SetSystemUIMode( kUIModeNormal, 0 );
-  }else if (on < 0) {
-    SetSystemUIMode( kUIModeAllHidden, kUIOptionAutoShowMenuBar );
-  }
-#endif
-}
-
+  m_cursor=setting;
 
 }
-
- 
-CPPEXTERN_NEW(gemcocoawindow)
-
-/////////////////////////////////////////////////////////
-//
-// gemcocoawindow
-//
-/////////////////////////////////////////////////////////
-// Constructor
-//
-/////////////////////////////////////////////////////////
-gemcocoawindow :: gemcocoawindow(void) :
-  m_pimpl(new PIMPL(this))
-{
-}
-
-/////////////////////////////////////////////////////////
-// Destructor
-//
-/////////////////////////////////////////////////////////
-gemcocoawindow :: ~gemcocoawindow()
-{
-  destroyMess();
-}
-
-
-
-bool gemcocoawindow :: makeCurrent(void){
-  if(NULL==m_pimpl->view)
-    return false;
-  return GemContext::makeCurrent();
-}
-
-
-/////////////////////////////////////////////////////////
-// bangMess
-//
-/////////////////////////////////////////////////////////
-void gemcocoawindow :: bang()
-{
-  GemContext::bang();
-}
-
-
-
 /////////////////////////////////////////////////////////
 // static member function
 //
 /////////////////////////////////////////////////////////
 void gemcocoawindow :: obj_setupCallback(t_class *classPtr)
 {
-  class_addbang(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::bangMessCallback));
+  int argc=0;
+  char*argv=NULL;
 
+  static bool firsttime=true;
+
+  if(firsttime) {
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500,500);
+    glutInit(&argc,&argv);
+  }
+  firsttime=false;
+
+  class_addbang(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::renderMessCallback));
+  
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::titleMessCallback),        gensym("title"), A_DEFSYM ,A_NULL);
-  class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::createMessCallback),       gensym("create"), A_DEFSYM ,A_NULL);
+  class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::createMessCallback),       gensym("create") ,A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::bufferMessCallback),       gensym("buffer"), A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::fullscreenMessCallback),   gensym("fullscreen"), A_FLOAT, A_NULL);
-  class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::menuBarMessCallback),      gensym("menubar"), A_FLOAT, A_NULL);
-  class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::secondscreenMessCallback), gensym("secondscreen"), A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::dimensionsMessCallback),   gensym("dimen"), A_FLOAT, A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::offsetMessCallback),       gensym("offset"), A_FLOAT, A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::cursorMessCallback),       gensym("cursor"), A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::destroyMessCallback),      gensym("destroy"), A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::printMessCallback),        gensym("print"), A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::borderMessCallback),       gensym("border"), A_FLOAT, A_NULL);
-  class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::topmostMessCallback),      gensym("topmost"), A_FLOAT, A_NULL);
   class_addmethod(classPtr, reinterpret_cast<t_method>(&gemcocoawindow::fsaaMessCallback),         gensym("FSAA"), A_FLOAT, A_NULL);
 }
-#define CALLPIMPL(data) if(NULL!=GetMyClass(data)->m_pimpl)GetMyClass(data)->m_pimpl
 void gemcocoawindow :: printMessCallback(void *)
 {
+  //  GemMan::printInfo();
 }
 void gemcocoawindow :: borderMessCallback(void *data, t_floatarg state)
 {
-  CALLPIMPL(data)->borderMess((int)state);
+  GetMyClass(data)->borderMess(static_cast<int>(state));
 }
 void gemcocoawindow :: destroyMessCallback(void *data)
 {
-  CALLPIMPL(data)->destroyMess();
+  GetMyClass(data)->destroyMess();
 }
-void gemcocoawindow :: bangMessCallback(void *data)
+void gemcocoawindow :: renderMessCallback(void *data)
 {
-  GetMyClass(data)->bangMess();
+  GetMyClass(data)->renderMess();
 }
 void gemcocoawindow :: titleMessCallback(void *data, t_symbol* disp)
 {
-  CALLPIMPL(data)->titleMess(disp->s_name);
+  GetMyClass(data)->titleMess(disp);
 }
-void gemcocoawindow :: createMessCallback(void *data, t_symbol* disp)
+void gemcocoawindow :: createMessCallback(void *data)
 {
-  CALLPIMPL(data)->createMess(disp->s_name);
+  GetMyClass(data)->createMess();
 }
 void gemcocoawindow :: bufferMessCallback(void *data, t_floatarg buf)
 {
-  CALLPIMPL(data)->bufferMess(static_cast<int>(buf));
+  GetMyClass(data)->bufferMess(static_cast<int>(buf));
 }
 void gemcocoawindow :: fullscreenMessCallback(void *data, t_floatarg on)
 {
-  CALLPIMPL(data)->fullscreenMess(static_cast<int>(on));
-}
-void gemcocoawindow :: menuBarMessCallback(void *data, t_floatarg on)
-{
-  CALLPIMPL(data)->menuBarMess(static_cast<int>(on));
-}
-void gemcocoawindow :: secondscreenMessCallback(void *data, t_floatarg on)
-{
-  CALLPIMPL(data)->secondscreenMess(static_cast<int>(on));
+  GetMyClass(data)->fullscreenMess(static_cast<int>(on));
 }
 void gemcocoawindow :: dimensionsMessCallback(void *data, t_floatarg width, t_floatarg height)
 {
-  CALLPIMPL(data)->dimensionsMess(static_cast<int>(width), static_cast<int>(height));
+  GetMyClass(data)->dimensionsMess(static_cast<int>(width), static_cast<int>(height));
 }
 void gemcocoawindow :: offsetMessCallback(void *data, t_floatarg x, t_floatarg y)
 {
-  CALLPIMPL(data)->offsetMess(static_cast<int>(x), static_cast<int>(y));
+  GetMyClass(data)->offsetMess(static_cast<int>(x), static_cast<int>(y));
 }
 void gemcocoawindow :: cursorMessCallback(void *data, t_floatarg val)
 {
-  CALLPIMPL(data)->cursorMess(val);
-}
-void gemcocoawindow :: topmostMessCallback(void *data, t_floatarg val)
-{
-  CALLPIMPL(data)->topmostMess(val);
+  GetMyClass(data)->cursorMess(val);
 }
 void gemcocoawindow :: fsaaMessCallback(void *data, t_floatarg val)
 {
-  CALLPIMPL(data)->fsaaMess(static_cast<int>(val));
+  GetMyClass(data)->fsaaMess(static_cast<int>(val));
 }
-
-#endif
