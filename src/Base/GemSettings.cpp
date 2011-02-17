81acfe6e8845f2170176d0558ef0f9c569778885
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-02-17 22:11:39 +0000

    remove Pd traces from public interface; reduce Pd traces in implementation
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3794 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Base/GemSettings.cpp
+++ src/Base/GemSettings.cpp
@@ -15,6 +15,9 @@
 /////////////////////////////////////////////////////////
 
 #include "Base/GemConfig.h"
+
+#include "m_pd.h"
+
 #include "GemSettings.h"
 #include "GemFiles.h"
 
@@ -61,48 +64,42 @@ struct _gemclass
 
 
 
-class GemSettingsData {
-  friend class GemSettings;
- public:
-  GemSettingsData(void);
-  ~GemSettingsData(void);
-
- protected:
+struct GemSettings::PIMPL {
   // dictionary for setting values
-  std::map <t_symbol*, t_atom*> data;
+  std::map <std::string, t_atom> data;
 
-  virtual t_atom*get(t_symbol*name) {
-    return data[name];
+  virtual t_atom*get(std::string name) {
+    std::map<std::string, t_atom>::iterator it=data.find(name);
+    if(it==data.end())
+      return NULL;
+    return &it->second;
   }
-  virtual void set(t_symbol*name, t_atom*value) {
+  virtual void set(std::string name, t_atom*value) {
     // LATER: we should expand envvariables
     if(value) {
-      t_atom*a=(t_atom*)getbytes(sizeof(t_atom));
-      memcpy(a, value, sizeof(t_atom));
-      data[name]=a;
+      data[name]= *value;
     } else {
       data.erase(name);
     }
   }
 
-  void setEnv(t_symbol*name, const char*env);
-  void set(t_symbol*name, int i) {
+  void set(std::string name, int i) {
     t_atom a;
     SETFLOAT(&a, i);
     set(name, &a);
   }
-  void set(t_symbol*name, t_float f) {
+  void set(std::string name, t_float f) {
     t_atom a;
     SETFLOAT(&a, f);
     set(name, &a);
   }
-  void set(t_symbol*name, t_symbol*s) {
+  void set(std::string name, std::string s) {
     t_atom a;
-    SETSYMBOL(&a, s);
+    SETSYMBOL(&a, gensym(s.c_str()));
     set(name, &a);
   }
 
-  //  t_symbol*expandEnv(t_symbol*, bool bashfilename=false);
+  //  std::string expandEnv(std::string , bool bashfilename=false);
 
 
   bool open(const char*filename, const char*dirname=NULL) {
@@ -128,22 +125,22 @@ class GemSettingsData {
     int ac=binbuf_getnatom(bb);
     t_atom*av=binbuf_getvec(bb);
 
-    t_symbol*s=NULL;
+    std::string s;
     t_atom*a;
     int state=0; /* 0=(next is ID); 1=(next is value); 2=(next is ignored) */
 
     while(ac--) {
       if (av->a_type == A_SEMI) {
         // done
-        if(NULL!=s) {
+        if(!s.empty()) {
           set(s, a);
         }
         state=0;
-        s=NULL;
+        s.clear();
       } else {
         switch (state) {
         case 0:
-          s=atom_getsymbol(av);
+          s=atom_getsymbol(av)->s_name;
           state=1;
           break;
         case 1:
@@ -162,139 +159,143 @@ class GemSettingsData {
   }
 
   void print(void) {
-    std::map <t_symbol*, t_atom*>::iterator it;
+    std::map <std::string , t_atom>::iterator it;
     for(it = data.begin(); 
         it != data.end();
         it++)
       {
-        if(it->first && it->first->s_name && it->second) {
-          startpost("key ['%s']: ", it->first->s_name);
-          postatom(1, it->second);
-          endpost();
+        if(!it->first.empty()) {
+          startpost("key ['%s']: '", it->first.c_str());
+          postatom(1, &it->second);
+	  
+          post("'");
         }
       }
   }
 
+
+
+  PIMPL(void)
+  {
+    int i=0;
+#ifdef GEM_DEFAULT_FONT
+    set("font.face", GEM_DEFAULT_FONT);
+#endif
+
+    setEnv("settings.file", "GEM_SETTINGS");
+    t_atom*a=NULL;
+    a=get("settings.file");
+    if(a) {
+      std::string s=atom_getsymbol(a)->s_name;
+      open(gem::files::expandEnv(s.c_str(), true).c_str(), ".");
+    } else {
+      while(s_configdir[i]) {
+	open(GEM_SETTINGS_FILE, s_configdir[i]);
+	i++;
+      }
+      open(GEM_SETTINGS_FILE, ".");
+    }
+
+    /* legacy settings via environmental variables */
+    setEnv("texture.rectangle", "GEM_RECTANGLE_TEXTURE");
+    setEnv("singlecontext", "GEM_SINGLE_CONTEXT"); // hmm, what's a better new name for this?
+    setEnv("font.face", "GEM_DEFAULT_FONT");
+
+
+    t_gemclass *c = (t_gemclass*)class_new(gensym("Gem"), 0, 0, 0, 0, A_NULL);
+    set("gem.path", c->c_externdir->s_name);
+
+    print();
+  }
+
+  ~PIMPL(void) {
+    
+  }
+
+  void setEnv(std::string key, const std::string env) {
+    if(env.empty())return;
+    if(key.empty())return;
+
+    char*result=getenv(env.c_str());
+    if(NULL==result) {
+      return;
+    }
+
+    t_atom a;
+    errno=0;
+
+    /* try integer */
+    long l=strtol(result, NULL, 0);
+    if(0==errno) {
+      SETFLOAT(&a, l);
+      set(key, &a);
+    }
+
+    /* try float */
+    double d=strtod(result, NULL);
+    if(0==errno) {
+      SETFLOAT(&a, d);
+      set(key, &a);
+    }
+
+    /* try symbol */
+    SETSYMBOL(&a, gensym(result));
+    set(key, &a);
+
+    // we ignore lists and other complex things for now
+  }
+#if 0
+  std::string expandEnv(std::string value, bool bashfilename) {
+    if(NULL==value)
+      return NULL;
+    verbose(2, "expanding '%s'", value->s_name);
+
+    if(bashfilename) {
+      char bashBuffer[MAXPDSTRING];
+      sys_bashfilename(value->s_name, bashBuffer);
+      value=gensym(bashBuffer);
+    }
+
+#ifdef HAVE_WORDEXP_H
+    wordexp_t pwordexp;
+
+    if(0==wordexp(value->s_name, &pwordexp, 0)) {
+      pwordexp.we_offs=0;
+      if(pwordexp.we_wordc) {
+	// we only take the first match into account 
+	value=gensym(pwordexp.we_wordv[0]);
+      }
+# ifdef __APPLE__
+      /* wordfree() broken on apple: keeps deallocating non-aligned memory */
+#  warning wordfree() not called
+# else
+      wordfree(&pwordexp);
+# endif
+    }
+#endif
+#ifdef _WIN32
+    char envVarBuffer[MAXPDSTRING];
+    ExpandEnvironmentStrings(value->s_name, envVarBuffer, MAX_PATH - 2);
+    value=gensym(envVarBuffer);
+#endif
+
+    verbose(2, "expanded '%s'", value->s_name);
+    return value;
+  }
+#endif
 };
 
 
-GemSettingsData::GemSettingsData(void)
-{
-  int i=0;
-#ifdef GEM_DEFAULT_FONT
-  set(gensym("font.face"), gensym(GEM_DEFAULT_FONT));
-#endif
 
-  setEnv(gensym("settings.file"), "GEM_SETTINGS");
-  t_atom*a=get(gensym("settings.file"));
-  if(a) {
-    t_symbol*s=atom_getsymbol(a);
-    open(gem::files::expandEnv(s->s_name, true).c_str(), ".");
-  } else {
-    while(s_configdir[i]) {
-      open(GEM_SETTINGS_FILE, s_configdir[i]);
-      i++;
-    }
-    open(GEM_SETTINGS_FILE, ".");
-  }
-
-  /* legacy settings via environmental variables */
-  setEnv(gensym("texture.rectangle"), "GEM_RECTANGLE_TEXTURE");
-  setEnv(gensym("singlecontext"), "GEM_SINGLE_CONTEXT"); // hmm, what's a better new name for this?
-  setEnv(gensym("font.face"), "GEM_DEFAULT_FONT");
-
-
-  t_gemclass *c = (t_gemclass*)class_new(gensym("Gem"), 0, 0, 0, 0, A_NULL);
-  set(gensym("gem.path"), c->c_externdir);
-
-
-  //  print();
-}
-
-GemSettingsData::~GemSettingsData(void) {
-
-}
-
-void GemSettingsData::setEnv(t_symbol*key, const char*env) {
-  if(NULL==env)return;
-  if(NULL==key)return;
-
-  char*result=getenv(env);
-  if(NULL==result) {
-    return;
-  }
-
-  t_atom a;
-  errno=0;
-
-  /* try integer */
-  long l=strtol(result, NULL, 0);
-  if(0==errno) {
-    SETFLOAT(&a, l);
-    set(key, &a);
-  }
-
-  /* try float */
-  double d=strtod(result, NULL);
-  if(0==errno) {
-    SETFLOAT(&a, d);
-    set(key, &a);
-  }
-
-  /* try symbol */
-  SETSYMBOL(&a, gensym(result));
-  set(key, &a);
-
-  // we ignore lists and other complex things for now
-}
-#if 0
-t_symbol*GemSettingsData::expandEnv(t_symbol*value, bool bashfilename) {
-  if(NULL==value)
-    return NULL;
-  verbose(2, "expanding '%s'", value->s_name);
-
-  if(bashfilename) {
-    char bashBuffer[MAXPDSTRING];
-    sys_bashfilename(value->s_name, bashBuffer);
-    value=gensym(bashBuffer);
-  }
-
-#ifdef HAVE_WORDEXP_H
-  wordexp_t pwordexp;
-
-  if(0==wordexp(value->s_name, &pwordexp, 0)) {
-    pwordexp.we_offs=0;
-    if(pwordexp.we_wordc) {
-      // we only take the first match into account 
-      value=gensym(pwordexp.we_wordv[0]);
-    }
-# ifdef __APPLE__
-/* wordfree() broken on apple: keeps deallocating non-aligned memory */
-#  warning wordfree() not called
-# else
-    wordfree(&pwordexp);
-# endif
-  }
-#endif
-#ifdef _WIN32
-  char envVarBuffer[MAXPDSTRING];
-  ExpandEnvironmentStrings(value->s_name, envVarBuffer, MAX_PATH - 2);
-  value=gensym(envVarBuffer);
-#endif
-
-  verbose(2, "expanded '%s'", value->s_name);
-  return value;
-}
-#endif
 
 /* GemSettings: the public API */
 
-GemSettingsData*GemSettings::settings=NULL;
+GemSettings::PIMPL*GemSettings::settings=NULL;
 
 /* public static functions */
 void GemSettings::init() {
   if(settings)return;
-  settings=new GemSettingsData(); 
+  settings=new PIMPL(); 
 }
 void GemSettings::print() {
   if(!settings)return;
@@ -307,44 +308,29 @@ void GemSettings::save() {
 
 
 
-t_atom*GemSettings::get(t_symbol*s) {
+t_atom*GemSettings::get(const std::string s) {
   if(NULL==settings) init();
-  return settings->get(s);
+  return settings->get(s.c_str());
 }
-t_atom*GemSettings::get(const char*s) {
-  return get(gensym(s));
-}
-
-void GemSettings::set(t_symbol*s, t_atom*v) {
-  if(NULL==settings) init();
-  settings->set(s, v);
-}
-void GemSettings::set(const char*s, t_atom*v) {
-  set(gensym(s), v);
+void GemSettings::set(const std::string s, t_atom*v) {
+  set(s.c_str(), v);
 }
 
 
-void GemSettings::get(const char*key, int&value) {
+void GemSettings::get(const std::string key, int&value) {
   t_atom*a=get(key);
   if(a && A_FLOAT==a->a_type) {
     value=atom_getint(a);
   }
 }
-void GemSettings::get(const char*key, t_float&value) {
+void GemSettings::get(const std::string key, t_float&value) {
   t_atom*a=get(key);
   if(a && A_FLOAT==a->a_type) {
     value=atom_getfloat(a);
   }
 }
 
-void GemSettings::get(const char*key, t_symbol*&value) {
-  t_atom*a=get(key);
-  if(a) {
-    value=atom_getsymbol(a);
-  }
-}
-
-void GemSettings::get(const char*key, char*&value) {
+void GemSettings::get(const std::string key, std::string&value) {
   t_atom*a=get(key);
   if(a) {
     value=atom_getsymbol(a)->s_name;
--- src/Base/GemSettings.h
+++ src/Base/GemSettings.h
@@ -13,32 +13,24 @@ LOG
 #ifndef INCLUDE_GEMSETTINGS_H_
 #define INCLUDE_GEMSETTINGS_H
 
+#include <string>
 
-#include "m_pd.h"
-
-class GemSettingsData;
-
+typedef struct _atom t_atom;
 class GemSettings {
  public:
   static void init(void);
   static void print(void);
   static void save(void);
 
-  static t_atom*get(t_symbol*key);
-  static void set(t_symbol*key, t_atom*value=NULL);
+  static t_atom*get(const std::string key);
+  static void set(const std::string key, t_atom*value=NULL);
 
-  /* shorthands without symbols */
-  static t_atom*get(const char*key);
-  static void set(const char*key, t_atom*value=NULL);
-
-
-  static void get(const char*key, int&value);
-  static void get(const char*key, t_float&value);
-  static void get(const char*key, t_symbol*&value);
-  static void get(const char*key, char*&value);
-
+  static void get(const std::string key, int&value);
+  static void get(const std::string key, float&value);
+  static void get(const std::string key, std::string&value);
 
  private:
-  static GemSettingsData*settings;
+  class PIMPL;
+  static PIMPL*settings;
 };
 #endif
