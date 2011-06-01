352a73056276c29945ac860a4eb598327e98fdfc
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-06-01 17:37:07 +0000

    base class for loading images
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3960 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/plugins/imageloader.cpp
@@ -0,0 +1,86 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.kug.ac.at
+//
+// Implementation file
+//
+//    Copyright (c) 2011 IOhannes m zmoelnig. forum::für::umläute. IEM
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+  
+#include "plugins/imageloader.h"
+//#include "Gem/RTE.h"
+
+using namespace gem;
+
+/////////////////////////////////////////////////////////
+//
+// imageloader
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+imageloader :: imageloader() {
+}
+
+/////////////////////////////////////////////////////////
+// Destructor
+//
+/////////////////////////////////////////////////////////
+imageloader :: ~imageloader()
+{
+}
+
+bool imageloader :: enumProperties(gem::Properties&readable,
+			     gem::Properties&writeable) 
+{
+  readable.clear();
+  writeable.clear();
+  return false;
+}
+
+void imageloader :: setProperties(gem::Properties&props) {
+  // nada
+  m_properties=props;
+#if 0
+  std::vector<std::string> keys=props.keys();
+  int i=0;
+  for(i=0; i<keys.size(); i++) {
+    enum gem::Properties::PropertyType typ=props.type(keys[i]);
+    std::cerr  << "key["<<keys[i]<<"]: "<<typ<<" :: ";
+    switch(typ) {
+    case (gem::Properties::NONE):
+      props.erase(keys[i]);
+      break;
+    case (gem::Properties::DOUBLE):
+      std::cerr << gem::any_cast<double>(props.get(keys[i]));
+      break;
+    case (gem::Properties::STRING):
+      std::cerr << "'" << gem::any_cast<std::string>(props.get(keys[i])) << "'";
+      break;
+    default:
+      std::cerr << "<unkown:" << props.get(keys[i]).get_type().name() << ">";
+      break;
+    }
+  }
+  std::cerr << std::endl;
+#endif
+}
+
+void imageloader :: getProperties(gem::Properties&props) {
+  // nada
+  std::vector<std::string>keys=props.keys();
+  int i=0;
+  for(i=0; i<keys.size(); i++) {
+    gem::any unset;
+    props.set(keys[i], unset);
+  }
+}
+
+
+INIT_IMAGELOADERFACTORY();
---
+++ src/plugins/imageloader.h
@@ -0,0 +1,113 @@
+/* -----------------------------------------------------------------
+
+GEM - Graphics Environment for Multimedia
+
+Load an image and return the frame(OS independant parent-class)
+
+Copyright (c) 2011 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_IMAGELOADER_H_
+#define INCLUDE_IMAGELOADER_H_
+
+#include "Base/GemPixUtil.h"
+
+#include <string>
+#include "plugins/PluginFactory.h"
+
+#include "Base/Properties.h"
+
+
+   /*-----------------------------------------------------------------
+     -------------------------------------------------------------------
+     CLASS
+     imageloader
+    
+     parent class for the system- and library-dependent imageloader-loader classes
+    
+     KEYWORDS
+     pix load an image
+    
+     DESCRIPTION
+
+     -----------------------------------------------------------------*/
+   namespace gem {
+  class GEM_EXTERN imageloader
+  {
+  public:
+  
+    //////////
+    // Constructor
+  
+    /* initialize the imageloaderer
+     */
+    imageloader(void);
+
+    ////////
+    // Destructor
+    /* free what is apropriate */
+    virtual ~imageloader();
+
+
+    /* read a image
+     */
+    /* returns TRUE if loading was successfull, FALSE otherwise */
+    virtual bool read(std::string filename, imageStruct&result);
+
+
+    /**
+     * list all properties this backend supports
+     * after calling, "readable" will hold a list of all properties that can be read
+     * and "writeable" will hold a list of all properties that can be set
+     * if the enumeration fails, this returns <code>false</code>
+     */
+    virtual bool enumProperties(gem::Properties&readable,
+                                gem::Properties&writeable);
+
+    /**
+     * set a number of properties (as defined by "props")
+     * the "props" may hold properties not supported by the currently opened device,
+     *  which is legal; in this case the superfluous properties are simply ignored
+     * this function MAY modify the props; 
+     */
+    virtual void setProperties(gem::Properties&props);
+
+    /**
+     * get the current value of the given properties from the device
+     * if props holds properties that can not be read from the device, they are set to UNSET 
+     */
+    virtual void getProperties(gem::Properties&props);
+
+  protected:
+    /* used to store the "set" properties */
+    gem::Properties m_properties;
+    
+
+  };
+
+}; // namespace gem
+
+
+/**
+ * \fn REGISTER_IMAGELOADERFACTORY(const char *id, Class imageloaderClass)
+ * registers a new class "imageloaderClass" with the imageloader-factory
+ *
+ * \param id a symbolic (const char*) ID for the given class
+ * \param imageloaderClass a class derived from "imageloader"
+ */
+#define REGISTER_IMAGELOADERFACTORY(id, TYP) static gem::PluginFactoryRegistrar::registrar<TYP, gem::imageloader> fac_imageloader_ ## TYP (gensym(id)->s_name)
+
+
+/**
+ * \fn INIT_IMAGELOADERFACTORY()
+ * initialized the factory
+ * \note call this before any externals register themselves
+ */
+#define INIT_IMAGELOADERFACTORY()                                         \
+  static gem::PluginFactoryRegistrar::dummy<gem::imageloader> fac_imageloaderdummy
+
+
+#endif	// for header file
