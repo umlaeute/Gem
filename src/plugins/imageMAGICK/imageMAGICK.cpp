43e4ba646e972dc8049fb3ffe06e5eb2c46a2783
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-06-04 12:43:46 +0000

    simple example for image plugin
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3966 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/plugins/imageMAGICK/imageMAGICK.cpp
@@ -0,0 +1,93 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.kug.ac.at
+//
+// Implementation file 
+//
+//    Copyright (c) 1997-1999 Mark Danks.
+//    Copyright (c) Günther Geiger.
+//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <string.h>
+#include "imageMAGICK.h"
+
+#include "Gem/RTE.h"
+
+
+using namespace gem::plugins;
+
+REGISTER_IMAGELOADERFACTORY("magick", imageMAGICK);
+
+
+/////////////////////////////////////////////////////////
+//
+// imageMAGICK
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+
+imageMAGICK :: imageMAGICK() 
+{
+  post("imageMAGICK");
+}
+imageMAGICK :: ~imageMAGICK()
+{
+  post("~imageMAGICK");
+}
+
+/////////////////////////////////////////////////////////
+// really open the file ! (OS dependent)
+//
+/////////////////////////////////////////////////////////
+bool imageMAGICK :: load(std::string filename, imageStruct&result)
+{
+#ifdef HAVE_MAGICKPLUPLUS
+  Magick::Image image;
+  try {
+    ::verbose(2, "reading '%s' with ImageMagick", filename);
+    // Read a file into image object
+    try {
+      image.read( filename );
+    } catch (Magick::Warning e) {
+      verbose(1, "magick loading problem: %s", e.what());
+    }
+
+    result.xsize=static_cast<GLint>(image.columns());
+    result.ysize=static_cast<GLint>(image.rows());
+    result.setCsizeByFormat(GL_RGBA);
+    result.reallocate();
+
+    result.upsidedown=true;
+
+    try {
+      image.write(0,0,result.xsize,result.ysize, 
+                  "RGBA",
+                  Magick::CharPixel,
+                  reinterpret_cast<void*>(result.data));
+    } catch (Magick::Warning e) {
+      verbose(1, "magick decoding problem: %s", e.what());
+    }
+  }catch( Magick::Exception e )  {
+    verbose(1, "magick loading image failed with: %s", e.what());
+    return false;
+  }
+  return true;
+#else
+  post("imageMagick: not really implemented");
+  return false;
+#endif
+}
+bool imageMAGICK::save(std::string filename, const imageStruct&result) {
+  return false;
+}
---
+++ src/plugins/imageMAGICK/imageMAGICK.h
@@ -0,0 +1,51 @@
+/*-----------------------------------------------------------------
+
+GEM - Graphics Environment for Multimedia
+
+Load a picture (using ImageMagick)
+
+Copyright (c) 2011 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+
+-----------------------------------------------------------------*/
+
+#ifndef INCLUDE_GEM_PLUGINS_IMAGEMAGICK_H_
+#define INCLUDE_GEM_PLUGINS_IMAGEMAGICK_H_
+#include "plugins/image.h"
+#include <stdio.h>
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  imageMAGICK
+  
+  Loads in a picture
+  
+  KEYWORDS
+  pix
+  
+  DESCRIPTION
+
+  -----------------------------------------------------------------*/
+namespace gem { namespace plugins { 
+class GEM_EXPORT imageMAGICK : public imageloader {
+ public:
+
+  //////////
+  // Constructor
+  imageMAGICK(void);
+  virtual ~imageMAGICK(void);
+
+  //////////
+  // read an image
+  virtual bool load(std::string filename, imageStruct&result);
+  //////////
+  // write an image
+  virtual bool save(std::string filename, const imageStruct&result);
+
+};
+};};
+
+#endif	// for header file
