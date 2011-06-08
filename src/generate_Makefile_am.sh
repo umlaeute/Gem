31738a022977f94b341090722a15829baea291a3
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-06-08 16:44:54 +0000

    simplistic generator for Makefile.am
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@3979 49b2a0ca-c541-4d49-a7ef-d785e4371512

---
+++ src/generate_Makefile_am.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TEMPLATEFILE=Makefile.am.template
+
+if [ -e "${TEMPLATEFILE}" ]; then
+ :
+else
+ LIBRARYNAME=$(pwd | sed -e 's|^.*/||')
+
+echo '
+AUTOMAKE_OPTIONS = foreign
+AM_CPPFLAGS = -I$(top_srcdir)
+
+noinst_LTLIBRARIES = lib@LTLIBRARYNAME@.la
+
+lib@LTLIBRARYNAME@_la_CXXFLAGS =
+lib@LTLIBRARYNAME@_la_LIBADD =
+
+# RTE flags
+lib@LTLIBRARYNAME@_la_CXXFLAGS += @GEM_RTE_CFLAGS@
+lib@LTLIBRARYNAME@_la_LIBADD   += @GEM_RTE_LIBS@
+
+lib@LTLIBRARYNAME@_la_SOURCES= @SOURCES@
+'| sed "s|@LTLIBRARYNAME@|${LIBRARYNAME}|g" > ${TEMPLATEFILE}
+
+fi
+
+cat ${TEMPLATEFILE} | \
+sed -e s/@SOURCES@/"$(ls *.cpp *.h | sort | sed -e 's/^/ \\\\\\n    /g' | tr -d '\n')"/
