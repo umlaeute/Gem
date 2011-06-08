2f8803b92cdf984e2fb473144e6596f1df7fddae
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-06-08 17:01:28 +0000

    copied over image syving code to imageQT
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4014 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/plugins/imageQT/imageQT.cpp
+++ src/plugins/imageQT/imageQT.cpp
@@ -24,11 +24,89 @@
 
 # include <Carbon/Carbon.h>
 # include <QuickTime/QuickTime.h>
+# include <QuickTime/ImageCompression.h>
 
+//# include <string.h>
+//# include <fcntl.h> 
 
 using namespace gem::plugins;
 
 REGISTER_IMAGEFACTORY("QT", imageQT);
+
+
+
+OSStatus
+FSPathMakeFSSpec(
+		 const UInt8 *path,
+		 FSSpec *spec,
+		 Boolean *isDirectory)   /* can be NULL */
+{
+  OSStatus   result;
+  FSRef      ref;
+  
+  /* check parameters */
+  require_action(NULL != spec, BadParameter, result = paramErr);
+   
+  /* convert the POSIX path to an FSRef */
+  result = FSPathMakeRef(path, &ref, isDirectory);
+  require_noerr(result, FSPathMakeRef);
+   
+  /* and then convert the FSRef to an FSSpec */
+  result = FSGetCatalogInfo(&ref, kFSCatInfoNone, NULL, NULL, spec, NULL);
+  require_noerr(result, FSGetCatalogInfo);
+   
+ FSGetCatalogInfo:
+ FSPathMakeRef:
+ BadParameter:
+
+  return ( result );
+}
+
+static void GetAppFSSpec(FSSpec *fileSpec)
+{
+  FSRef processRef;
+  FSCatalogInfo processInfo;
+  ProcessSerialNumber psn = {0, kCurrentProcess};
+  GetProcessBundleLocation(&psn, &processRef);
+  FSGetCatalogInfo(&processRef, kFSCatInfoNodeFlags, &processInfo, NULL, fileSpec, NULL);
+}
+
+// MemAlloc
+// Simple memory allocation wrapper
+static void* MemAlloc(unsigned long memsize)
+{
+  if(memsize <= 0)
+    return NULL;
+  else
+    return (malloc(memsize));
+}
+
+unsigned char* CStringToPString(char *string)
+{
+  unsigned char *newString = (unsigned char*)MemAlloc(strlen(string) + 1);
+  size_t i = 0;
+    
+  for(i = 0; i < strlen(string); i++)
+    newString[i+1] = string[i];
+    
+  newString[0] = i;
+    
+  return newString;
+}
+
+
+void InvertGLImage( unsigned char *imageData, unsigned char * outData, long imageSize, long rowBytes )
+{
+  long i, j;
+  // This is not an optimized routine!
+
+  // FIXXME use a flip function in GemPixUtils for this
+
+  // Copy rows into tmp buffer one at a time, reversing their order
+  for (i = 0, j = imageSize - rowBytes; i < imageSize; i += rowBytes, j -= rowBytes) {
+    memcpy( &outData[j], &imageData[i], static_cast<size_t>(rowBytes) );
+  }
+}
 
 
 /////////////////////////////////////////////////////////
@@ -108,8 +186,8 @@ static bool QuickTimeImage2mem(GraphicsImportComponent inImporter, imageStruct&r
                                  result.data, 
                                  static_cast<long>(result.xsize * result.csize));
   if (err) {
-      error("Can't create QTNewWorld");
-   }
+    error("Can't create QTNewWorld");
+  }
 
   ::GraphicsImportSetGWorld(inImporter, gw, NULL);
   ::GraphicsImportDraw(inImporter);
@@ -117,34 +195,6 @@ static bool QuickTimeImage2mem(GraphicsImportComponent inImporter, imageStruct&r
   gw = NULL;
 
   return true;
-}
-
-
-OSStatus
-FSPathMakeFSSpec(
-		 const UInt8 *path,
-		 FSSpec *spec,
-		 Boolean *isDirectory)   /* can be NULL */
-{
-  OSStatus   result;
-  FSRef      ref;
-  
-  /* check parameters */
-  require_action(NULL != spec, BadParameter, result = paramErr);
-   
-  /* convert the POSIX path to an FSRef */
-  result = FSPathMakeRef(path, &ref, isDirectory);
-  require_noerr(result, FSPathMakeRef);
-   
-  /* and then convert the FSRef to an FSSpec */
-  result = FSGetCatalogInfo(&ref, kFSCatInfoNone, NULL, NULL, spec, NULL);
-  require_noerr(result, FSGetCatalogInfo);
-   
- FSGetCatalogInfo:
- FSPathMakeRef:
- BadParameter:
-
-  return ( result );
 }
 
 bool imageQT :: load(std::string filename, imageStruct&result)
@@ -177,6 +227,160 @@ bool imageQT :: load(std::string filename, imageStruct&result)
   return ret;
 }
 bool imageQT::save(std::string filename, const imageStruct&constimage) {
+
+  OSErr			err;
+  ComponentResult		cErr 	= 0;
+    
+  GWorldPtr 			img	= NULL;
+  GraphicsExportComponent 	geComp 	= NULL;
+  Rect			r;
+    
+  OSType			osFileType; 	//= kQTFileTypeJPEG; //kQTFileTypeTIFF fot Tiff kQTFileTypeSGIImage for sgi
+  FSSpec			spec;
+  FSRef			ref;
+
+  unsigned char *data = NULL;
+
+  const UInt8*filename8=reinterpret_cast<const UInt8*>(filename.c_str());
+
+#warning output format
+  int type=1;
+
+
+  if(!constimage.upsidedown) { // the image is openGL-oriented, not quicktime-oriented! flip it!
+    int rowBytes = constimage.xsize * constimage.csize;
+    int imageSize = constimage.ysize * rowBytes;
+
+    data = new unsigned char[imageSize];
+      
+    InvertGLImage(constimage.data, data, imageSize, rowBytes);
+  }
+
+  switch (type){
+  case 0:
+    osFileType 	=kQTFileTypeTIFF;
+    break;
+  case 1:
+    osFileType 	= kQTFileTypeJPEG;
+    break;
+  default:
+    osFileType 	=kQTFileTypeTIFF;
+    break;
+  }
+    
+  err = ::FSPathMakeRef(filename8, &ref, NULL );
+
+  if (err == fnfErr) {
+    // if the file does not yet exist, then let's create the file
+    int fd;
+    fd = open(filename.c_str(), O_CREAT | O_RDWR, 0600);
+    if (fd < 0)
+      return 0;
+    write(fd, " ", 1);
+    close(fd);
+    err = FSPathMakeRef(filename8, &ref, NULL);
+  }
+    
+  if (err != noErr)
+    {
+      error("ERROR: %d in FSPathMakeRef()", err);
+    }
+
+  err = ::FSGetCatalogInfo(&ref, kFSCatInfoNodeFlags, NULL, NULL, &spec, NULL);
+
+  if (err != noErr)
+    {
+      error("ERROR: %d in FSGetCatalogInfo()", err);
+    }
+
+  //  err = FSMakeFSSpec(spec.vRefNum, 0, filename.c_str(), &spec); //spits out -37 error but still works?
+  err = FSMakeFSSpec(spec.vRefNum, spec.parID, filename8, &spec);  //this always gives an error -37 ???
+
+  if (err != noErr && err != -37)
+    {
+      error("ERROR: %d in FSMakeFSSpec()", err);
+    }
+        
+  err = OpenADefaultComponent(GraphicsExporterComponentType, osFileType, &geComp);
+  if (err != noErr)
+    {
+      error("ERROR: %d in OpenADefaultComponent()", err);
+      return 0; // FIXME:
+    }
+    
+  r.top = 0;
+  r.left = 0;
+  r.bottom = constimage.ysize;
+  r.right = constimage.xsize;
+    
+  // ::OffsetRect(&r, -r.left, -r.top);
+  //SetRect(&r,r.left,r.bottom,r.right,r.top);
+  err = QTNewGWorldFromPtr(&img,  
+			   //k32RGBAPixelFormat,
+			   k32ARGBPixelFormat,
+			   &r, NULL, NULL, 0,
+			   // keepLocal,	
+			   //useDistantHdwrMem, 
+			   (data?data:constimage.data),
+			   static_cast<long>(constimage.xsize * constimage.csize));
+
+  // is this the right place to free the "data" buffer (if used)?
+  // i don't know, whether quicktime still needs the buffer...
+                
+  if (err != noErr)
+    {
+      error("ERROR: %d in QTNewGWorldFromPtr()", err);
+      if(data)delete[]data;
+      return 0; // FIXME:
+    }
+    
+    
+  // Set the input GWorld for the exporter
+  cErr = GraphicsExportSetInputGWorld(geComp, img);
+  if (cErr != noErr)
+    {
+      error("ERROR: %d in GraphicsExportSetInputGWorld()", cErr);
+      if(data)delete[]data;
+      return 0; // FIXME:
+    }
+    
+  // Set the output file to our FSSpec
+  cErr = GraphicsExportSetOutputFile(geComp, &spec);
+  if (cErr != noErr)
+    {
+      error("ERROR: %i in GraphicsExportSetOutputFile()", cErr);
+      if(data)delete[]data;
+      return 0; // FIXME:
+    }
+        
+  // Set the compression quality (needed for JPEG, not necessarily for other formats)
+  if (osFileType 	== kQTFileTypeJPEG){
+    //   cErr = GraphicsExportSetCompressionQuality(geComp, codecLosslessQuality);
+    cErr = GraphicsExportSetCompressionQuality(geComp,  codecHighQuality);
+
+    if (cErr != noErr)
+      {
+	error("ERROR: %i in GraphicsExportSetCompressionQuality()", cErr);
+	if(data)delete[]data;
+	return 0; // FIXME:
+      }
+  }
+        
+  // Export it
+  cErr = GraphicsExportDoExport(geComp, NULL);
+  if (cErr != noErr)
+    {
+      error("ERROR: %i in GraphicsExportDoExport()", cErr);
+      if(data)delete[]data;
+      return 0; // FIXME:
+    }
+        
+  // finally, close the component
+  if (geComp != NULL)
+    CloseComponent(geComp);
+
+  if(data)delete[]data;
+
 
   return true;
 }
