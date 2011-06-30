7ec5e5d0c65c715ac4fd7cd51dbcdb3b5c78e311
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-06-30 09:01:26 +0000

    added clean target
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4152 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/plugins/symlink_ltlib.mk
+++ src/plugins/symlink_ltlib.mk
@@ -3,7 +3,7 @@
 # create symlinks to all pkglib_LTLIBRARIES binaries into your top builddir
 
 
-.PHONY: convenience-link
+.PHONY: convenience-link clean-convenience-link
 
 convenience-link: $(pkglib_LTLIBRARIES)
 	  @for soname in `$(EGREP) "^dlname=" $^ | $(SED) -e "s|^dlname='\(.*\)'|\1|"`; do  \
@@ -13,5 +13,13 @@ convenience-link: $(pkglib_LTLIBRARIES)
 	    $(LN_S) $(abs_builddir)/.libs/$$soname $(top_builddir)/$$soname; \
 	  done 
 
+clean-convenience-link:
+	  @for soname in `$(EGREP) "^dlname=" $(pkglib_LTLIBRARIES) | $(SED) -e "s|^dlname='\(.*\)'|\1|"`; do  \
+            echo "$$soname: cleaning convenience links"; \
+            test -L $(top_builddir)/$$soname && rm -f $(top_builddir)/$$soname; \
+	  done 
+	
+
 all-local:: convenience-link
 
+clean-local:: clean-convenience-link
