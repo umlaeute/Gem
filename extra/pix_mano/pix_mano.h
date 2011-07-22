fe76a96a8d7e95ff0af06788d4eb5276fa537e39
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-07-22 12:29:30 +0000

    code cleanup
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4349 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- extra/pix_mano/pix_mano.h
+++ extra/pix_mano/pix_mano.h
@@ -31,57 +31,53 @@
 
 class GEM_EXPORT pix_mano : public GemPixObj
 {
-    CPPEXTERN_HEADER(pix_mano, GemPixObj)
+  CPPEXTERN_HEADER(pix_mano, GemPixObj)
 
     public:
 
-        //////////
-        // Constructor
-    	pix_mano();
+  //////////
+  // Constructor
+  pix_mano();
     	
-    protected:
+ protected:
     	
-    	//////////
-    	// Destructor
-    	virtual ~pix_mano();
+  //////////
+  // Destructor
+  virtual ~pix_mano();
 
-    	//////////
-    	// Do the processing
-    	virtual void 	processGrayImage(imageStruct &image);
+  //////////
+  // Do the processing
+  virtual void 	processGrayImage(imageStruct &image);
 
-        //////////
-    	// Set the new threshold vector
-    	void	    	vecBoundsMess(int argc, t_atom *argv);
+  //////////
+  // Set the new threshold vector
+  void	    	vecBoundsMess(t_symbol*,int argc, t_atom *argv);
     	
-    	//////////
-    	// Set the new threshold value
-    	void	    	vecThreshMess(int argc, t_atom *argv);
+  //////////
+  // Set the new threshold value
+  void	    	vecThreshMess(t_symbol*,int argc, t_atom *argv);
 		
-		//////////
-    	// Set the new threshold value
-    	void	    	vecParamsMess(int argc, t_atom *argv);
+  //////////
+  // Set the new threshold value
+  void	    	vecParamsMess(t_symbol*,int argc, t_atom *argv);
     	
-    	//////////
-    	// The new color
-		t_outlet *outlet1;
-		t_outlet *outlet2;
-		t_outlet *outlet3;
-		t_outlet *outlet4;
-		t_outlet *outlet5;
-		t_outlet *outlet6;
-		int head, bottom, mode, left, right, pixavg, pixtip, min_entry_size, min_perim, pixsamp;
-		float thresh, tip_scalar;
-		float partialx_prev[10000], partialy_prev[10000];
-		int Xsize, Ysize, hop, prev_tip;
-		float tp_i[50], tp_x[50], tp_y[50], tp_m[50], tp_a[50], tp_s[50];
+  //////////
+  // The new color
+  t_outlet *outlet1;
+  t_outlet *outlet2;
+  t_outlet *outlet3;
+  t_outlet *outlet4;
+  t_outlet *outlet5;
+  t_outlet *outlet6;
+  int head, bottom, mode, left, right, pixavg, pixtip, min_entry_size, min_perim, pixsamp;
+  float thresh, tip_scalar;
+  float partialx_prev[10000], partialy_prev[10000];
+  int Xsize, Ysize, hop, prev_tip;
+  float tp_i[50], tp_x[50], tp_y[50], tp_m[50], tp_a[50], tp_s[50];
     
-    private:
-    
-    	//////////
-    	// Static member functions
-    	static void 	vecBoundsMessCallback(void *data, t_symbol *, int argc, t_atom *argv);
-    	static void 	vecThreshMessCallback(void *data, t_symbol *, int argc, t_atom *argv);
-		static void 	vecParamsMessCallback(void *data, t_symbol *, int argc, t_atom *argv);
+ private:
+  class tipData;
+  tipData*m_tipData;
 };
 
 #endif	// for header file
