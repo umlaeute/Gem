bea8b8d7cebd159c87f9b3d9899306eb5d1a3c1d
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-08-05 16:06:34 +0000

    prefix PATH when loadingplugins, as on w32 it disappears...
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4423 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/plugins/PluginFactory.cpp
+++ src/plugins/PluginFactory.cpp
@@ -32,21 +32,22 @@ gem::BasePluginFactory::~BasePluginFactory(void) {
 }
 
 int gem::BasePluginFactory::doLoadPlugins(std::string basename, std::string path) {
-  std::cerr << "load plugins '" << basename << "' in '" << path << "'" << std::endl;
-
   if(path.empty()){
     GemSettings::get("gem.path", path);
   }
-
   if(!path.empty()){
     path=path+std::string("/");
   }
+  std::cerr << "load plugins '" << basename << "' in '" << path << "'" << std::endl;
 
   std::string pattern = path+std::string("gem_") + basename+std::string("*")+GemDylib::getDefaultExtension();
   std::cerr << "pattern : " << pattern << std::endl;
 
+  unsigned int count=0;
+
   std::vector<std::string>files=gem::files::getFilenameListing(pattern);
   unsigned int i=0;
+
   for(i=0; i<files.size(); i++) {
     GemDylib*dll=NULL;
     const std::string f=files[i];
@@ -63,15 +64,39 @@ int gem::BasePluginFactory::doLoadPlugins(std::string basename, std::string path
     if(alreadyloaded)continue;
 
     std::cerr << "dylib loading file '" << f << "'!" << std::endl;
+    dll=NULL;
     try {
       dll=new GemDylib(f, "");
-      m_pimpl->p_loaded.push_back(f);
     } catch (GemException x) {
-      std::cerr << "library loading returned: " << x.what() << std::endl;
+        // oops, on w32 this might simply be because getFilenameListing() stripped the path
+        // so let's try again, with Path added...
+        if(f.find(path) == f.npos) {
+            try {
+                std::string f1=path;
+                f1+=f;
+                dll=new GemDylib(f1, "");
+            } catch (GemException x1) {
+                // giving up
+                std::cerr << "library loading returned: " << x1.what() << std::endl;
+                dll=NULL;
+            }
+        } else {
+            std::cerr << "library loading returned: " << x.what() << std::endl;
+            dll=NULL;
+        }
     }
+    if(dll){ // loading succeeded
+        try {
+            m_pimpl->p_loaded.push_back(f);
+            count++;
+        } catch (GemException x) {
+            std::cerr << "plugin loading returned: " << x.what() << std::endl;
+        }
+    }
+
   }
 
-  return 0;
+  return count;
 }
 
 std::vector<std::string>gem::BasePluginFactory::get() {
