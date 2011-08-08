87039ff20755c568c0f0b85f3fc1a2bfe88f2ad7
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-08-08 14:24:10 +0000

    split 'video' pluginclass into 'video' interface and 'videoBase' implementation
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4432 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/plugins/videoBase.h
+++ src/plugins/videoBase.h
@@ -10,55 +10,90 @@ WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
 -----------------------------------------------------------------*/
 
-#ifndef INCLUDE_GEM_VIDEO_H_
-#define INCLUDE_GEM_VIDEO_H_
+#ifndef INCLUDE_GEM_VIDEOBASE_H_
+#define INCLUDE_GEM_VIDEOBASE_H_
 
-#include "Gem/Properties.h"
-#include <vector>
-#include <string>
+#include "plugins/video.h"
+#include "Gem/Image.h"
 
 /*-----------------------------------------------------------------
   -------------------------------------------------------------------
   CLASS
 	video
     
-	a OS-indendent interface for grabbing video-frames
+	a OS-indendent parent-class for grabbing video-frames
 	
   KEYWORDS
 	pix, capture
     
   -----------------------------------------------------------------*/
-class pixBlock;
 namespace gem { namespace plugins {
- class GEM_EXTERN video {
+ class GEM_EXTERN videoBase : public video {
   public:
     /**
      * returns TRUE if the object can be used in a thread or FALSE otherwise
      * if a backend implements threading itself, it should return FALSE
      * in order to prevent double threading
      */
-    virtual bool isThreadable(void) = 0;
+    virtual bool isThreadable(void);
 
     /**
      * reset the backend, possibly re-enumerating devices
      * returns TRUE if reset was successfull
      */
-    virtual bool          reset(void) = 0;
+    virtual bool          reset(void);
 
+ protected:
+    /**
+     * open the video-device
+     * the exact video device is either the default one or has to be set prior with setDevice()
+     * the device need not start streaming yet
+     * this comes in handy when determining the correct backend for a certain device
+     * \return TRUE when we successfully opened the device and can startTransfer immediately
+     */
+    virtual bool           openDevice(gem::Properties&props) = 0;
+
+    /**
+     * close the video device, freeing all ressources
+     * once the device has been closed it should be useable by other applications,...
+     * this get's called when switching to another backend or when deleting the object
+     */
+    virtual void          closeDevice(void) = 0;
+
+    /**
+     * Start up the video device (called on startRendering)
+     * \return FALSE is something failed, TRUE otherwise
+     */
+    virtual bool	    	startTransfer(void) = 0;
+
+    /**
+     * Stop the video device (called on stopRendering)
+     * \return TRUE if a transfer was going on, FALSE if the transfer was already stopped
+     */
+    virtual bool	   	stopTransfer(void) = 0;
+
+
+    /**
+     * Stops the video device and if it was running restarts it
+     * \return the return code of startTransfer()
+     */
+    virtual bool	   	restartTransfer(void);
+
+ public:
     /** 
      * get the next frame (called when rendering)
      * grab the next frame from the device
      * if no new frame is available, this should set the "newimage" flag to false
      * \return the new frame or NULL on error
      */
-    virtual pixBlock *getFrame(void) = 0;
+    virtual pixBlock *getFrame(void);
 
     /**
      * release a frame (after use)
      * this gets called once for each frame retrieved via getFrame()
      * if you are using DMA or the like, now is the time to release the ressource
      */
-    virtual void releaseFrame(void) = 0;
+    virtual void releaseFrame(void);
 
 
     /** turn on/off "asynchronous"-grabbing
@@ -68,7 +103,7 @@ namespace gem { namespace plugins {
      *   (thus potentially reducing the CPU-load to what is needed, at the cost of slightly outdated images
      * returns: the old state
      */
-    virtual bool grabAsynchronous(bool) = 0;
+    virtual bool grabAsynchronous(bool);
 
     //////////////////////
     // device settings
@@ -77,7 +112,7 @@ namespace gem { namespace plugins {
      * enumerate known devices
      * \return a list of device names (if they can be enumerated)
      */
-    virtual std::vector<std::string>enumerate(void) = 0;
+    virtual std::vector<std::string>enumerate(void);
 
     /**
      * set the device to be opened next time
@@ -87,7 +122,7 @@ namespace gem { namespace plugins {
      * the default implementation (which you normally shouldn't need to override)
      * will simply set m_devicenum and clear m_devicename
      */
-    virtual bool	    	setDevice(int ID) = 0;
+    virtual bool	    	setDevice(int ID);
 
     /**
      * set the device to be opened next time
@@ -98,7 +133,7 @@ namespace gem { namespace plugins {
      * the default implementation (which you normally shouldn't need to override)
      * will simply set m_devicename and clear m_devicenum
      */
-    virtual bool	    	setDevice(const std::string) = 0;
+    virtual bool	    	setDevice(const std::string);
 
     /**
      * list all properties the currently opened device supports
@@ -120,7 +155,7 @@ namespace gem { namespace plugins {
      *    "quality"          "quality" message (float)
      */
     virtual bool enumProperties(gem::Properties&readable,
-				gem::Properties&writeable) = 0;
+				gem::Properties&writeable);
 
     /**
      * set a number of properties (as defined by "props")
@@ -130,13 +165,13 @@ namespace gem { namespace plugins {
      * namely one-shot properties (e.g. "do-white-balance-now") 
      *     should be removed from the props
      */
-    virtual void setProperties(gem::Properties&props) = 0;
+    virtual void setProperties(gem::Properties&props);
 
     /**
      * get the current value of the given properties from the device
      * if props holds properties that can not be read from the device, they are set to UNSET 
      */
-    virtual void getProperties(gem::Properties&props) = 0;
+    virtual void getProperties(gem::Properties&props);
 
 
     /**
@@ -149,68 +184,129 @@ namespace gem { namespace plugins {
      * if the system does support dialogs and the user has specified which one they want, 
      * return TRUE if at least one dialog could be handled
      */
-    virtual bool	    	dialog(std::vector<std::string>names=std::vector<std::string>()) = 0;
+    virtual bool	    	dialog(std::vector<std::string>names=std::vector<std::string>());
     /**
      * enumerate list of possible dialogs (if any)
      */
-    virtual std::vector<std::string>dialogs(void) = 0;
+    virtual std::vector<std::string>dialogs(void);
 
     /**
      * Set the preferred colorspace (of the frames returned by getFrame()
      * \return FALSE if the colorspace cannot be set (e.g. while grabbing is active)
      */
-    virtual bool	    	setColor(int) = 0;
+    virtual bool	    	setColor(int);
   
-    //! open the device (calls openDevice())
-    virtual bool open(gem::Properties&props) = 0;
-    //! close the device (calls closeDevice())
-    virtual void close(void) = 0;
-    //! start the transmission (calls startTransfer())
-    virtual bool start(void) = 0;
-    //! stop the transmission (calls stopTransfer())
-    virtual bool stop(void) = 0;
+    //////////
+    // Constructor
+    // if numlocks>0 we will use a thread to capture the image create <numlocks> mutexes
+    // 
+    videoBase(const std::string name);
+  
+    //////////
+    // Destructor
+    virtual ~videoBase(void);
 
+    //! open the device (calls openDevice())
+    virtual bool open(gem::Properties&props);
+    //! close the device (calls closeDevice())
+    virtual void close(void);
+    //! start the transmission (calls startTransfer())
+    virtual bool start(void);
+    //! stop the transmission (calls stopTransfer())
+    virtual bool stop(void);
+
+  protected:
+
+    /**
+     * in the constructor, you ought to call provide() for each generic backend your plugin is capable of
+     * e.g. the DirectShow plugin will call: provide("analog"); provide("dv");
+     * everything you add here, will be automaticalyl enumerated with the provides() methods
+     */
+    void provide(const std::string);
+
+    //! grab a frame (work-horse)
+    /* this will be run in a separate thread (if threading is enabled)
+     * makes the data accessible in the "m_image" struct!
+     * access to m_image MUST be protected by lock() and unlock()
+     * this should not be used anymore!
+     */
+    virtual bool grabFrame(void){/* lock(); m_image.image.data=NULL; unlock(); */ return false; };
+
+    /***************
+     ** THREADING
+     ** these are here for legacy reasons
+     ** DO NOT USE THEM IN NEW CODE
+     */
+
+    /* starts the thread that will call grabFrame() (if threads are requested in the ctor) */
+    bool startThread(void);
+    /* stops the thread; waits at most "timeout" microseconds; if 0 waits forever; if -1 waits for time specified in ctor */
+    bool stopThread(int timeout=-1);
+
+    /* locks the mutex #<id>; 
+     * if the mutex does not exist (e.g. no threading), this will simply return
+     * the default mutex #0 is locked by default in the getFrame() to protect the m_image ressource
+     */
+    void lock(unsigned int id=0);
+    /* unlocks the mutex #<id>; 
+     * if the mutex does not exist (e.g. no threading), this will simply return
+     * the default mutex #0 is locked by default in the getFrame() to protect the m_image ressource
+     */
+    void unlock(unsigned int id=0);
+
+    /* sleep a selected time in usec
+     * convenience wrapper around select()
+     */
+    void usleep(unsigned long usec);
+
+    //////////
+    // Constructor
+    // if numlocks>0 we will use a thread to capture the image create <numlocks> mutexes
+    // 
+    videoBase(const std::string name, unsigned int numlocks);
+
+    /*
+     * THREADING
+     ************** */
+
+  protected:
+    //! indicates valid transfer (automatically set in start()/stop())
+    bool m_capturing;
+    //! indicates valid device (automatically set in open()/close())
+    bool m_haveVideo;
+    //! a place to store the image with grabFrame()
+    pixBlock m_image;
+  
+    unsigned int m_width;
+    unsigned int m_height;
+    int m_reqFormat;
+
+    /* specify either devicename XOR devicenum */
+    /* if !m_devicename.empty() 
+     *  then use m_devicename
+     * elif m_devicenum>=0
+     *  use m_devicenum (preferrable as index in the list returned by enumerate()
+     * else
+     *  try open a "default" device
+     */
+    std::string m_devicename;
+    int m_devicenum;
+
+  public:
     // for pix_video: query whether this backend provides access to this class of devices
     // (e.g. "dv")
-    virtual bool provides(const std::string) = 0;
+    virtual bool provides(const std::string);
     // get a list of all provided devices
-    virtual std::vector<std::string>provides(void) = 0;
+    virtual std::vector<std::string>provides(void);
 
     // get's the name of the backend (e.g. "v4l")
-    virtual const std::string getName(void) = 0;
+    virtual const std::string getName(void);
 
 
-  //////////
-  // returns an instance wrapping all plugins or NULL
-  // if NULL is returned, you might still try your luck with manually accessing the 
-  // PluginFactory
-    static video*getInstance(void);
+  private:
+    class PIMPL;
+    PIMPL*m_pimpl;
 };
 };}; // namespace
-
-/* 
- * factory code:
- * to use these macros, you have to include "plugins/PluginFactory.h"
- */
-
-
-/**
- * \fn REGISTER_VIDEOFACTORY(const char *id, Class videoClass)
- * registers a new class "videoClass" with the video-factory
- *
- * \param id a symbolic (const char*) ID for the given class
- * \param videoClass a class derived from "video"
- */
-#define REGISTER_VIDEOFACTORY(id, TYP) static gem::PluginFactoryRegistrar::registrar<TYP, gem::plugins::video> fac_video_ ## TYP (gensym(id)->s_name)
-
-
-/**
- * \fn INIT_VIDEOFACTORY()
- * initialized the factory
- * \note call this before any externals register themselves
- */
-#define INIT_VIDEOFACTORY()                                             \
-  static gem::PluginFactoryRegistrar::dummy<gem::plugins::video> fac_videodummy
-
 
 #endif	// for header file
