16530ea875014a11bc0acccfb71915bac1c688e6
zmoelnig <zmoelnig@49b2a0ca-c541-4d49-a7ef-d785e4371512>
2011-08-16 20:42:59 +0000

    use CLAMP_TO_EDGE if available
    git-svn-id: https://pd-gem.svn.sourceforge.net/svnroot/pd-gem/trunk/Gem@4481 49b2a0ca-c541-4d49-a7ef-d785e4371512

--- src/Controls/gemframebuffer.cpp
+++ src/Controls/gemframebuffer.cpp
@@ -281,8 +281,11 @@ void gemframebuffer :: initFBO()
   // so switching back to GL_NEAREST
   glTexParameteri(m_texTarget, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
   glTexParameteri(m_texTarget, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexParameterf(m_texTarget, GL_TEXTURE_WRAP_S, GL_CLAMP);
-  glTexParameterf(m_texTarget, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+  GLuint wrapmode = (GLEW_EXT_texture_edge_clamp)?GL_CLAMP_TO_EDGE:GL_CLAMP;
+
+  glTexParameterf(m_texTarget, GL_TEXTURE_WRAP_S, wrapmode);
+  glTexParameterf(m_texTarget, GL_TEXTURE_WRAP_T, wrapmode);
   
   // Initialize the render buffer.
   glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, m_depthBufferIndex);
