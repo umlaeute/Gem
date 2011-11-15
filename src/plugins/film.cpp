6114835e6b0841fe888a601ce239964e098b7638
IOhannes m zmölnig <zmoelnig@iem.at>
2011-11-15 21:20:46 +0100

    film: stub implementation of factory wrapper

--- src/plugins/film.cpp
+++ src/plugins/film.cpp
@@ -16,12 +16,178 @@
 
 #include "film.h"
 #include "plugins/PluginFactory.h"
+#include "Gem/RTE.h"
+#include "Gem/Exception.h"
+#include "Gem/Properties.h"
 
 gem::plugins::film :: ~film(void) {}
-
-gem::plugins::film*gem::plugins::film::getInstance(void) {
- return NULL;
-}
-
 /* initialize the film factory */
 static gem::PluginFactoryRegistrar::dummy<gem::plugins::film> fac_filmdummy;
+
+namespace gem { namespace plugins {
+  class filmMeta : public gem::plugins::film {
+  private:
+    static filmMeta*s_instance;
+    std::vector<gem::plugins::film*>m_handles; // all available handles
+    gem::plugins::film*m_handle; // currently opened handle (or NULL)
+    std::vector<std::string>m_ids; // list of handle names
+
+    // set to TRUE if we can use the current handle in another thread
+    bool m_canThread;
+
+    bool addPlugin( std::vector<std::string>available, std::string ID=std::string("")) {
+      int count=0;
+
+      std::vector<std::string>id;
+      if(!ID.empty()) {
+        // if requested 'cid' is in 'available' add it to the list of 'id's
+        if(std::find(available.begin(), available.end(), ID)!=available.end()) {
+          id.push_back(ID);
+        } else {
+          // request for an unavailable ID
+          verbose(2, "backend '%s' unavailable", ID.c_str());
+          return false;
+        }
+      } else {
+        // no 'ID' given: add all available IDs
+        id=available;
+      }
+
+      unsigned int i=0;
+      for(i=0; i<id.size(); i++) {
+        std::string key=id[i];
+        verbose(2, "trying to add '%s' as backend", key.c_str());
+        if(std::find(m_ids.begin(), m_ids.end(), key)==m_ids.end()) {
+          // not yet added, do so now!
+          gem::plugins::film*handle=NULL;
+          try {
+            handle=gem::PluginFactory<gem::plugins::film>::getInstance(key);
+          } catch(GemException&x) {
+            handle=NULL;
+            verbose(1, "cannot use film plugin '%s': %s", key.c_str(), x.what());
+          }
+          if(NULL==handle)continue;
+          m_ids.push_back(key);
+          m_handles.push_back(handle);
+          count++;
+          verbose(2, "added backend#%d '%s'", m_handles.size()-1, key.c_str());
+        }
+      }
+      return (count>0);
+    }
+
+  public:
+    filmMeta(void) :
+      m_handle(NULL),
+      m_canThread(true)
+    {
+      gem::PluginFactory<gem::plugins::film>::loadPlugins("film");
+      std::vector<std::string>ids=gem::PluginFactory<gem::plugins::film>::getIDs();
+
+      if(!addPlugin(ids, "DirectShow"))
+	addPlugin(ids, "AVI");
+
+      addPlugin(ids, "gmerlin");
+      addPlugin(ids, "QuickTime");
+      addPlugin(ids, "quicktime4linux");
+      addPlugin(ids, "MPEG3");
+      addPlugin(ids, "aviplay");
+      addPlugin(ids, "MPEG1");
+      addPlugin(ids);
+
+      unsigned int i;
+      for(i=0; i<m_handles.size(); i++) {
+	if(!m_handles[i]->isThreadable()) {
+	  m_canThread=false;
+	  break;
+	}
+      }
+    }
+
+    virtual ~filmMeta(void) {
+      unsigned int i;
+      for(i=0; i<m_handles.size(); i++) {
+        delete m_handles[i];
+        m_handles[i]=NULL;
+      }
+    }
+
+    virtual bool open(const std::string name, const gem::Properties&requestprops) {
+      if(m_handle)close();
+      int id=-1;
+      post("open() stub: implement 'codec' property");
+
+      if(id>=0) {
+	id=id % m_handles.size();
+	if(m_handles[id] && m_handles[id]->open(name, requestprops)) {
+	  m_handle=m_handles[id];
+	}
+      }
+      if(!m_handle) {
+	unsigned int i=0;
+	for(i=0; i<m_handles.size(); i++) {
+	  if(m_handles[id] && m_handles[id]->open(name, requestprops)) {
+	    m_handle=m_handles[id];
+	    break;
+	  } else {
+
+	  }
+	}
+      }
+      return (NULL!=m_handle);
+    }
+
+    virtual errCode changeImage(int imgNum, int trackNum=-1){
+      if(m_handle)
+	return m_handle->changeImage(imgNum, trackNum);
+
+      return FAILURE;
+    }
+
+    virtual pixBlock* getFrame(void) {
+      if(m_handle)
+	return m_handle->getFrame();
+      return NULL;
+    }
+
+    virtual void close(void)  {
+      if(m_handle)
+	m_handle->close();
+      m_handle=NULL;
+    }
+
+    virtual bool isThreadable(void) {
+      if(m_handle)
+	return m_handle->isThreadable();
+
+      return m_canThread;
+    }
+
+    virtual bool enumProperties(gem::Properties&readable,
+				gem::Properties&writeable) {
+      readable.clear();
+      writeable.clear();
+      post("enumProperties stub");
+      return false;
+    }
+
+    virtual void setProperties(gem::Properties&props) {
+      if(m_handle)
+	m_handle->setProperties(props);
+    }
+    virtual void getProperties(gem::Properties&props) {
+      if(m_handle)
+	m_handle->getProperties(props);
+      else
+	props.clear();
+    }
+  };
+};
+};
+
+
+gem::plugins::film*gem::plugins::film::getInstance(void) {
+  gem::plugins::film*result=new filmMeta();
+  return result;
+}
+
