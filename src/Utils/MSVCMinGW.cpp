396a5fc7f28cef07c48689f4b2cc858152759a1c
IOhannes m zmölnig <zmoelnig@iem.at>
2011-12-08 12:55:08 +0100

    export the M$VC/MinGW stubs

--- src/Utils/MSVCMinGW.cpp
+++ src/Utils/MSVCMinGW.cpp
@@ -3,6 +3,11 @@
  * seems to be needed since MinGW provides ISO-C99 compatible printf() implementation
  * which older MSVCrt does not provide yet
  */
+#include "Gem/ExportDef.h"
 
-
-extern "C" int _get_output_format( void ) { return 1; }
+extern "C"  {
+GEM_EXTERN
+int _get_output_format( void ) { 
+	return 1; 
+}
+}
