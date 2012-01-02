7efa7498fd399a2fcbbcafb71962bd055041ff5a
IOhannes m zmölnig <zmoelnig@iem.at>
2012-01-02 14:28:15 +0100

    use a struct for all vertexbuffers

--- src/Geos/gemvertexbuffer.cpp
+++ src/Geos/gemvertexbuffer.cpp
@@ -31,6 +31,60 @@
 
 CPPEXTERN_NEW_WITH_ONE_ARG(gemvertexbuffer, t_floatarg, A_DEFFLOAT);
 
+gemvertexbuffer :: VertexBuffer:: VertexBuffer (unsigned int size_) :
+  size(size_),
+  vbo(0),
+  array(new float[size_]),
+  dirty(false),
+  enabled(false) {
+  ::post("created VertexBuffer[%p] with %d elements at %p", this, size, array);
+}
+gemvertexbuffer :: VertexBuffer:: ~VertexBuffer (void) {
+  ::post("destroying VertexBuffer[%p] with %d elements at %p", this, size, array);
+  destroy();
+  if(array)
+    delete[]array;
+  array=NULL;
+}
+void gemvertexbuffer :: VertexBuffer:: resize (unsigned int size_) {
+  if(array)delete[]array; 
+  array=new float[size_];
+  size=size_;
+  dirty=true;
+}
+
+bool gemvertexbuffer :: VertexBuffer:: create (void) {
+  if(!vbo) {
+    glGenBuffers(1, &vbo);
+  }
+  if(vbo) {
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), array, GL_DYNAMIC_DRAW);
+  }
+  return (0!=vbo);
+}
+bool gemvertexbuffer :: VertexBuffer:: render (void) {
+  // render from the VBO
+  if ( enabled ) {
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    if ( dirty ) {
+      //~ printf("push pos vertex\n");
+      glBufferData(GL_ARRAY_BUFFER, size * sizeof(float), array, GL_DYNAMIC_DRAW);
+      dirty = false;
+    }
+  }
+  return enabled;
+}
+void gemvertexbuffer :: VertexBuffer:: destroy (void) {
+	if ( vbo ){
+		glBindBuffer(1, vbo);
+		glDeleteBuffers(1, &vbo);
+	}
+  vbo=0;
+}
+
+
+
 /////////////////////////////////////////////////////////
 //
 // gemvertexbuffer
@@ -41,18 +95,9 @@ CPPEXTERN_NEW_WITH_ONE_ARG(gemvertexbuffer, t_floatarg, A_DEFFLOAT);
 /////////////////////////////////////////////////////////
 gemvertexbuffer :: gemvertexbuffer(t_floatarg size) :
   vbo_size(size>0?size:(256*256)),
-	posArray_dirty(false),  texArray_dirty(false),  colArray_dirty(false),  normArray_dirty(false),
-	posVBO_enable(false),   texVBO_enable(false),   colVBO_enable(false),   normVBO_enable(false),
 	size_change_flag(false),
-	posArray(NULL),         texArray(NULL),         colArray(NULL),         normArray(NULL),
-	posVBO(0),             	texVBO(0),             	colVBO(0),              normVBO(0)
+  m_position(vbo_size*3), m_texture(vbo_size*2), m_color(vbo_size*4), m_normal(vbo_size*3)
 { 
-	//~ printf("gemvertexbuffer build on %s at %s\n", __DATE__, __TIME__);
-
-	posArray = new float[vbo_size*3];
-	texArray = new float[vbo_size*2];
-	colArray = new float[vbo_size*4];
-	normArray = new float[vbo_size*3];
 }
 
 /////////////////////////////////////////////////////////
@@ -61,42 +106,6 @@ gemvertexbuffer :: gemvertexbuffer(t_floatarg size) :
 /////////////////////////////////////////////////////////
 gemvertexbuffer :: ~gemvertexbuffer(void)
 {
-	cleanUp();
-}
-
-/////////////////////////////////////////////////////////
-// cleanup
-//
-/////////////////////////////////////////////////////////
-void gemvertexbuffer :: cleanUp(void)
-{
-	// delete VBO
-	//~ printf("delete VBO\n");
-	if ( posVBO ){
-		glBindBuffer(1, posVBO);
-		glDeleteBuffers(1, &posVBO);
-	}
-    if ( texVBO ){
-		glBindBuffer(1, texVBO);
-		glDeleteBuffers(1, &texVBO);
-	}
-    if ( colVBO ){
-		glBindBuffer(1, colVBO);
-		glDeleteBuffers(1, &colVBO);
-	}
-	if ( normVBO ){
-		glBindBuffer(1, normVBO);
-		glDeleteBuffers(1, &normVBO);
-	}
-    
-  posVBO = texVBO = colVBO = normVBO = 0;
-    
-  // delete Array 
-  //~ printf("free array\n");
-  delete[]posArray; posArray=NULL;
-  delete[]texArray; texArray=NULL;
-  delete[]colArray; colArray=NULL;
-  delete[]normArray; normArray=NULL;
 }
 
 /////////////////////////////////////////////////////////
@@ -106,68 +115,32 @@ void gemvertexbuffer :: cleanUp(void)
 void gemvertexbuffer :: renderShape(GemState *state)
 {
 	if ( m_drawType == GL_DEFAULT_GEM ) m_drawType = GL_POINTS;
-	if ( !posVBO || !texVBO || !colVBO || !normVBO || size_change_flag ) {
+	if ( !m_position.vbo || !m_texture.vbo || !m_color.vbo || !m_normal.vbo || size_change_flag ) {
 //		printf("create VBO\n");
 		createVBO();
 		size_change_flag = false;
-	}	
-		// render from the VBO
-		if ( posVBO_enable )
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, posVBO);
-			if ( posArray_dirty ) {
-				//~ printf("push pos vertex\n");
-				glBufferData(GL_ARRAY_BUFFER, vbo_size * 3 * sizeof(float), posArray, GL_DYNAMIC_DRAW);
-				posArray_dirty = false;
-			}
-			glVertexPointer(3, GL_FLOAT, 0, 0);
-		}
-
-		if ( texVBO_enable )
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, texVBO);
-			if ( texArray_dirty ) {
-				//~ printf("push tex vertex\n");
-				glBufferData(GL_ARRAY_BUFFER, vbo_size * 2 * sizeof(float), texArray, GL_DYNAMIC_DRAW);
-				texArray_dirty = false;
-			}
-			glTexCoordPointer(2, GL_FLOAT, 0, 0);
-		}
-		
-		if ( colVBO_enable )
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, colVBO);
-			if ( colArray_dirty ) {
-				//~ printf("push col vertex\n");
-				glBufferData(GL_ARRAY_BUFFER, vbo_size * 4 * sizeof(float), colArray, GL_DYNAMIC_DRAW);
-				colArray_dirty = false;
-			}
+	}
+  // render from the VBO
+  if(m_position.render())
+    glVertexPointer(3, GL_FLOAT, 0, 0);
+  if(m_texture.render())
+    glTexCoordPointer(2, GL_FLOAT, 0, 0);
+  if(m_color.render())
 			glColorPointer(4, GL_FLOAT, 0, 0);
-		}
-		
-		if ( normVBO_enable )
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, normVBO);
-			if ( normArray_dirty ) {
-				//~ printf("push norm vertex\n");
-				glBufferData(GL_ARRAY_BUFFER, vbo_size * 3 * sizeof(float), normArray, GL_DYNAMIC_DRAW);
-				normArray_dirty = false;
-			}
+  if(m_normal.render())
 			glNormalPointer(GL_FLOAT, 0, 0);
-		}
 		
-		if ( posVBO_enable ) glEnableClientState(GL_VERTEX_ARRAY);
-		if ( colVBO_enable ) glEnableClientState(GL_COLOR_ARRAY);
-		if ( texVBO_enable ) glEnableClientState(GL_TEXTURE_COORD_ARRAY); 
-		if ( normVBO_enable ) glEnableClientState(GL_NORMAL_ARRAY); 
+  if ( m_position.enabled ) glEnableClientState(GL_VERTEX_ARRAY);
+  if ( m_color.enabled ) glEnableClientState(GL_COLOR_ARRAY);
+  if ( m_texture.enabled ) glEnableClientState(GL_TEXTURE_COORD_ARRAY); 
+  if ( m_normal.enabled ) glEnableClientState(GL_NORMAL_ARRAY); 
 		
-		glDrawArrays(m_drawType, 0, vbo_size);
+  glDrawArrays(m_drawType, 0, vbo_size);
 		
-		if ( posVBO_enable ) glDisableClientState(GL_VERTEX_ARRAY);
-		if ( colVBO_enable ) glDisableClientState(GL_COLOR_ARRAY);
-		if ( texVBO_enable ) glDisableClientState(GL_TEXTURE_COORD_ARRAY); 
-		if ( normVBO_enable ) glDisableClientState(GL_NORMAL_ARRAY); 
-		
+  if ( m_position.enabled ) glDisableClientState(GL_VERTEX_ARRAY);
+  if ( m_color.enabled ) glDisableClientState(GL_COLOR_ARRAY);
+  if ( m_texture.enabled ) glDisableClientState(GL_TEXTURE_COORD_ARRAY); 
+  if ( m_normal.enabled ) glDisableClientState(GL_NORMAL_ARRAY);	
 }
 
 /////////////////////////////////////////////////////////
@@ -203,96 +176,96 @@ void gemvertexbuffer :: obj_setupCallback(t_class *classPtr)
 	class_addmethod(classPtr, reinterpret_cast<t_method>(&gemvertexbuffer::resizeMessCallback), 
 		gensym("resize"), A_FLOAT, A_NULL);
 	class_addmethod(classPtr, reinterpret_cast<t_method>(&gemvertexbuffer::posVBO_enableMessCallback), 
-		gensym("posVBO_enable"), A_FLOAT, A_NULL);
+		gensym("m_position.enabled"), A_FLOAT, A_NULL);
 	class_addmethod(classPtr, reinterpret_cast<t_method>(&gemvertexbuffer::colVBO_enableMessCallback), 
-		gensym("colVBO_enable"), A_FLOAT, A_NULL);
+		gensym("m_color.enabled"), A_FLOAT, A_NULL);
 	class_addmethod(classPtr, reinterpret_cast<t_method>(&gemvertexbuffer::texVBO_enableMessCallback), 
-		gensym("texVBO_enable"), A_FLOAT, A_NULL);
+		gensym("m_texture.enabled"), A_FLOAT, A_NULL);
 	class_addmethod(classPtr, reinterpret_cast<t_method>(&gemvertexbuffer::normVBO_enableMessCallback), 
-		gensym("normVBO_enable"), A_FLOAT, A_NULL);			
+		gensym("m_normal.enabled"), A_FLOAT, A_NULL);			
 }
 
 void gemvertexbuffer :: posxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {	
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->posArray, 3, 0);
-	GetMyClass(data)->posArray_dirty = true;
-	GetMyClass(data)->posVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_position, 3, 0);
+	GetMyClass(data)->m_position.dirty = true;
+	GetMyClass(data)->m_position.enabled = true;
 	   
 }
 void gemvertexbuffer :: posyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->posArray, 3, 1);
-	GetMyClass(data)->posArray_dirty = true;
-	GetMyClass(data)->posVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_position, 3, 1);
+	GetMyClass(data)->m_position.dirty = true;
+	GetMyClass(data)->m_position.enabled = true;
 }
 void gemvertexbuffer :: poszMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->posArray, 3, 2);
-	GetMyClass(data)->posArray_dirty = true;
-	GetMyClass(data)->posVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_position, 3, 2);
+	GetMyClass(data)->m_position.dirty = true;
+	GetMyClass(data)->m_position.enabled = true;
 }
 
 void gemvertexbuffer :: colrMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->colArray, 4, 0);
-	GetMyClass(data)->colArray_dirty = true;
-	GetMyClass(data)->colVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_color, 4, 0);
+	GetMyClass(data)->m_color.dirty = true;
+	GetMyClass(data)->m_color.enabled = true;
 }
 
 void gemvertexbuffer :: colgMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->colArray, 4, 1);
-	GetMyClass(data)->colArray_dirty = true;
-	GetMyClass(data)->colVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_color, 4, 1);
+	GetMyClass(data)->m_color.dirty = true;
+	GetMyClass(data)->m_color.enabled = true;
 }
 
 void gemvertexbuffer :: colbMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->colArray, 4, 2);
-	GetMyClass(data)->colArray_dirty = true;
-	GetMyClass(data)->colVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_color, 4, 2);
+	GetMyClass(data)->m_color.dirty = true;
+	GetMyClass(data)->m_color.enabled = true;
 }
 
 void gemvertexbuffer :: colaMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->colArray, 4, 3);
-	GetMyClass(data)->colArray_dirty = true;
-	GetMyClass(data)->colVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_color, 4, 3);
+	GetMyClass(data)->m_color.dirty = true;
+	GetMyClass(data)->m_color.enabled = true;
 }
 
 void gemvertexbuffer :: texuMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->texArray, 2, 0);
-	GetMyClass(data)->texArray_dirty = true;
-	GetMyClass(data)->texVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_texture, 2, 0);
+	GetMyClass(data)->m_texture.dirty = true;
+	GetMyClass(data)->m_texture.enabled = true;
 }
 
 void gemvertexbuffer :: texvMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->texArray, 2, 1);
-	GetMyClass(data)->texArray_dirty = true;
-	GetMyClass(data)->texVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_texture, 2, 1);
+	GetMyClass(data)->m_texture.dirty = true;
+	GetMyClass(data)->m_texture.enabled = true;
 }
 
 void gemvertexbuffer :: normxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->normArray, 3, 0);
-	GetMyClass(data)->normArray_dirty = true;
-	GetMyClass(data)->normVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_normal, 3, 0);
+	GetMyClass(data)->m_normal.dirty = true;
+	GetMyClass(data)->m_normal.enabled = true;
 }
 
 void gemvertexbuffer :: normyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->normArray, 3, 1);
-	GetMyClass(data)->normArray_dirty = true;
-	GetMyClass(data)->normVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_normal, 3, 1);
+	GetMyClass(data)->m_normal.dirty = true;
+	GetMyClass(data)->m_normal.enabled = true;
 }
 
 void gemvertexbuffer :: normzMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv)
 {
-	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->normArray, 3, 2);
-	GetMyClass(data)->normArray_dirty = true;
-	GetMyClass(data)->normVBO_enable = true;
+	GetMyClass(data)->tabMess(argc,argv, GetMyClass(data)->m_normal, 3, 2);
+	GetMyClass(data)->m_normal.dirty = true;
+	GetMyClass(data)->m_normal.enabled = true;
 }
 
 void gemvertexbuffer :: resizeMessCallback(void *data, float size)
@@ -302,25 +275,25 @@ void gemvertexbuffer :: resizeMessCallback(void *data, float size)
 
 void gemvertexbuffer :: posVBO_enableMessCallback(void *data, float flag)
 {
-	GetMyClass(data)->posVBO_enable = (flag != 0);
+	GetMyClass(data)->m_position.enabled = (flag != 0);
 }
 
 void gemvertexbuffer :: colVBO_enableMessCallback(void *data, float flag)
 {
-	GetMyClass(data)->colVBO_enable = (flag != 0);
+	GetMyClass(data)->m_color.enabled = (flag != 0);
 }
 
 void gemvertexbuffer :: texVBO_enableMessCallback(void *data, float flag)
 {
-	GetMyClass(data)->texVBO_enable = (flag != 0);
+	GetMyClass(data)->m_texture.enabled = (flag != 0);
 }
 
 void gemvertexbuffer :: normVBO_enableMessCallback(void *data, float flag)
 {
-	GetMyClass(data)->normVBO_enable = (flag != 0);
+	GetMyClass(data)->m_normal.enabled = (flag != 0);
 }
 
-void gemvertexbuffer :: tabMess(int argc, t_atom *argv, float *array, int stride, int offset)
+void gemvertexbuffer :: tabMess(int argc, t_atom *argv, VertexBuffer&array, int stride, int offset)
 {
 	int offset2 = 0;
 	t_symbol *tab_name;
@@ -346,13 +319,11 @@ void gemvertexbuffer :: resizeMess(float size)
 {
 	vbo_size = size>1?size:1;
 	//~ printf("cleanup\n");
-	cleanUp();
-	//~ printf("define array\n");
-	posArray = new float[vbo_size*3];
-	texArray = new float[vbo_size*2];
-	colArray = new float[vbo_size*4];
-	normArray = new float[vbo_size*3];
-	//~ printf("printf clear VBO\n");
+  m_position.resize(vbo_size*3);
+  m_texture .resize(vbo_size*2);
+  m_color   .resize(vbo_size*4);
+  m_normal  .resize(vbo_size*3);
+
 	size_change_flag = true;
 }
 
@@ -360,39 +331,20 @@ void gemvertexbuffer :: resizeMess(float size)
 //*****************************************************************************
 void gemvertexbuffer :: createVBO(void)
 {
-    // create VBO
- 	unsigned int i, j;
-	
-	// create buffer object
-	//~ printf("create buffer object\n");
-	glGenBuffers(1, &posVBO);
-	glGenBuffers(1, &colVBO);
-	glGenBuffers(1, &normVBO);
-	glGenBuffers(1, &texVBO);
-	
-	//~ printf("bind buffer\n");
-	glBindBuffer(GL_ARRAY_BUFFER, posVBO);
-	glBufferData(GL_ARRAY_BUFFER, vbo_size * 3 * sizeof(float), posArray, GL_DYNAMIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, texVBO);
-	glBufferData(GL_ARRAY_BUFFER, vbo_size * 2 * sizeof(float), texArray, GL_DYNAMIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, colVBO);
-	glBufferData(GL_ARRAY_BUFFER, vbo_size * 4 * sizeof(float), colArray, GL_DYNAMIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, normVBO);
-	glBufferData(GL_ARRAY_BUFFER, vbo_size * 3 * sizeof(float), normArray, GL_DYNAMIC_DRAW);
-	
-	//~ printf("done\n");
+  m_position.create();
+  m_texture.create();
+  m_color.create();
+  m_texture.create();
 }
 
-void gemvertexbuffer :: copyArray(t_symbol *tab_name, float *array, unsigned int stride, unsigned int offset)
+void gemvertexbuffer :: copyArray(t_symbol *tab_name, VertexBuffer&buf, unsigned int stride, unsigned int offset)
 {
 	t_garray *a;
 	int npoints, i;
 	t_word *vec;
 	t_float posx;
-	
+
+  t_float*array=buf.array;
 	//~ printf("copy table %s to array\n", tab_name->s_name);
 	pd_findbyclass(tab_name, garray_class);
 	if (!(a = (t_garray *)pd_findbyclass(tab_name, garray_class)))
--- src/Geos/gemvertexbuffer.h
+++ src/Geos/gemvertexbuffer.h
@@ -1,100 +1,104 @@
 /*-----------------------------------------------------------------
-LOG
-    GEM - Graphics Environment for Multimedia
+  LOG
+  GEM - Graphics Environment for Multimedia
 
-    A gemvertexbuffer
+  A gemvertexbuffer
 
-    Copyright (c) 1997-2000 Mark Danks. mark@danks.org
-    Copyright (c) Günther Geiger. geiger@epy.co.at
-    Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
-    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+  Copyright (c) 1997-2000 Mark Danks. mark@danks.org
+  Copyright (c) Günther Geiger. geiger@epy.co.at
+  Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
------------------------------------------------------------------*/
+  -----------------------------------------------------------------*/
 
 #include "Base/GemShape.h"
 #include "Gem/Image.h"
 #include "Gem/State.h"
 	
 /*-----------------------------------------------------------------
--------------------------------------------------------------------
-CLASS
-    gemvertexbuffer
+  -------------------------------------------------------------------
+  CLASS
+  gemvertexbuffer
     
-    Creates a gemvertexbuffer
+  Creates a gemvertexbuffer
 
-KEYWORDS
-    geo
+  KEYWORDS
+  geo
     
-DESCRIPTION
+  DESCRIPTION
     
------------------------------------------------------------------*/
+  -----------------------------------------------------------------*/
 class GEM_EXTERN gemvertexbuffer : public GemShape
 {
-    CPPEXTERN_HEADER(gemvertexbuffer, GemShape);
+  CPPEXTERN_HEADER(gemvertexbuffer, GemShape);
 
-    public:
+ protected:
+  class VertexBuffer {
+  public:
+    VertexBuffer(unsigned int size);
+    ~VertexBuffer(void);
+    void resize(unsigned int);
+    bool create(void);
+    bool render(void);
+    void destroy(void);
 
-        //////////
-        // Constructor
-    	gemvertexbuffer(t_floatarg size);
+    unsigned int size;
+
+    GLuint vbo;
+    float*array;
+    bool dirty;
+    bool enabled;
+  };
+
+
+ public:
+  //////////
+  // Constructor
+  gemvertexbuffer(t_floatarg size);
     	
-    	/////////////
-    	// Variables
-    	//float range_x;
-    	//float range_y;
+  /////////////
+    // Variables
+    //float range_x;
+    //float range_y;
     	
     protected:
     	
-    	//////////
-    	// Destructor
-    	virtual ~gemvertexbuffer(void);
+  //////////
+  // Destructor
+  virtual ~gemvertexbuffer(void);
     	
-    	//////////
-    	// Destructor
-    	void cleanUp(void);    	
-        
-    	//////////
-    	// Do the rendering
-    	virtual void renderShape(GemState *state);
-    	//virtual void 	runKernel();
-    	void tabMess(int argc, t_atom *argv, float *array, int stride, int offset);
- 		void resizeMess(float size);
+  //////////
+  // Do the rendering
+  virtual void renderShape(GemState *state);
+  //virtual void 	runKernel();
+  void tabMess(int argc, t_atom *argv, VertexBuffer&array, int stride, int offset);
+  void resizeMess(float size);
 
-    	
+ private :
+  // GL functionality
+  void createVBO(void);
+  void copyArray(t_symbol *tab_name, VertexBuffer&array, unsigned int stride, unsigned int offset);
+  static void posxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void posyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void poszMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void colrMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void colgMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void colbMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void colaMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void texuMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void texvMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void normxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void normyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void normzMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
+  static void resizeMessCallback(void *data, float size);
+  static void posVBO_enableMessCallback(void *data, float flag);
+  static void colVBO_enableMessCallback(void *data, float flag);
+  static void texVBO_enableMessCallback(void *data, float flag);
+  static void normVBO_enableMessCallback(void *data, float flag);
 
-	private :
-    	    	
-		// GL functionality
-		void createVBO(void);
-		void copyArray(t_symbol *tab_name, float *array, unsigned int stride, unsigned int offset);
-		static void posxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void posyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void poszMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void colrMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void colgMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void colbMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void colaMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void texuMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void texvMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void normxMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void normyMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void normzMessCallback(void *data, t_symbol *symbol, int argc, t_atom *argv);
-		static void resizeMessCallback(void *data, float size);
-		static void posVBO_enableMessCallback(void *data, float flag);
-		static void colVBO_enableMessCallback(void *data, float flag);
-		static void texVBO_enableMessCallback(void *data, float flag);
-		static void normVBO_enableMessCallback(void *data, float flag);
-
-		
-		// Rendering window vars
-		int vbo_size;
-		bool posArray_dirty, 	texArray_dirty, colArray_dirty, normArray_dirty;
-		bool posVBO_enable, 	texVBO_enable, 	colVBO_enable, 	normVBO_enable;
-		bool size_change_flag;
-
-		float *posArray, *texArray, *colArray, *normArray;
-
-		// vbo variables
-		GLuint posVBO, texVBO, colVBO, normVBO;
+  // Rendering window vars
+  int vbo_size;
+  bool size_change_flag;
+  VertexBuffer m_position, m_texture, m_color, m_normal;
 };
