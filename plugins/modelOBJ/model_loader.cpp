937855756904756c9da255c49ede505beb92af39
IOhannes m zmölnig <zmoelnig@iem.at>
2012-01-10 12:14:19 +0100

    make GLMmodel an opaque type
    put struct definition into implementation file; it's not needed in the header...

--- plugins/modelOBJ/model_loader.cpp
+++ plugins/modelOBJ/model_loader.cpp
@@ -19,6 +19,81 @@
 
 #define T(x) (model->triangles[(x)])
 
+
+/* GLMmaterial: Structure that defines a material in a model.
+ */
+typedef struct _GLMmaterial
+{
+  char* name;                   /* name of material */
+  GLfloat diffuse[4];           /* diffuse component */
+  GLfloat ambient[4];           /* ambient component */
+  GLfloat specular[4];          /* specular component */
+  GLfloat emmissive[4];         /* emmissive component */
+  GLfloat shininess;            /* specular exponent */
+} GLMmaterial;
+
+/* GLMtriangle: Structure that defines a triangle in a model.
+ */
+typedef struct _GLMtriangle {
+  GLuint vindices[3];           /* array of triangle vertex indices */
+  GLuint nindices[3];           /* array of triangle normal indices */
+  GLuint tindices[3];           /* array of (generated) triangle texcoord indices*/
+  GLuint uvtindices[3];         /* array of triangle texcoord indices*/
+  GLuint findex;                /* index of triangle facet normal */
+} GLMtriangle;
+
+/* GLMgroup: Structure that defines a group in a model.
+ */
+typedef struct _GLMgroup {
+  char*             name;           /* name of this group */
+  GLuint            numtriangles;   /* number of triangles in this group */
+  GLuint*           triangles;      /* array of triangle indices */
+  GLuint            material;       /* index to material for group */
+  struct _GLMgroup* next;           /* pointer to next group in model */
+} GLMgroup;
+
+/* GLMmodel: Structure that defines a model.
+ */
+typedef struct _GLMmodel {
+  char*    pathname;            /* path to this model */
+  char*    mtllibname;          /* name of the material library */
+
+  GLuint   numvertices;         /* number of vertices in model */
+  GLfloat* vertices;            /* array of vertices  */
+
+  GLuint   numnormals;          /* number of normals in model */
+  GLfloat* normals;             /* array of normals */
+
+  /*
+   * generated texcoords
+   */
+  GLuint   numtexcoords;        /* number of texcoords in model */
+  GLfloat* texcoords;           /* array of texture coordinates */
+
+  GLuint   numfacetnorms;       /* number of facetnorms in model */
+  GLfloat* facetnorms;          /* array of facetnorms */
+
+  GLuint       numtriangles;    /* number of triangles in model */
+  GLMtriangle* triangles;       /* array of triangles */
+
+  GLuint       nummaterials;    /* number of materials in model */
+  GLMmaterial* materials;       /* array of materials */
+
+  GLuint       numgroups;       /* number of groups in model */
+  GLMgroup*    groups;          /* linked list of groups */
+
+  GLfloat position[3];          /* position of the model */
+
+  /*
+   * texcoords as stored in the model;
+   * must never be modified
+   */
+  GLuint   numuvtexcoords;        /* number of texcoords in model */
+  GLfloat* uvtexcoords;           /* array of texture coordinates */
+} GLMmodel;
+
+
+
 /* _GLMnode: general purpose node */
 typedef struct _GLMnode {
   GLuint         index;
