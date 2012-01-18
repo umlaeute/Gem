1f41d612c488ebbe40c6ef41948aae2cf9d37b77
IOhannes m zmölnig <zmoelnig@iem.at>
2012-01-18 14:07:48 +0100

    working a bit on installer script

--- build/osx-installer/buildinstaller.sh
+++ build/osx-installer/buildinstaller.sh
@@ -1,41 +1,56 @@
 #!/bin/sh
-
 ## this is shamelessly taken from Pd-extended...
 
+SRCDIR=$1
 VOLUME_NAME = Gem
-hdiutil create -format UDRW -fs HFS+ -srcfolder "$(BUILD_BASE)" -volname $(VOLUME_NAME) build.dmg
-# detach one with the same name first
-hdiutil detach "/Volumes/$(VOLUME_NAME)"
-hdiutil attach build.dmg
-install -p $(DESTDIR)$(manualsdir)/Pd/ReadMe.html "/Volumes/$(VOLUME_NAME)"
+PACKAGE_NAME = ${VOLUME_NAME}
+DESTDIR=/Volumes/${VOLUME_NAME}
 
-# add link to /Applications for easy install
-ln -s -f /Applications /Volumes/$(VOLUME_NAME)/Applications
+if [ -d "${SRCDIR}" ]; then
+ echo "creating Gem installer from ${SRCDIR}"
+else
+ echo "please provide a valid /path/to/Gem" 1>&2
+ exit 1
+fi
+
+
+hdiutil create -format UDRW -fs HFS+ -srcfolder "${SRCDIR}" -volname "${VOLUME_NAME}" build.dmg
+# detach one with the same name first
+hdiutil detach "${DESTDIR}"
+hdiutil attach build.dmg
+#install -p ${SRCDIR}${manualsdir}/Pd/ReadMe.html "${DESTDIR}"
+
+# add link to /Library/Pd for easy install
+/usr/bin/osacompile -o "${DESTDIR}/System-wide Installation.app" droplet_system.as
+./icon2app "${DESTDIR}/System-wide Installation.app" /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/LibraryFolderIcon.icns
+
+# add link to ~/Library/Pd for easy install
+/usr/bin/osacompile -o "${DESTDIR}/User-specific Installation.app" droplet_user.as
+./icon2app "${DESTDIR}/User-specific Installation.app" /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/HomeFolderIcon.icns
 
 # Install to .background for easier manual DS_Store makeing 
-install -d "/Volumes/$(VOLUME_NAME)/.background"
+install -d "${DESTDIR}/.background"
 # The full path to the background image needs to be added to .DS_Store to work
 # Hence the background image will only show when the disk image is called 
-# "$(VOLUME_NAME)=Pd-extended - that is what the current DS_Store uses.
+# VOLUME_NAME=Pd-extended - that is what the current DS_Store uses.
 # To create the DS_Store file one must... search the net.
-install -p background.png "/Volumes/$(VOLUME_NAME)/.background/"
-install -p DS_Store "/Volumes/$(VOLUME_NAME)/.DS_Store"
-install -p VolumeIcon.icns "/Volumes/$(VOLUME_NAME)/.VolumeIcon.icns"
+install -p background.png "${DESTDIR}/.background/"
+install -p DS_Store "${DESTDIR}/.DS_Store"
 
 # To enable the local image icon
-/Developer/Tools/SetFile -a C /Volumes/$(VOLUME_NAME)/.VolumeIcon.icns /Volumes/$(VOLUME_NAME)
-$(MAKE) -C $(packages_src) DESTDIR="/Volumes/$(VOLUME_NAME)/$(PD_APP_NAME).app/Contents/Resources" doc_format
+install -p VolumeIcon.icns "${DESTDIR}/.VolumeIcon.icns"
+/Developer/Tools/SetFile -a C "${DESTDIR}/.VolumeIcon.icns" "${DESTDIR}"
 
-chmod -R a-w "/Volumes/$(VOLUME_NAME)/$(PD_APP_NAME).app/Contents/Resources"
+#chmod -R a-w "${DESTDIR}/${PD_APP_NAME}.app/Contents/Resources"
 
 
-hdiutil detach $(mount | grep $(VOLUME_NAME) | cut -d ' ' -f 1)
-hdiutil convert -format UDZO -o "$(PACKAGE_NAME).dmg" build.dmg
+hdiutil detach $(mount | grep "${VOLUME_NAME}" | cut -d ' ' -f 1)
+hdiutil convert -format UDZO -o "${PACKAGE_NAME}.dmg" build.dmg
 rm -f build.dmg
 
 
 # install license
-hdiutil unflatten "$(PACKAGE_NAME).dmg"
-/Developer/Tools/Rez /Developer/Headers/FlatCarbon/*.r SLA.r -a -o "$(PACKAGE_NAME).dmg"
-hdiutil flatten "$(PACKAGE_NAME).dmg"
+hdiutil unflatten "${PACKAGE_NAME}.dmg"
+/Developer/Tools/Rez /Developer/Headers/FlatCarbon/*.r SLA.r -a -o "${PACKAGE_NAME}.dmg"
+hdiutil flatten "${PACKAGE_NAME}.dmg"
 
