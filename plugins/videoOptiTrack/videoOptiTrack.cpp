6d9ca106f55096cb52c412980363ebb2bc3c5c5f
IOhannes m zmölnig <zmoelnig@iem.at>
2012-01-19 13:30:02 +0100

    stub for OptiTrack camera SDK

---
+++ plugins/videoOptiTrack/videoOptiTrack.cpp
@@ -0,0 +1,151 @@
+////////////////////////////////////////////////////////
+//
+// videoOptiTrack - Graphics Environment for Multimedia
+//
+// daniel@bogusfront.org
+// zmoelnig@iem.at
+//
+// Implementation file 
+//
+//    Copyright (c) 2003 Daniel Heckenberg.
+//    Copyright (c) 2010-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#ifdef HAVE_LIBOPTITRACK
+
+#include "videoOptiTrack.h"
+using namespace gem::plugins;
+#include "Gem/RTE.h"
+#include "plugins/PluginFactory.h"
+
+REGISTER_VIDEOFACTORY("OptiTrack", videoOptiTrack);
+
+
+using namespace CameraLibrary;
+
+
+videoOptiTrack::videoOptiTrack(void) :
+  m_name(std::string("OptiTrack")),
+{
+
+  //  CameraManager::X().WaitForInitialization();
+  throw(GemException("couldn't initialize OptiTrack"));
+
+  m_pixBlock.image.xsize = 64;
+  m_pixBlock.image.ysize = 64;
+  m_pixBlock.image.setCsizeByFormat(GL_RGBA);
+  m_pixBlock.image.reallocate();
+}
+
+videoOptiTrack::~videoOptiTrack(void) {
+}
+void videoOptiTrack::close(void) {
+}
+
+bool videoOptiTrack::open(gem::Properties&props) {
+  return false;
+}
+
+pixBlock*videoOptiTrack::getFrame(void) {
+  return &m_pixBlock;
+}
+
+void videoOptiTrack::releaseFrame(void) {
+}
+
+std::vector<std::string>videoOptiTrack::enumerate(void) {
+  std::vector<std::string>result;
+  //  result.push_back("vlc");
+  return result;
+}
+
+bool videoOptiTrack::setDevice(int ID) {
+  m_devname.clear();
+  return false;
+}
+bool videoOptiTrack::setDevice(std::string device) {
+  m_devname=device;
+  return true;
+}
+bool videoOptiTrack::enumProperties(gem::Properties&readable,
+			       gem::Properties&writeable) {
+  readable.clear();
+  writeable.clear();
+
+  writeable.set("width",  m_pixBlock.image.xsize);  readable.set("width",  m_pixBlock.image.xsize);
+  writeable.set("height",  m_pixBlock.image.ysize); readable.set("height",  m_pixBlock.image.ysize);
+  return false;
+}
+void videoOptiTrack::setProperties(gem::Properties&props) {
+  int width=-1;
+  int height=-1;
+
+  m_props=props;
+
+  double d;
+  if(props.get("width", d)) {
+    if(d>0)
+      width = d;
+  }
+  if(props.get("height", d)) {
+    if(d>0)
+      height=d;
+  }
+
+#if 0
+  if(!m_mediaplayer) {
+    if(width>0)
+      m_pixBlock.image.xsize=width;
+    if(height>0)
+      m_pixBlock.image.ysize=height;
+  } else {
+    // changes will take effect with next restart
+  }
+#endif
+}
+
+
+void videoOptiTrack::getProperties(gem::Properties&props) {
+  std::vector<std::string>keys=props.keys();
+  double d;
+  int i;
+
+  for(i=0; i<keys.size(); i++) {
+    if("width"==keys[i]) {
+      props.set(keys[i], m_pixBlock.image.xsize);
+    }
+    if("height"==keys[i]) {
+      props.set(keys[i], m_pixBlock.image.ysize);
+    }
+  }
+}
+
+std::vector<std::string>videoOptiTrack::dialogs(void) {
+  std::vector<std::string>result;
+  return result;
+}
+bool videoOptiTrack::provides(const std::string name) {
+  return (name==m_name);
+}
+std::vector<std::string>videoOptiTrack::provides(void) {
+  std::vector<std::string>result;
+  result.push_back(m_name);
+  return result;
+}
+const std::string videoOptiTrack::getName(void) {
+  return m_name;
+}
+
+
+bool videoOptiTrack::start(void) {
+  return false;
+}
+bool videoOptiTrack::stop (void) {
+  return false;
+}
+#endif /* HAVE_LIBOPTITRACK */
---
+++ plugins/videoOptiTrack/videoOptiTrack.h
@@ -0,0 +1,76 @@
+/*-----------------------------------------------------------------
+
+GEM - Graphics Environment for Multimedia
+
+Copyright (c) 2012 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+#ifndef _INCLUDE_GEMPLUGIN__VIDEOOPTITRACK_VIDEOOPTITRACK_H_
+#define _INCLUDE_GEMPLUGIN__VIDEOOPTITRACK_VIDEOOPTITRACK_H_
+
+#include "plugins/video.h"
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  pix_video
+    
+  captures a video using NaturalPoints OptiTrack cameras
+    
+  KEYWORDS
+  pix
+    
+  -----------------------------------------------------------------*/
+namespace gem { namespace plugins {
+ class GEM_EXPORT videoOptiTrack : public video {
+  public:
+   //////////
+   // Constructor
+   videoOptiTrack(void);
+    	    	
+    //////////^
+   // Destructor
+   virtual ~videoOptiTrack(void);
+
+   virtual bool open(gem::Properties&props);
+   virtual pixBlock *getFrame(void);
+
+   virtual std::vector<std::string>enumerate(void);
+   virtual bool setDevice(int ID);
+   virtual bool setDevice(const std::string);
+
+   virtual bool enumProperties(gem::Properties&readable,
+                               gem::Properties&writeable);
+   virtual void setProperties(gem::Properties&props);
+   virtual void getProperties(gem::Properties&props);
+
+   virtual std::vector<std::string>dialogs(void);
+   // for pix_video: query whether this backend provides access to this class of devices
+   // (e.g. "dv")
+   virtual bool provides(const std::string);
+   // get a list of all provided devices
+   virtual std::vector<std::string>provides(void);
+
+   // get's the name of the backend (e.g. "v4l")
+   virtual const std::string getName(void);
+
+   virtual bool isThreadable(void) {return true;}
+   virtual bool reset(void) {return true;}
+   virtual void releaseFrame(void);
+   virtual bool grabAsynchronous(bool) {return true;}
+   virtual bool dialog(std::vector<std::string>names=std::vector<std::string>()) {return false;}
+   virtual bool setColor(int) {return false;}
+
+   virtual void close(void);
+   virtual bool start(void);
+   virtual bool stop(void);
+   
+  protected:
+  }; 
+};};
+
+
+#endif	// for header file
