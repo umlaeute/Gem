102a83fe7eb614c36415c0e815530a7cc27d3315
IOhannes m zmölnig <zmoelnig@iem.at>
2012-01-25 12:08:45 +0100

    indentation; reset() implementation

--- plugins/videoOptiTrack/videoOptiTrack.h
+++ plugins/videoOptiTrack/videoOptiTrack.h
@@ -31,62 +31,62 @@ WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
   -----------------------------------------------------------------*/
 namespace gem { namespace plugins {
- class GEM_EXPORT videoOptiTrack : public video {
-  public:
-   //////////
-   // Constructor
-   videoOptiTrack(void);
+class GEM_EXPORT videoOptiTrack : public video {
+ public:
+  //////////
+  // Constructor
+  videoOptiTrack(void);
 
-   //////////
-   // Destructor
-   virtual ~videoOptiTrack(void);
+  //////////
+  // Destructor
+  virtual ~videoOptiTrack(void);
 
-   virtual bool open(gem::Properties&props);
-   virtual pixBlock *getFrame(void);
+  virtual bool open(gem::Properties&props);
+  virtual pixBlock *getFrame(void);
 
-   virtual std::vector<std::string>enumerate(void);
-   virtual bool setDevice(int ID);
-   virtual bool setDevice(const std::string);
+  virtual std::vector<std::string>enumerate(void);
+  virtual bool setDevice(int ID);
+  virtual bool setDevice(const std::string);
 
-   virtual bool enumProperties(gem::Properties&readable,
-                               gem::Properties&writeable);
-   virtual void setProperties(gem::Properties&props);
-   virtual void getProperties(gem::Properties&props);
+  virtual bool enumProperties(gem::Properties&readable,
+                              gem::Properties&writeable);
+  virtual void setProperties(gem::Properties&props);
+  virtual void getProperties(gem::Properties&props);
 
-   virtual std::vector<std::string>dialogs(void);
-   // for pix_video: query whether this backend provides access to this class of devices
-   // (e.g. "dv")
-   virtual bool provides(const std::string);
-   // get a list of all provided devices
-   virtual std::vector<std::string>provides(void);
+  virtual std::vector<std::string>dialogs(void);
+  // for pix_video: query whether this backend provides access to this class of devices
+  // (e.g. "dv")
+  virtual bool provides(const std::string);
+  // get a list of all provided devices
+  virtual std::vector<std::string>provides(void);
 
-   // get's the name of the backend (e.g. "v4l")
-   virtual const std::string getName(void);
+  // get's the name of the backend (e.g. "v4l")
+  virtual const std::string getName(void);
 
-   virtual bool isThreadable(void) {return true;}
-   virtual bool reset(void) {return true;}
-   virtual void releaseFrame(void);
-   virtual bool grabAsynchronous(bool) {return true;}
-   virtual bool dialog(std::vector<std::string>names=std::vector<std::string>()) {return false;}
-   virtual bool setColor(int) {return false;}
+  virtual bool isThreadable(void) {return true;}
+  virtual bool reset(void);
+  virtual void releaseFrame(void);
+  virtual bool grabAsynchronous(bool) {return true;}
+  virtual bool dialog(std::vector<std::string>names=std::vector<std::string>()) {return false;}
+  virtual bool setColor(int) {return false;}
 
-   virtual void close(void);
-   virtual bool start(void);
-   virtual bool stop(void);
+  virtual void close(void);
+  virtual bool start(void);
+  virtual bool stop(void);
 
-  protected:
-	  gem::Properties m_props;
-	  std::string m_devname;
-	  int m_devnum;
-	  int m_quality;
+ protected:
+  gem::Properties m_props;
+  std::string m_devname;
+  int m_devnum;
+  int m_quality;
 
-	  CameraLibrary::Camera*m_camera;
-	  CameraLibrary::Frame *m_frame;
+  CameraLibrary::Camera*m_camera;
+  CameraLibrary::Frame *m_frame;
 
-	  bool m_resize;
-	  pixBlock m_pixBlock;
+  bool m_resize;
+  pixBlock m_pixBlock;
 
-  };
+};
 };};
 
 
