30ad2fbb265d6a50135fb0b94fd65f066b15c574
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2012-02-12 19:59:07 +0100

    removed PPM read code from glm

--- plugins/modelOBJ/model_loader.h
+++ plugins/modelOBJ/model_loader.h
@@ -227,37 +227,6 @@ glmListGroup(const GLMmodel* model, GLuint mode, int groupNumber);
 GLvoid
 glmWeld(GLMmodel* model, GLfloat epsilon);
 
-/* glmReadPPM: read a PPM raw (type P6) file.  The PPM file has a header
- * that should look something like:
- *
- *    P6
- *    # comment
- *    width height max_value
- *    rgbrgbrgb...
- *
- * where "P6" is the magic cookie which identifies the file type and
- * should be the only characters on the first line followed by a
- * carriage return.  Any line starting with a # mark will be treated
- * as a comment and discarded.   After the magic cookie, three integer
- * values are expected: width, height of the image and the maximum
- * value for a pixel (max_value must be < 256 for PPM raw files).  The
- * data section consists of width*height rgb triplets (one byte each)
- * in binary format (i.e., such as that written with fwrite() or
- * equivalent).
- *
- * The rgb data is returned as an array of unsigned chars (packed
- * rgb).  The malloc()'d memory should be free()'d by the caller.  If
- * an error occurs, an error message is sent to stderr and NULL is
- * returned.
- *
- * filename   - name of the .ppm file.
- * width      - will contain the width of the image on return.
- * height     - will contain the height of the image on return.
- *
- */
-GLubyte*
-glmReadPPM(const char* filename, int* width, int* height);
-
 #ifdef __cplusplus
 }
 #endif
