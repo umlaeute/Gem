e1bb90360b546c3c6500550412070b507345d08e
IOhannes m zmölnig <zmoelnig@umlautM.umlaeute.mur.at>
2012-08-01 17:52:42 +0200

    configNT.h is no longer needed
    all config is done in property pages

--- src/Gem/configNT.h
+++ src/Gem/configNT.h
@@ -4,23 +4,6 @@
  * for default-defines on _MSC_VER see
  * see http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vclang/html/_predir_predefined_macros.asp for available macros
  *
- * most of these settings require special libraries to be linked against
- * so you might have to edit Project->Settings too
  *
- * NOTE: LATER think about loading the libs from within this file on M$VC
- *       #pragma comment(lib, "library")
- *       see http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vclang/html/_predir_comment.asp
+ * this file is empty as all the settings are done via property pages
  */
-
-/* use FTGL for font-rendering */
-#define FTGL
-#define HAVE_FTGL_FTGL_H
-
-/* **********************************************************************
- * now do a bit of magic based on the information given above
- * probably you don't want to edit this
- * (but who knows)
- * ******************************************************************** */
-#ifdef FTGL
-# define FTGL_LIBRARY_STATIC
-#endif
