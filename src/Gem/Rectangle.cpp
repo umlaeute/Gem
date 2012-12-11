efb32796bf339187f195268a948c9f018489ee14
IOhannes m zmölnig <zmoelnig@iem.at>
2012-12-11 16:06:35 +0100

    simple abstract Rectangle
    to be used as ROI

---
+++ src/Gem/Rectangle.cpp
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.kug.ac.at
+//
+// Implementation file
+//
+//    Copyright (c) 2012 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+
+#include "Rectangle.h"
+
+/////////////////////////////////////////////////////////
+//
+// Rectangle
+//
+namespace gem {
+  Rectangle :: Rectangle(void) :
+    x1(0.f), y1(0.f), x2(1.f), y2(1.f)
+  {
+  }
+
+#define CLAMP01(x) if(x<0.)x=0.;else if(x>1.)x=1.;
+  Rectangle Rectangle :: createNormalized(float x1, float y1, float x2, float y2) {
+    Rectangle result;
+
+    float minX=(x1<x2)?x1:x2;
+    float maxX=(x1>x2)?x1:x2;
+    float minY=(y1<y2)?y1:y2;
+    float maxY=(y1>y2)?y1:y2;
+    CLAMP01(minX);
+    CLAMP01(maxX);
+    CLAMP01(minY);
+    CLAMP01(maxY);
+
+    result.x1=minX;
+    result.x2=maxX;
+    result.y1=minY;
+    result.y2=maxY;
+
+    return result;
+  }
+};
---
+++ src/Gem/Rectangle.h
@@ -0,0 +1,44 @@
+/*-----------------------------------------------------------------
+  LOG
+  GEM - Graphics Environment for Multimedia
+
+  an abstract rectangle representation
+
+  Copyright (c) 1997-1999 Mark Danks. mark@danks.org
+  Copyright (c) Günther Geiger. geiger@epy.co.at
+  Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+  -----------------------------------------------------------------*/
+
+#ifndef _INCLUDE__GEM_GEM_RECTANGLE_H_
+#define _INCLUDE__GEM_GEM_RECTANGLE_H_
+
+#include "Gem/ExportDef.h"
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  Rectangle
+
+  abstract rectangle class (to be used e.g. as ROI)
+
+  DESCRIPTION
+
+  -----------------------------------------------------------------*/
+namespace gem {
+  class GEM_EXTERN Rectangle
+  {
+  public:
+
+    //////////
+    // Constructor
+    Rectangle(void);
+    float x1, y1,  x2, y2;
+
+    static Rectangle createNormalized(float x1, float y1, float x2, float y2);
+  };
+};
+
+#endif	// for header file
