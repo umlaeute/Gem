68a4739e5546e0762b9c4eb586689a24a67f6970
IOhannes m zmölnig <zmoelnig@iem.at>
2013-01-07 19:18:40 +0100

    String utility
    currently this is mainly unicode / bidirectional rendering support

---
+++ src/Utils/String.cpp
@@ -0,0 +1,144 @@
+#include "Gem/GemConfig.h"
+
+#include "String.h"
+
+
+#ifdef _WIN32
+typedef unsigned __int8 uint8_t;
+#else
+# include <stdint.h>
+#endif
+
+#ifdef HAVE_LIBFRIBIDI
+# include <fribidi.h>
+
+#include "m_pd.h"
+
+namespace gem {
+  namespace string {
+    std::wstring getVisualLine(const std::string&instring) {
+      std::wstring outstring;
+      const FriBidiCharSet enc= FRIBIDI_CHAR_SET_UTF8;
+      FriBidiParType direction=FRIBIDI_PAR_ON;
+      size_t len2=instring.size()*2;
+
+      FriBidiChar*str_in  = new FriBidiChar[len2];
+      FriBidiChar*str_out = new FriBidiChar[len2];
+
+      char*output=new char[len2];
+
+      /* convert UTF8 to UTF32 */
+      FriBidiStrIndex ulen = fribidi_charset_to_unicode(enc, instring.c_str(), instring.size(), str_in);
+      /* reshape the UTF32 string */
+      FriBidiLevel lvl = fribidi_log2vis(str_in, ulen, &direction, str_out, 0, 0, 0);
+      if(lvl) {
+        outstring=std::wstring((wchar_t*)str_out);
+      } else {
+        //outstring=instring;
+      }
+      delete[]str_in;
+      delete[]str_out;
+      return outstring;
+    }
+
+    std::wstring getVisualLine(const std::wstring&instring) {
+      std::wstring outstring;
+
+      const FriBidiCharSet enc= FRIBIDI_CHAR_SET_UTF8;
+      FriBidiParType direction=FRIBIDI_PAR_ON;
+      size_t len2=instring.size()*2;
+
+      const FriBidiChar*str_in  = (const FriBidiChar*)instring.c_str();
+      FriBidiChar*str_out = new FriBidiChar[len2];
+
+      /* reshape the UTF32 string */
+      FriBidiLevel lvl = fribidi_log2vis(str_in, instring.size(), &direction, str_out, 0, 0, 0);
+      if(lvl) {
+        wchar_t*output=(wchar_t*)str_out;
+        outstring=std::wstring(output, instring.size());
+      } else {
+        outstring=instring;
+      }
+      delete[]str_out;
+
+      return outstring;
+    }
+
+
+  };
+};
+
+
+#else /* !HAVE_LIBFRIBIDI */
+namespace gem {
+  namespace string {
+    std::wstring getVisualLine(const std::string&instring)  { return toWstring(instring.c_str()); }
+    std::wstring getVisualLine(const std::wstring&widestring) {
+      return widestring;
+    }
+  };
+};
+#endif /* !HAVE_LIBFRIBIDI */
+
+
+static const uint8_t utf8d[] = {
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+
+  070,070,070,070,070,070,070,070,070,070,070,070,070,070,070,070,
+  050,050,050,050,050,050,050,050,050,050,050,050,050,050,050,050,
+  030,030,030,030,030,030,030,030,030,030,030,030,030,030,030,030,
+  030,030,030,030,030,030,030,030,030,030,030,030,030,030,030,030,
+  204,204,188,188,188,188,188,188,188,188,188,188,188,188,188,188,
+  188,188,188,188,188,188,188,188,188,188,188,188,188,188,188,188,
+  174,158,158,158,158,158,158,158,158,158,158,158,158,142,126,126,
+  111, 95, 95, 95, 79,207,207,207,207,207,207,207,207,207,207,207,
+
+  0,1,1,1,8,7,6,4,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,8,7,6,4,5,4,3,2,1,1,1,1,
+
+};
+
+
+namespace gem {
+  namespace string {
+    std::wstring toWstring(const char*str) throw(int){
+      std::wstring result;
+
+      wchar_t unic = 0;
+      uint8_t data, byte, stat = 9;
+
+      int len=0;
+
+      while((byte=*str++)) {
+        data = utf8d[ byte ];
+        stat = utf8d[ 256 + (stat << 4) + (data >> 4) ];
+        byte = (byte ^ (uint8_t)(data << 4));
+
+        unic = (unic << 6) | byte;
+
+        if (!stat) {
+          // unic is now a proper code point, we just print it out.
+          result+=unic;
+          unic = 0;
+        }
+
+        if (stat == 1) {
+          // the byte is not allowed here; the state would have to
+          // be reset to continue meaningful reading of the string
+
+          throw(len);
+        }
+
+        len++;
+      }
+
+      return result;
+    }
+  };
+};
---
+++ src/Utils/String.h
@@ -0,0 +1,35 @@
+/*-----------------------------------------------------------------
+LOG
+    GEM - Graphics Environment for Multimedia
+
+	- bidirectional text support
+
+    Copyright (c) 2010-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+-----------------------------------------------------------------*/
+
+
+
+#ifndef _INCLUDE__GEM_GEM_STRING_H_
+#define _INCLUDE__GEM_GEM_STRING_H_
+
+#include <string>
+#include "Gem/ExportDef.h"
+
+class CPPExtern;
+namespace gem {
+
+  namespace string {
+
+    /* convert a string to it's visual representation (UTF-8) */
+    GEM_EXTERN std::wstring getVisualLine(const std::string&);
+    GEM_EXTERN std::wstring getVisualLine(const std::wstring&);
+
+    /* convert a UTF-8 string to wchar */
+    GEM_EXTERN std::wstring toWstring(const char*str) throw(int);
+  };
+};
+
+#endif /* _INCLUDE__GEM_GEM_BIDI_H_ */
