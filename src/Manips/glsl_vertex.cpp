d5f9de327e11abb681ad4ea3ca0326030b5fa4f0
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-08-20 17:21:42 +0200

    fixing umlaut

--- src/Manips/glsl_vertex.cpp
+++ src/Manips/glsl_vertex.cpp
@@ -8,7 +8,7 @@
 // Implementation file
 //
 //    Copyright (c) 1997-1999 Mark Danks.
-//    Copyright (c) GÂžnther Geiger.
+//    Copyright (c) Günther Geiger.
 //    Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
