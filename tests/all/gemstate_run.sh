871de190695c6c15c3881b2a798ccb9cd4339650
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-08-25 18:21:33 +0200

    run test (for parallel)

---
+++ tests/all/gemstate_run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#echo "(not) running test: $@"
+#exit 0
+
+if test "x${PD}" = "x"
+then
+ PD=pd
+fi
+PDARGS="-noprefs -stderr"
+PDARGS="${PDARGS} -oss -nosound -nrt"
+PDARGS="${PDARGS} -nogui"
+PDARGS="${PDARGS} -path ../../abstractions:../../:../../examples/data -lib Gem"
+
+
+obj=$1
+patch=$2
+order=$3
+echo "testing ${patch} ($order) with ${obj}..." 1>&2
+"${PD}" ${PDARGS} -open "object_gemstate.${patch}.pd" -send "order $(echo $order | grep -o .)" -send "object ${obj}" 2>&1
