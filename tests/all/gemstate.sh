6f7ba7a4360d29bbd3a0e981029c0a26f447c985
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-08-25 18:23:53 +0200

    prepare to use GNU parallel

--- tests/all/gemstate.sh
+++ tests/all/gemstate.sh
@@ -11,36 +11,49 @@ PDARGS="-noprefs -stderr"
 PDARGS="${PDARGS} -oss -nosound -nrt"
 PDARGS="${PDARGS} -nogui"
 PDARGS="${PDARGS} -path ../../abstractions:../../:../../examples/data -lib Gem"
-OBJECTS="objects.txt"
+OBJECT_FILE="objects.txt"
 
 if [ -r "$1" ]; then
- OBJECTS=$1
+ OBJECT_FILE=$1
 fi
 
+if [ "x${TESTPATCHES}" = "x" ]; then
 TESTPATCHES="          \
 	nop      \
 	_object_pix  \
 	_object_tex  \
 	"
-
+fi
 
 if [ "x${ORDERS}" = "x" ]; then
  ORDERS="${ORDERS} Oo OWo OWow OWwo WOo WOow WOwo"
  ORDERS="${ORDERS} OWRo WORo WROo OWRrwo OWRrow WORrwo WORrow WROrwo WROrow"
 fi
 
-rm -f "${LOGFILE}"
-cat "${OBJECTS}" | sed -e 's|;$||' | while read obj
-do
- for patch in ${TESTPATCHES}; do
-  for order in ${ORDERS}; do
-    echo "testing ${patch} ($order) with ${obj}..." 1>&2
+
+
+for patch in ${TESTPATCHES}; do
+    prerender=${patch}
     if test ! -e "${patch}.pd" ; then
-     patch="t a"
+     prerender="t a"
     fi
-    sed -e "s|@PRERENDER@|${patch}|g" object_gemstate.template > object_gemstate.pd
-    order=$(echo $order | grep -o .)
-    "${PD}" ${PDARGS} -open object_gemstate.pd -send "order ${order}" -send "object ${obj}" 2>&1
+    sed -e "s|@PRERENDER@|${prerender}|g" object_gemstate.template > "object_gemstate.${patch}.pd"
+done
+
+
+rm -f "${LOGFILE}"
+if which parallel >/dev/null; then
+ echo "parallel found"
+ parallel ./gemstate_run.sh :::: "${OBJECT_FILE}" ::: ${TESTPATCHES} ::: ${ORDERS} >> "${LOGFILE}"
+else
+ cat "${OBJECT_FILE}" | while read obj
+ do
+  for patch in ${TESTPATCHES}; do
+   for order in ${ORDERS}; do
+     echo "testing ${patch} ($order) with ${obj}..." 1>&2
+     order=$(echo $order | grep -o .)
+     "${PD}" ${PDARGS} -open "object_gemstate.${patch}.pd" -send "order ${order}" -send "object ${obj}" 2>&1
+   done
   done
- done
-done >> "${LOGFILE}"
+ done >> "${LOGFILE}"
+fi
