b6eea7c3d952a5b9e661109b4305f3c7c5f8282e
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-09-19 13:59:01 +0200

    fixed copyright

--- src/Output/gemglxwindow.cpp
+++ src/Output/gemglxwindow.cpp
@@ -2,13 +2,13 @@
 //
 // GEM - Graphics Environment for Multimedia
 //
-// zmoelnig@iem.kug.ac.at
+// zmoelnig@iem.at
 //
 // Implementation file
 //
 //    Copyright (c) 1997-2000 Mark Danks.
 //    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2013 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//    Copyright (c) 2001-2014 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 //
