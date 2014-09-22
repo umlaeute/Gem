3bcb0bad704d65269a96dc2f193a78fe1fbc217f
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-09-22 11:38:28 +0200

    GLuintMap implementation
    to map glsl-shaderIDs to t_floats

--- src/Utils/GLUtil.cpp
+++ src/Utils/GLUtil.cpp
@@ -16,9 +16,7 @@
 /////////////////////////////////////////////////////////
 
 #include "GLUtil.h"
-#include <ctype.h>
-#include <string.h>
-
+#include <map>
 #include "Gem/RTE.h"
 
 // I hate Microsoft...I shouldn't have to do this!
@@ -44,9 +42,61 @@ GLenum gem::utils::gl::glReportError (bool verbose)
     return err;
 }
 
+#warning TODO: use gem::ContextData
+using namespace gem::utils::gl;
+struct gem::utils::gl::GLuintMap::PIMPL {
+  std::map<float, GLuint>idmap;
+  float nextfloat;
+  PIMPL(void)
+    : nextfloat(-0.5)
+  {}
+  float getNewKey() {
+    nextfloat+=1.0;
+    while(idmap.count(nextfloat>0))
+      nextfloat+=1.0;
+    return nextfloat;
+  }
+  void del(float id){
+    std::map<float,GLuint>::iterator it = idmap.find(id);
+    if(idmap.end() != it) {
+      idmap.erase(it);
     }
   }
 
-}
+  static std::map<std::string, PIMPL*>s_pool;
+  static PIMPL*getFromPool(const std::string&name) {
+    if(s_pool.count(name)>0) {
+      return s_pool[name];
+    }
+    PIMPL*pimpl=new PIMPL();
+    s_pool[name]=pimpl;
+    return pimpl;
   }
+};
+std::map<std::string, GLuintMap::PIMPL*>GLuintMap::PIMPL::s_pool;
+
+GLuintMap::GLuintMap(const std::string&name)
+  : m_pimpl(PIMPL::getFromPool(name))
+{}
+
+GLuintMap::~GLuintMap()
+{}
+GLuint GLuintMap::get(float f) throw(GemException&){
+  if(m_pimpl->idmap.count(f)>0) {
+    GLuint i = m_pimpl->idmap[f];
+    return i;
+  } else {
+    throw(GemException("illegal key"));
+  }
+  return 0;
+}
+float GLuintMap::set(GLuint i, float f){
+  if(f==0.f) {
+    f=m_pimpl->getNewKey();
+  }
+  m_pimpl->idmap[f]=i;
+  return f;
+}
+void GLuintMap::del(float f){
+  m_pimpl->del(f);
 }
