7c3423df0857c7880606ad5b0b8fd7dd3c39c30b
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-09-22 11:38:41 +0200

    fixed copyright

--- src/Utils/GLUtil.cpp
+++ src/Utils/GLUtil.cpp
@@ -2,13 +2,13 @@
 //
 // GEM - Graphics Environment for Multimedia
 //
-// zmoelnig@iem.kug.ac.at
+// zmoelnig@iem.at
 //
 // Implementation file
 //
 //    Copyright (c) 1997-1999 Mark Danks.
 //    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//    Copyright (c) 2001-2014 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
 //
 //    For information on usage and redistribution, and for a DISCLAIMER OF ALL
 //    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
