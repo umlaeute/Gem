7bdd509b9f5342578bf5f2298fd41cbea0eaa37d
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2014-11-27 23:00:17 +0100

    dependency install-script

---
+++ build/travis-ci/depinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SCRIPTDIR=${0%/*}
+cd "${SCRIPTDIR}"
+INSTALLSCRIPT="./depinstall-${TRAVIS_OS_NAME}.sh"
+
+if [ -x "${INSTALLSCRIPT}" ]; then
+ envfile=$(INSTALLSCRIPT)}
+ mv "${envfile}" gem.env
+else
+ echo "no install-script found for: ${TRAVIS_OS_NAME}" 1>&2
+ echo "assuming all is well" 1>&2
+fi
+
+
