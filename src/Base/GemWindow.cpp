da52d9589493f9656e364383d95412831dd7194c
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-03-20 23:03:22 +0100

    on window-destruction, broadcast "context destroyed"

--- src/Base/GemWindow.cpp
+++ src/Base/GemWindow.cpp
@@ -322,6 +322,9 @@ bool GemWindow::createGemWindow(void){
 
 
 void GemWindow::destroyGemWindow(void){
+  // tell all objects that this context is vanishing
+  sendContextDestroyedMsg(gensym("__gemBase")->s_thing);
+  // do the rest
   m_pimpl->mycontext=destroyContext(m_pimpl->mycontext);
   m_pimpl->undispatch();
   m_context=m_pimpl->mycontext;
