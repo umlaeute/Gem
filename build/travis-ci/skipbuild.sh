95aa8130b1322495f4c1a25e809e9e0c3b20d216
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-03-22 20:35:50 +0100

    don't skip coverity_branch builds on OSX
    as the current method just pushes an empty archive to coverity, resulting in a failed analysis

--- build/travis-ci/skipbuild.sh
+++ build/travis-ci/skipbuild.sh
@@ -6,8 +6,8 @@ error() {
 
 ## skip coverity_scan builds on anything but linux
 if [ "x${TRAVIS_OS_NAME}" = "xosx" -a "x${COVERITY_SCAN_BRANCH}" = "x1" ]; then
-  error "skipping coverity_scan build on OSX"
-  exit 0
+  error "(not) skipping coverity_scan build on OSX"
+  #exit 0
 fi
 
 exit 1
