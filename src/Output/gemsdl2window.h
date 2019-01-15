35a3996c1cf90c7be13853c87dcf8e733b86b13a
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-01-15 08:35:53 +0100

    added (non-compilable) gemsdl2window class
    simple rename from gemsdlwindow

---
+++ src/Output/gemsdl2window.h
@@ -0,0 +1,117 @@
+/*-----------------------------------------------------------------
+  LOG
+  GEM - Graphics Environment for Multimedia
+
+  Interface for the window manager
+
+  Copyright (c) 2009-2012 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+  For information on usage and redistribution, and for a DISCLAIMER OF ALL
+  WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+  -----------------------------------------------------------------*/
+
+#ifndef _INCLUDE__GEM_OUTPUT_GEMSDL2WINDOW_H_
+#define _INCLUDE__GEM_OUTPUT_GEMSDL2WINDOW_H_
+
+#include "Base/GemWindow.h"
+#include <SDL.h>
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  gemsdl2window
+
+  The window manager
+
+  DESCRIPTION
+
+  Access to GemMan.
+
+  "bang"  - swap the buffers
+  "render" - render a frame now
+
+  "create" - create a graphics window
+  "destroy" - destroy the graphics window
+
+
+  "buffer" - single or double buffered
+  "fsaa" - full screen anti-aliasing
+
+  "title" - set a title for the graphics window
+  "border" - whether we want a border as decoration or not
+
+  "dimen" - the window dimensions
+  "fullscreen" - fullscreen mode
+  "offset" - the window offset
+  "secondscreen" - render to the secondscreen (auto-offset)
+
+  "cursor" - whether we want a cursor or not
+  "menubar" - hide notorious menubars
+  "topmost" - set the window to stay on top
+
+  -----------------------------------------------------------------*/
+
+
+class GEM_EXPORT gemsdl2window : public GemWindow
+{
+  CPPEXTERN_HEADER(gemsdl2window, GemWindow);
+
+public:
+
+  //////////
+  // Constructor
+  gemsdl2window(void);
+
+private:
+
+  //////////
+  // Destructor
+  virtual ~gemsdl2window(void);
+
+  void doRender(void);
+
+  /* rendering */
+  void renderMess(void);
+
+  /* render context (pre creation) */
+  void  bufferMess(int buf);
+
+  /* window decoration (pre creation) */
+  virtual void titleMess(const std::string&s);
+
+  /* window position/dimension (pre creation) */
+  virtual void    dimensionsMess(unsigned int width, unsigned int height);
+  virtual void    fullscreenMess(int on);
+
+  /* creation/destruction */
+  virtual bool        create(void);
+  virtual void destroy(void);
+
+  virtual void        createMess(const std::string&);
+  virtual void       destroyMess(void);
+
+  /* post creation */
+  void menuMess(void);
+  void addMenuMess(t_symbol*, int, t_atom*);
+
+
+  // check whether we have a window and if so, make it current
+  virtual bool makeCurrent(void);
+  // swap buffers
+  virtual void swapBuffers(void);
+  // dispatch events
+  virtual void dispatch(void);
+
+private:
+
+  /* the SDL surface */
+  SDL_Surface*m_surface;
+
+  /* hints how to create the window */
+  int m_videoFlags;
+
+  /* bits per pixel */
+  unsigned int m_bpp;
+};
+
+#endif    // for header file
