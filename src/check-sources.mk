81018156843d655a33ef6b5116fd6eb21293d8b1
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-01-16 13:35:16 +0100

    improve check-sources target for out-of-tree builds

--- src/check-sources.mk
+++ src/check-sources.mk
@@ -6,7 +6,7 @@
 
 all-local:: check-sources
 
-_check_sources_all=$(wildcard *.cpp *.h)
+_check_sources_all=$(notdir $(wildcard $(srcdir)/*.cpp $(srcdir)/*.h))
 _check_sources_unused=$(filter-out $(DIST_SOURCES) $(CHECKUNUSED_IGNORE), $(_check_sources_all))
 
 check-sources:
