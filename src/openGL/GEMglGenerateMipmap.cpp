933a429ca937088a4738e3ad8a16a98cccfc06c3
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-01-16 11:27:02 +0100

    added [GEMglGenerateMipMap]
    Closes: https://github.com/umlaeute/Gem/issues/208

---
+++ src/openGL/GEMglGenerateMipmap.cpp
@@ -0,0 +1,95 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// Implementation file
+//
+// Copyright (c) 2002-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//      zmoelnig@iem.at
+//  For information on usage and redistribution, and for a DISCLAIMER
+//  *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+//
+//  this file has been generated...
+////////////////////////////////////////////////////////
+
+#include "GEMglGenerateMipmap.h"
+#include "Gem/Exception.h"
+
+CPPEXTERN_NEW_WITH_GIMME ( GEMglGenerateMipmap );
+using namespace gem::utils::gl;
+
+/////////////////////////////////////////////////////////
+//
+// GEMglViewport
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+GEMglGenerateMipmap :: GEMglGenerateMipmap (int argc, t_atom*argv) :
+  target(0)
+{
+  switch(argc) {
+  case 0: break;
+  case 1:
+    targetMess(argv[0]);
+    break;
+  default:
+    throw(GemException("invalid number of arguments"));
+  }
+  m_inlet[0] = inlet_new(this->x_obj, &this->x_obj->ob_pd, &s_float,
+                         gensym("target"));
+}
+/////////////////////////////////////////////////////////
+// Destructor
+//
+GEMglGenerateMipmap :: ~GEMglGenerateMipmap ()
+{
+  inlet_free(m_inlet[0]);
+}
+//////////////////
+// extension check
+bool GEMglGenerateMipmap:: isRunnable(void)
+{
+  if(GLEW_VERSION_1_3) {
+    return true;
+  }
+  error("your system does not support OpenGL-1.3");
+  return false;
+}
+
+/////////////////////////////////////////////////////////
+// Render
+//
+void GEMglGenerateMipmap :: render(GemState *state)
+{
+  glGenerateMipmap (target);
+}
+
+/////////////////////////////////////////////////////////
+// Variables
+//
+void GEMglGenerateMipmap :: targetMess (t_atom arg)        // FUN
+{
+  target = static_cast<GLenum>(getGLdefine(&arg));
+  setModified();
+}
+
+
+/////////////////////////////////////////////////////////
+// static member functions
+//
+
+void GEMglGenerateMipmap :: obj_setupCallback(t_class *classPtr)
+{
+  class_addmethod(classPtr,
+                  reinterpret_cast<t_method>(&GEMglGenerateMipmap::targetMessCallback),   gensym("target"),
+                  A_GIMME, A_NULL);
+};
+
+void GEMglGenerateMipmap :: targetMessCallback (void* data, t_symbol*, int argc,
+                                     t_atom*argv)
+{
+  if(argc==1) {
+    GetMyClass(data)->targetMess ( argv[0]);
+  }
+}
---
+++ src/openGL/GEMglGenerateMipmap.h
@@ -0,0 +1,56 @@
+/* ------------------------------------------------------------------
+ * GEM - Graphics Environment for Multimedia
+ *
+ *  Copyright (c) 2002-2018 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+ *     zmoelnig@iem.at
+ *  For information on usage and redistribution, and for a DISCLAIMER
+ *  OF ALL WARRANTIES, see the file, "GEM.LICENSE.TERMS"
+ *
+ *  this file has been generated...
+ * ------------------------------------------------------------------
+ */
+
+#ifndef _INCLUDE__GEM_OPENGL_GEMGLGENERATEMIPMAP_H_
+#define _INCLUDE__GEM_OPENGL_GEMGLGENERATEMIPMAP_H_
+
+#include "Base/GemGLBase.h"
+
+/*
+ CLASS
+        GEMglGenerateMipmap
+ KEYWORDS
+        openGL  0
+ DESCRIPTION
+        wrapper for the openGL-function
+        "glGenerateMipmap( GLenum target)"
+ */
+
+class GEM_EXTERN GEMglGenerateMipmap : public GemGLBase
+{
+  CPPEXTERN_HEADER(GEMglGenerateMipmap, GemGLBase);
+
+public:
+  // Constructor
+  GEMglGenerateMipmap (int, t_atom*); // CON
+
+protected:
+  // Destructor
+  virtual ~GEMglGenerateMipmap ();
+  // Do the rendering
+  virtual bool isRunnable(void);
+  virtual void  render (GemState *state);
+
+  // variables
+  GLenum        target;            // VAR
+  virtual void  targetMess(t_atom);        // FUN
+
+
+private:
+
+  // we need some inlets
+  t_inlet *m_inlet[1];
+
+  // static member functions
+  static void    targetMessCallback (void*,t_symbol*,int,t_atom*);
+};
+#endif // for header file
