c89df285c8172671a2958747186e582971367eb5
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-01-20 23:47:00 +0100

    image reading/saving with STB

---
+++ plugins/imageSTB/imageSTB.cpp
@@ -0,0 +1,139 @@
+////////////////////////////////////////////////////////
+//
+// GEM - Graphics Environment for Multimedia
+//
+// zmoelnig@iem.at
+//
+// Implementation file
+//
+//    Copyright (c) 1997-1999 Mark Danks.
+//    Copyright (c) Günther Geiger.
+//    Copyright (c) 2001-2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
+//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+//
+/////////////////////////////////////////////////////////
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <string.h>
+#include "imageSTB.h"
+#include "Gem/RTE.h"
+#include "plugins/PluginFactory.h"
+
+#ifdef HAVE_LIBSTB
+# define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+#ifdef HAVE_STB_STB_IMAGE_H
+# include "stb/stb_image.h"
+# include "stb/stb_image_write.h"
+#else
+# include "stb_image.h"
+# include "stb_image_write.h"
+#endif
+
+using namespace gem::plugins;
+
+REGISTER_IMAGELOADERFACTORY("STB", imageSTB);
+REGISTER_IMAGESAVERFACTORY ("STB", imageSTB);
+
+/////////////////////////////////////////////////////////
+//
+// imageSTB
+//
+/////////////////////////////////////////////////////////
+// Constructor
+//
+/////////////////////////////////////////////////////////
+imageSTB :: imageSTB(void)
+{
+}
+imageSTB :: ~imageSTB(void)
+{
+}
+
+/////////////////////////////////////////////////////////
+// really open the file ! (OS dependent)
+//
+/////////////////////////////////////////////////////////
+bool imageSTB :: load(std::string filename, imageStruct&result,
+                      gem::Properties&props)
+{
+  int xsize, ysize, csize;
+  unsigned char *data = stbi_load(filename.c_str(), &xsize, &ysize, &csize, 4);
+
+  if(!data)
+    return(false);
+
+  result.xsize=xsize;
+  result.ysize=ysize;
+  result.setCsizeByFormat(GEM_RGBA);
+  result.reallocate();
+
+  result.fromRGBA(data);
+  stbi_image_free(data);
+  return true;
+}
+
+bool imageSTB::save(const imageStruct&image, const std::string&filename,
+                    const std::string&mimetype, const gem::Properties&props)
+{
+  int err = 0;
+  imageStruct img;
+  double fquality=100;
+  int quality=fquality;
+
+  if(props.get("quality", fquality)) {
+    quality=fquality;
+  }
+
+  image.convertTo(&img, GEM_RGBA);
+
+  if("image/png" == mimetype) {
+    err = stbi_write_png(filename.c_str(), img.xsize, img.ysize, img.csize, img.data, img.xsize * img.csize);
+  } else if ("image/bmp" == mimetype) {
+    err = stbi_write_bmp(filename.c_str(), img.xsize, img.ysize, img.csize, img.data);
+  } else if ("image/tga" == mimetype) {
+    err = stbi_write_tga(filename.c_str(), img.xsize, img.ysize, img.csize, img.data);
+  } else if ("image/jpeg" == mimetype) {
+    err = stbi_write_jpg(filename.c_str(), img.xsize, img.ysize, img.csize, img.data, quality);
+  }
+
+  return (0!=err);
+}
+float imageSTB::estimateSave(const imageStruct&img,
+                             const std::string&filename, const std::string&mimetype,
+                             const gem::Properties&props)
+{
+  float result=0;
+  if("image/png" == mimetype) {
+    result+=80;
+  } else if ("image/bmp" == mimetype) {
+    result+=80;
+  } else if ("image/tga" == mimetype) {
+    result+=80;
+  } else if ("image/jpeg" == mimetype) {
+    result+=80;
+    if(gem::Properties::UNSET != props.type("quality")) {
+      result += 1.;
+    }
+
+  }
+  return result;
+}
+void imageSTB::getWriteCapabilities(std::vector<std::string>&mimetypes,
+                                    gem::Properties&props)
+{
+  mimetypes.clear();
+  props.clear();
+  mimetypes.push_back("image/png");
+  mimetypes.push_back("image/bmp");
+  mimetypes.push_back("image/tga");
+  mimetypes.push_back("image/jpeg");
+  //mimetypes.push_back("image/hdr"); // ??; expects float
+  gem::any value;
+
+  value=100.f;
+  props.set("quality", value);
+}
---
+++ plugins/imageSTB/imageSTB.h
@@ -0,0 +1,76 @@
+/*-----------------------------------------------------------------
+
+GEM - Graphics Environment for Multimedia
+
+Use Sean T. Barret's single-file header-lib to load images
+
+Copyright (c) 2011 IOhannes m zmölnig. forum::für::umläute. IEM. zmoelnig@iem.at
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
+
+
+-----------------------------------------------------------------*/
+
+#ifndef _INCLUDE_GEMPLUGIN__IMAGESTB_IMAGESTB_H_
+#define _INCLUDE_GEMPLUGIN__IMAGESTB_IMAGESTB_H_
+#include "plugins/imageloader.h"
+#include "plugins/imagesaver.h"
+#include <stdio.h>
+
+/*-----------------------------------------------------------------
+  -------------------------------------------------------------------
+  CLASS
+  imageSTB
+
+  Loads in a picture
+
+  KEYWORDS
+  pix
+
+  DESCRIPTION
+
+  -----------------------------------------------------------------*/
+namespace gem
+{
+namespace plugins
+{
+class GEM_EXPORT imageSTB : public gem::plugins::imageloader,
+  public gem::plugins::imagesaver
+{
+public:
+
+  //////////
+  // Constructor
+  imageSTB(void);
+  virtual ~imageSTB(void);
+
+  //////////
+  // read an image
+  virtual bool load(std::string filename, imageStruct&result,
+                    gem::Properties&props);
+  //////////
+  // write an image
+  virtual bool          save(const imageStruct&img,
+                             const std::string&filename, const std::string&mimetype,
+                             const gem::Properties&props);
+  //////////
+  // estimate, how well we could save this image
+  virtual float estimateSave(const imageStruct&img,
+                             const std::string&filename, const std::string&mimetype,
+                             const gem::Properties&props);
+
+
+  // this is always threadable
+  virtual bool isThreadable(void)
+  {
+    return true;
+  }
+  virtual void getWriteCapabilities(std::vector<std::string>&mimetypes,
+                                    gem::Properties&props);
+
+
+};
+};
+};
+
+#endif  // for header file
