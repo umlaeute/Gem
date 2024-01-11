622908f72d8d39a14b650bc13a9e12a55d06a227
IOhannes m zmölnig <zmoelnig@iem.at>
2024-01-11 16:48:05 +0100

    use PIMPL idiom for gem::modelGL

--- src/Gem/model.cpp
+++ src/Gem/model.cpp
@@ -15,6 +15,7 @@
 #include "model.h"
 #include <algorithm> // std::min
 #include "m_pd.h"
+#include "plugins/modelloader.h"
 
 namespace
 {
@@ -25,64 +26,85 @@ namespace
     else
       vec.clear();
   }
+  struct modelmesh {
+    unsigned int size;
+    std::vector<float> vVertices, vNormals, vColors, vTexcoords;
+    gem::VBO vertices, normals, colors, texcoords;
+    gem::plugins::modelloader::material material;
+
+    modelmesh(gem::plugins::modelloader::mesh*m)
+      : size(m->size)
+      , vertices(GL_VERTEX_ARRAY)
+      , normals(GL_NORMAL_ARRAY)
+      , colors(GL_COLOR_ARRAY)
+      , texcoords(GL_TEXTURE_COORD_ARRAY)
+      , material(m->material)
+    {
+      setvector(vVertices, m->vertices, m->size * 3);
+      setvector(vNormals, m->normals, m->size * 3);
+      setvector(vColors, m->colors, m->size * 4);
+      setvector(vTexcoords, m->texcoords, m->size * 2);
+    }
+    void update(void)
+    {
+      vertices.update(size, vVertices.data());
+      normals.update(size, vNormals.data());
+      colors.update(size, vColors.data());
+      texcoords.update(size, vTexcoords.data());
+    }
+    void render(GLenum drawType) const
+    {
+      std::vector<size_t>sizes;
+      size_t sizeV=0, sizeN=0, sizeC=0, sizeT=0;
+      sizeV = vertices.render();
+      if(sizeV>0)sizes.push_back(sizeV);
+      sizeN = normals.render();
+      if(sizeN>0)sizes.push_back(sizeN);
+      sizeC = colors.render();
+      if(sizeC>0)sizes.push_back(sizeC);
+      sizeT = texcoords.render();
+      if(sizeT>0)sizes.push_back(sizeT);
+
+      if ( sizes.size() > 0 ) {
+        unsigned int npoints = *std::min_element(sizes.begin(),sizes.end());
+        glDrawArrays(drawType, 0, npoints);
+      }
+
+      if(sizeV)glDisableClientState(GL_VERTEX_ARRAY);
+      if(sizeN)glDisableClientState(GL_NORMAL_ARRAY);
+      if(sizeC)glDisableClientState(GL_COLOR_ARRAY);
+      if(sizeT)glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    }
+  };
+
+
 };
 
 namespace gem {
-  modelGL :: modelmesh :: modelmesh(gem::plugins::modelloader::mesh*m)
-    : size(m->size)
-    , vertices(GL_VERTEX_ARRAY)
-    , normals(GL_NORMAL_ARRAY)
-    , colors(GL_COLOR_ARRAY)
-    , texcoords(GL_TEXTURE_COORD_ARRAY)
-    , material(m->material)
-  {
-    setvector(vVertices, m->vertices, m->size * 3);
-    setvector(vNormals, m->normals, m->size * 3);
-    setvector(vColors, m->colors, m->size * 4);
-    setvector(vTexcoords, m->texcoords, m->size * 2);
-  }
-  void modelGL :: modelmesh :: update(void)
-  {
-    vertices.update(size, vVertices.data());
-    normals.update(size, vNormals.data());
-    colors.update(size, vColors.data());
-    texcoords.update(size, vTexcoords.data());
-  }
-  void modelGL :: modelmesh :: render(GLenum drawType) const
-  {
-    std::vector<size_t>sizes;
-    size_t sizeV=0, sizeN=0, sizeC=0, sizeT=0;
-    sizeV = vertices.render();
-    if(sizeV>0)sizes.push_back(sizeV);
-    sizeN = normals.render();
-    if(sizeN>0)sizes.push_back(sizeN);
-    sizeC = colors.render();
-    if(sizeC>0)sizes.push_back(sizeC);
-    sizeT = texcoords.render();
-    if(sizeT>0)sizes.push_back(sizeT);
+  class modelGL :: PIMPL {
+  public:
+    std::vector<struct modelmesh>mesh;
 
-    if ( sizes.size() > 0 ) {
-      unsigned int npoints = *std::min_element(sizes.begin(),sizes.end());
-      glDrawArrays(drawType, 0, npoints);
-    }
+    unsigned int drawType;
+    bool useMaterial;
+    float texScale[2];
 
-    if(sizeV)glDisableClientState(GL_VERTEX_ARRAY);
-    if(sizeN)glDisableClientState(GL_NORMAL_ARRAY);
-    if(sizeC)glDisableClientState(GL_COLOR_ARRAY);
-    if(sizeT)glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  }
+    PIMPL()
+      : drawType(GL_TRIANGLES)
+      , useMaterial(false)
+      , texScale{1.0, 1.0}
+    { ; }
+  };
 
 
   modelGL::modelGL(gem::plugins::modelloader*loader)
-    : m_drawType(GL_TRIANGLES)
-    , m_useMaterial(false)
-    , m_texscale{1.0, 1.0}
+    : m_pimpl(new PIMPL())
   {
     const size_t nummeshes = loader->getNumMeshes();
     for(size_t n=0; n < nummeshes; n++) {
       gem::plugins::modelloader::mesh*mesh = loader->getMesh(n);
       if(mesh) {
-        m_mesh.push_back(modelmesh(mesh));
+        m_pimpl->mesh.push_back(modelmesh(mesh));
       }
     }
   }
@@ -90,7 +112,7 @@ namespace gem {
     if(true) {
       if(GLEW_VERSION_1_5) {
         /* update the VBOs */
-        for (auto&m: m_mesh) {
+        for (auto&m: m_pimpl->mesh) {
           m.update();
         }
       }
@@ -99,7 +121,7 @@ namespace gem {
     return false;
   }
   void modelGL::render(void) {
-    const unsigned int count = m_mesh.size();
+    const unsigned int count = m_pimpl->mesh.size();
     std::vector<unsigned int>groups;
     for (unsigned int g=0; g<count; g++) {
       groups.push_back(g);
@@ -107,23 +129,23 @@ namespace gem {
     render(groups);
   }
   void modelGL::render(std::vector<unsigned int>&meshes) {
-    const unsigned int numMeshes = m_mesh.size();
+    const unsigned int numMeshes = m_pimpl->mesh.size();
     if(!GLEW_VERSION_1_5) {
       for(auto n: meshes) {
         if(n >= numMeshes)
           continue;
-        const auto &m = m_mesh[n];
+        const auto &m = m_pimpl->mesh[n];
         const size_t size = m.size;
         const float*positions = size?m.vVertices.data():0;
         const float*textures = size?m.vTexcoords.data():0;
         const float*colors = size?m.vColors.data():0;
         const float*normals = size?m.vNormals.data():0;
 
-        if(m_useMaterial) {
+        if(m_pimpl->useMaterial) {
           gem::plugins::modelutils::render_material(m.material);
         }
 
-        glBegin(m_drawType);
+        glBegin(m_pimpl->drawType);
         for (unsigned int i=0; i<size; i++) {
           if(normals) {
             glNormal3fv(normals);
@@ -147,24 +169,24 @@ namespace gem {
     } else { /* openGL-2+ */
       for(auto n: meshes) {
         if (n >= numMeshes) continue;
-        const auto&m = m_mesh[n];
-        if(m_useMaterial) {
+        const auto&m = m_pimpl->mesh[n];
+        if(m_pimpl->useMaterial) {
           gem::plugins::modelutils::render_material(m.material);
         }
-        m.render(m_drawType);
+        m.render(m_pimpl->drawType);
       }
     }
   }
 
   void modelGL :: setDrawType(GLenum drawtype) {
-    m_drawType = drawtype;
+    m_pimpl->drawType = drawtype;
   }
   void modelGL :: useMaterial(bool use) {
-    m_useMaterial = use;
+    m_pimpl->useMaterial = use;
   }
   void modelGL :: setTexture(float w, float h) {
-    m_texscale[0] = w;
-    m_texscale[1] = h;
+    m_pimpl->texScale[0] = w;
+    m_pimpl->texScale[1] = h;
   }
 
 };
--- src/Gem/model.h
+++ src/Gem/model.h
@@ -18,30 +18,24 @@
 #define _INCLUDE__GEM_MODEL_H_
 
 #include "Gem/VertexBuffer.h"
-#include "plugins/modelloader.h"
 #include <vector>
+
 
 namespace gem
 {
+  namespace plugins {
+    class modelloader;
+  };
   /* holds a model and renders it */
   class modelGL {
   public:
-  struct modelmesh {
-    unsigned int size;
-    std::vector<float> vVertices, vNormals, vColors, vTexcoords;
-    gem::VBO vertices, normals, colors, texcoords;
-    gem::plugins::modelloader::material material;
-    modelmesh(gem::plugins::modelloader::mesh*m);
-    void update(void);
-    void render(GLenum drawtype) const;
-  };
     /* ctor
      * initialize the model
      */
     modelGL(gem::plugins::modelloader*loader);
-    /* update data from the loader */
+    /* update data */
     bool update(void);
-    /* render the model in the currrent openGL context */
+    /* render the model in the current openGL context */
     void render(void);
     /* render specified meshes of the model in the currrent openGL context */
     void render(std::vector<unsigned int>&meshes);
@@ -51,11 +45,8 @@ namespace gem
     void useMaterial(bool);
     void setTexture(float w, float h);
  private:
-    std::vector<struct modelmesh>m_mesh;
-
-    unsigned int m_drawType;
-    bool m_useMaterial;
-    float m_texscale[2];
+    class PIMPL;
+    PIMPL*m_pimpl;
   };
 }; /* namespace gem */
 
