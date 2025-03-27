8902fadfa09beb26a45109500480ec1654e85edf
IOhannes m zmölnig <zmoelnig@iem.at>
2025-03-27 12:00:47 +0100

    Gem: forward [debugGL( messages to all Gem objects
    Closes: https://github.com/umlaeute/Gem/issues/475

--- src/Geos/GemSplash.cpp
+++ src/Geos/GemSplash.cpp
@@ -59,4 +59,29 @@ void GemSplash :: obj_setupCallback(t_class *classPtr)
 {
   class_addcreator(reinterpret_cast<t_newmethod>(create_GemSplash),
                    gensym("Gem"), A_GIMME, A_NULL);
+
+  /* debugging GL errors */
+  struct _CallbackClass_debugGL {
+    static void callback(void*data, t_float v0)
+    {
+      static bool sending = false;
+      if(!sending) {
+        sending = true;
+        t_symbol*s = gensym("__gemBase");
+        if(s->s_thing) {
+          t_atom a[1];
+          SETFLOAT(a+0, v0);
+          pd_typedmess(s->s_thing, gensym("debugGL"), 1, a);
+        }
+      }
+      sending = false;
+
+    }
+    explicit _CallbackClass_debugGL (struct _class*c)
+    {
+      class_addmethod(c, reinterpret_cast<t_method>(callback),
+                      gensym("debugGL"), A_FLOAT, A_NULL);
+    }
+  };
+  _CallbackClass_debugGL _CallbackClassInstance_debugGL (classPtr);
 }
