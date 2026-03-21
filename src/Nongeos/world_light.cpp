c85178b2d1ed260f76f66255924fe1ff759cf553
Ben Wesch <benjamin.wesch@gmail.com>
2026-03-21 10:20:32 +0100

    fix light initialization across multiple contexts

--- src/Nongeos/world_light.cpp
+++ src/Nongeos/world_light.cpp
@@ -195,7 +195,7 @@ void world_light :: render(GemState *state)
     return;
   }
 
-  if (m_change) {
+  if (m_change || !glIsEnabled(m_light)) {
     m_change = 0;
     if ( !m_on ) {
       glDisable(m_light);
