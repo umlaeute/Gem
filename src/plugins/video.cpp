b2b4567b8cd577f75af0770e30fe6007deeebf3e
IOhannes m zmölnig <zmoelnig@iem.at>
2026-05-06 10:53:20 +0200

    video: print the usable plugins, not all
    Closes: https://github.com/umlaeute/Gem/issues/544

--- src/plugins/video.cpp
+++ src/plugins/video.cpp
@@ -132,10 +132,10 @@ public:
     }
 
     static bool firsttime=true;
-    if(firsttime && ids.size()>0) {
+    if(firsttime && m_ids.size()>0) {
       startpost("GEM: video capture plugins:");
-      for(unsigned int i=0; i<ids.size(); i++) {
-        startpost(" %s", ids[i].c_str());
+      for(unsigned int i=0; i<m_ids.size(); i++) {
+        startpost(" %s", m_ids[i].c_str());
       }
       endpost();
     }
